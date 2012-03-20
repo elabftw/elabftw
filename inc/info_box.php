1c4d3463454d19f6e1795e32490ac023fb6d5273
Nicolas CARPi <nicolas.carpi@curie.fr>
2012-03-20 12:36:09 +0100

    added images to info box, icons from https://drupal.org/project/protocons

--- inc/info_box.php
+++ inc/info_box.php
@@ -1,7 +1,7 @@
 <?php
 // INFO BOX
 if( isset($_SESSION['errors']) && is_array($_SESSION['errors']) && count($_SESSION['errors']) >0 ) {
-    echo '<ul class="err">';
+    echo "<ul class='err'><img src='themes/".$_SESSION['prefs']['theme']."/img/error.png' alt='fail' /> ";
     foreach($_SESSION['errors'] as $msg) {
         echo '<li>',$msg,'</li>'; 
     }
@@ -9,7 +9,7 @@ if( isset($_SESSION['errors']) && is_array($_SESSION['errors']) && count($_SESSI
     unset($_SESSION['errors']);
 }
 if( isset($_SESSION['infos']) && is_array($_SESSION['infos']) && count($_SESSION['infos']) >0 ) {
-    echo "<ul class='infos'>";
+    echo "<ul class='infos'><img src='themes/".$_SESSION['prefs']['theme']."/img/ok.png' alt='ok' /> ";
     foreach($_SESSION['infos'] as $msg) {
         echo "<li>".$msg."</li>";
     }

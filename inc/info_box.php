da0799749b3926e6951e2ee376e39771f5698afd
Nicolas CARPi <nicolas.carpi@curie.fr>
2012-03-21 14:42:27 +0100

    changed css for inline li

--- inc/info_box.php
+++ inc/info_box.php
@@ -3,7 +3,7 @@
 if( isset($_SESSION['errors']) && is_array($_SESSION['errors']) && count($_SESSION['errors']) >0 ) {
     echo "<ul class='err'><img src='img/error.png' alt='fail' /> ";
     foreach($_SESSION['errors'] as $msg) {
-        echo '<li>',$msg,'</li>'; 
+        echo "<li class='inline'>".$msg."</li>"; 
     }
     echo '</ul>';
     unset($_SESSION['errors']);
@@ -11,7 +11,7 @@ if( isset($_SESSION['errors']) && is_array($_SESSION['errors']) && count($_SESSI
 if( isset($_SESSION['infos']) && is_array($_SESSION['infos']) && count($_SESSION['infos']) >0 ) {
     echo "<ul class='infos'><img src='img/ok.png' alt='ok' /> ";
     foreach($_SESSION['infos'] as $msg) {
-        echo "<li>".$msg."</li>";
+        echo "<li class='inline'>".$msg."</li>"; 
     }
     echo "</ul>";
     unset($_SESSION['infos']);

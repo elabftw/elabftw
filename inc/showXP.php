cabd719844a1be558e6e0f66917b37ead57dc325
Nicolas CARPi <nicolas.carpi@curie.fr>
2012-04-03 00:50:43 +0200

    yep

--- inc/showXP.php
+++ inc/showXP.php
@@ -28,8 +28,7 @@ require_once("themes/".$_SESSION['prefs']['theme']."/highlight.css");
 
 <h2>EXPERIMENTS</h2>
 <hr class='flourishes'>
-<div id='submenu'><a href="create_item.php?type=exp"><img src="themes/<?php echo $_SESSION['prefs']['theme'];?>/img/create.gif" alt="" /> Create experiment</a> | 
-<a href='search.php'><img src='themes/<?php echo $_SESSION['prefs']['theme'];?>/img/search.png' alt='' /> Search</a>
+<div id='submenu'><a href="create_item.php?type=exp"><img src="themes/<?php echo $_SESSION['prefs']['theme'];?>/img/create.gif" alt="" /> Create experiment</a>
 <!-- Quick Search Box (search tags) -->
 <form id='quicksearch' method='get' action='experiments.php'>
 <input type='search' name='tag' placeholder='Search tag' />

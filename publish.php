c30ed46d21018e4728ab4b246b477a0b3b308e2f
Nicolas CARPi <nicolas.carpi@curie.fr>
2012-04-16 15:53:05 +0200

    addedd some rejection letters to publish.php

--- publish.php
+++ publish.php
@@ -29,29 +29,125 @@ $sql = "SELECT firstname, lastname, email FROM users WHERE userid=".$_SESSION['u
 $req = $bdd->prepare($sql);
 $req->execute();
 $data = $req->fetch();
-$email = $data['email'];
-$firstname = $data['firstname'];
-$lastname = $data['lastname'];
-$req->closeCursor();
+
+$body_pnas = "Dear Prof. ".$data['lastname'].",
+
+    I regret to inform you that the Editorial Board has rejected your manuscript. 
+We receive many more good papers than we can publish, so the Board has to decide which ones go on for further
+review. The Board concluded that your paper, while interesting, fell a
+bit below the standards of novelty and importance necessary for further
+consideration by the journal.
+
+Once a paper has been rejected, it may not be resubmitted through an
+Academy Member. Note that the copyright assignment conveyed at initial
+submission is terminated.
+
+Thank you for submitting to PNAS. I am sorry we cannot be more
+encouraging this time, and I hope that you will consider us in the
+future.
+
+    Sincerely yours,
+    Randy Schekman
+    Editor-in-Chief";
+
+$body_cell = "Dear ".$data['firstname'].",
+
+Your manuscript has now been evaluated by three reviewers, and I am enclo
+sing their comments.  I am sorry for the length of the review process.  U
+nfortunately, the consensus recommendation is against publication of the 
+paper in its current form in Cell.  As you will see, the reviewers find a
+spects of the work to be well done and potentially interesting; however, 
+each one raises a number of technical concerns about the results.  Perhap
+s more importantly to my mind, they also question whether the overall con
+clusions rise to the level of conceptual advance that would be required f
+or publication.  It would therefore be premature to proceed further with 
+the manuscript on the basis of the present results.  At this point, it is
+ not clear that a revised manuscript would be a strong candidate for publ
+ication, and it is therefore difficult for me to recommend resubmission.
+
+I am sorry that the outcome for this manuscript was not more positive.  I
+ do want to emphasize, however, that this is not intended to imply a lack
+ of interest on our part in either your work in particular or this field 
+in general, and we hope that you will continue to consider Cell for futur
+e submissions.
+
+
+Best wishes,
+
+Lara
+
+Lara Szewczak, Ph.D.
+Scientific Editor, Cell";
+
+$body_bioinformatics = "
+On behalf of the Associate Editor of Bioinformatics, Martin Bishop, I would
+ like to thank you for your time and effort in reviewing the above manuscri
+pt.
+
+After careful consideration of yours' and colleagues' criticisms, the final
+ decision on this paper was: Reject after Review.
+
+For your information, a copy of the decision is below with the reviewer and
+ editor comments included (if applicable).
+
+Many thanks for your assistance with the assessment of this submission.=
+
+Best regards,
+
+Alison Hutchins
+Editorial Office
+Bioinformatics
+
+_______________________________________________
+Decision letter:
+
+Dear Mr. ".$data['lastname'].",
+
+The reviews of your manuscript are now in hand. The reviewers had substanti
+al concerns about the manuscript and the Associate Editor, Martin Bishop, h
+as decided to reject your manuscript based on their advice.
+
+As the journal receives more publishable manuscripts than its space will ac
+commodate, acceptance must be limited to manuscripts receiving the most fav
+ourable recommendations from reviewers.  Unfortunately, I must decline your
+ request to have the manuscript published in Bioinformatics.   The Associat
+e Editor's general comments and those of the reviewers, can be found at the
+ foot of this e-mail. We will not consider any further versions of this man
+uscript, but I hope that you find the comments useful should you revise you
+r manuscript for another journal.
+
+On behalf of the Executive Editor, I would like to thank you for considerin
+g Bioinformatics to present your work and I look forward to the possibility
+ of receiving other manuscripts from you in the future.
+
+Best regards,
+Alison Hutchins
+Bioinformatics  Editorial office";
+
+// Select journal
+$journal = $_GET['journal'];
+if ($journal === 'pnas') {
+    $body = $body_pnas;
+} elseif ($journal === 'cell') {
+    $body = $body_cell;
+} elseif ($journal === 'bioinformatics') {
+    $body = $body_bioinformatics;
+} else {
+    die('Bad journal type.');
+}
+
 // EMAIL
 require_once('lib/swift_required.php');
 // Create the message
 $message = Swift_Message::newInstance()
 // Give the message a subject
-->setSubject('Your submission to Nature has been reviewed')
+->setSubject('Your submission to '.$journal.' has been reviewed')
 // Set the From address with an associative array
 ->setFrom(array('elabftw.net@gmail.com' => 'Nature Publishing Group'))
 // Set the To addresses with an associative array
-->setTo(array($email => $firstname.' '.$lastname))
+->setTo(array($data['email'] => $data['firstname'].' '.$data['lastname']))
 // Give it a body
-->setBody("Dear Dr.".$lastname.",
-    We have thoroughly read your manuscript and although we think it is great, we cannot publish it.
-    Keep it up bro.
-        
-Regards,
-~John B. ROOT
-Chief editor
-Nature publishing group
+->setBody($body."
 
 * This is NOT a real email; it is a joke from elabFTW *");
 // SEND
@@ -61,7 +157,7 @@ $transport = Swift_SmtpTransport::newInstance($ini_arr['smtp_address'], $ini_arr
     $mailer = Swift_Mailer::newInstance($transport);
     $result = $mailer->send($message);
     if ($result){
-$msg_arr[] = "Experiment successfully sent to Nature for publication. You should get an answer promptly by email.";
+$msg_arr[] = "Experiment successfully sent to ".$journal." for publication. You should get an answer promptly by email.";
 $_SESSION['infos'] = $msg_arr;
 session_write_close();
 header('Location: experiments.php');

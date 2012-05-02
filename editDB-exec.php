f2c20ff8b3bf601338d14781c2c36472457d3161
Nicolas CARPi <nicolas.carpi@curie.fr>
2012-05-02 15:29:35 +0200

    fix bug on file display/upload

--- editDB-exec.php
+++ editDB-exec.php
@@ -88,7 +88,7 @@ if (is_uploaded_file($_FILES['files']['tmp_name'][0])){
         'comment' => $filecomments[$i],
         'item_id' => $item_id,
         'userid' => $_SESSION['userid'],
-        'type' => 'item'
+        'type' => 'database'
     ));
     $req->closeCursor();
             } // end for each file loop

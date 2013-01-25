77a93aedf4b8b31af14318f3e15e1fd4ce71e400
Nicolas CARPi <nicolas.carpi@curie.fr>
2013-01-25 10:57:31 +0100

    no need for id in download.php

--- inc/display_file.php
+++ inc/display_file.php
@@ -59,7 +59,7 @@ if($count > 0){
             } // end if extension is valid
         } // end gd here
         // END THUMBNAIL GENERATION
-        echo "<img src='themes/".$_SESSION['prefs']['theme']."/img/attached_file.png' alt='' /> <a href='download.php?id=".$uploads_data['id']."&f=".$uploads_data['long_name']."&name=".$uploads_data['real_name']."' target='_blank'>".$uploads_data['real_name']."</a>
+        echo "<img src='themes/".$_SESSION['prefs']['theme']."/img/attached_file.png' alt='' /> <a href='download.php?f=".$uploads_data['long_name']."&name=".$uploads_data['real_name']."' target='_blank'>".$uploads_data['real_name']."</a>
         <span class='filesize'> (".format_bytes(filesize('uploads/'.$uploads_data['long_name'])).")</span><br />";
         echo "<img src='themes/".$_SESSION['prefs']['theme']."/img/comments.png' alt='comment' /> <p class='editable' id='comment_".$uploads_data['id']."'>".stripslashes($uploads_data['comment'])."</p></div>";
     } // end while

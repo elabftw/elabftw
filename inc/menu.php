0f50d4b203de6ad8c17f375fb56b1f3462a28311
Nicolas CARPi <nicolas.carpi@gmail.com>
2013-07-16 15:15:25 +0200

    git add curl check and admin/config.ini-EXAMPLE proxy

--- inc/menu.php
+++ inc/menu.php
@@ -42,51 +42,57 @@ if (isset($_SESSION['auth']) && $_SESSION['is_admin'] === '1') {
     // check if a new update is available
     // but before, check if git exists on the system 
     if (check_executable('git')) {
-        // get what is the latest commit on master branch
-        // we use curl and not git ls-remote to be able to input proxy settings
-        $ch = curl_init();
-        // set url
-        // for branch next
-        //curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/NicolasCARPi/elabftw/git/refs/heads/next");
-        // for branch master
-        curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/NicolasCARPi/elabftw/git/refs/heads/master");
-        // this is to get content
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // check if the proxy settings exists
-        if (!isset($ini_arr['proxy'])) {
-            echo "<a class='errors' href='https://github.com/NicolasCARPi/elabftw/wiki/Troubleshooting#manual-intervention-needed'>This update needs a manual intervention. Click this message.</a>";
-        } else {
-            // add proxy if there is one
-            if(strlen($ini_arr['proxy']) > 0) {
-                curl_setopt($ch, CURLOPT_PROXY, $ini_arr['proxy']);
+        // check that curl extension is installed and loaded
+        if (extension_loaded('curl')) {
+            // get what is the latest commit on master branch
+            // we use curl and not git ls-remote to be able to input proxy settings
+            $ch = curl_init();
+            // set url
+            // for branch next
+            //curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/NicolasCARPi/elabftw/git/refs/heads/next");
+            // for branch master
+            curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/NicolasCARPi/elabftw/git/refs/heads/master");
+            // this is to get content
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            // check if the proxy settings exists
+            if (!isset($ini_arr['proxy'])) {
+                echo "<a class='errors' href='https://github.com/NicolasCARPi/elabftw/wiki/Troubleshooting#manual-intervention-needed'>This update needs a manual intervention. Click this message.</a>";
+            } else {
+                // add proxy if there is one
+                if(strlen($ini_arr['proxy']) > 0) {
+                    curl_setopt($ch, CURLOPT_PROXY, $ini_arr['proxy']);
+                }
+                // options to verify the github certificate
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+                curl_setopt($ch, CURLOPT_CAPATH, $ini_arr['path']."ca_github.com.pem");
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+
+                // set a timeout of 500 millisecond
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 500);
+
+                // get the json data and put in an array
+                $result = json_decode(curl_exec($ch), true);
+                // free resources
+                curl_close($ch);
+
+                // sha1sum of the latest commit on branch master on github.com
+                $latest_version = $result['object']['sha'];
+                // get curent version from local system
+                $current_version = exec("git log -1 --format='%H'");
+                /*
+                echo "latest : ".$latest_version."<br />";
+                echo "current : ".$current_version;
+                 */
+                // do the check and display message if both versions differ
+                // we check also the size of latest version, or we get the message if it couldn't connect
+                if(strlen($latest_version) == 40 && $latest_version != $current_version) {
+                    echo "<a class='errors' href='https://github.com/NicolasCARPi/elabftw#updating'>Update available !</a> ";
+                }
             }
-            // options to verify the github certificate
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-            curl_setopt($ch, CURLOPT_CAPATH, $ini_arr['path']."ca_github.com.pem");
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-
-            // set a timeout of 500 millisecond
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 500);
-
-            // get the json data and put in an array
-            $result = json_decode(curl_exec($ch), true);
-            // free resources
-            curl_close($ch);
-
-            // sha1sum of the latest commit on branch master on github.com
-            $latest_version = $result['object']['sha'];
-            // get curent version from local system
-            $current_version = exec("git log -1 --format='%H'");
-            /*
-            echo "latest : ".$latest_version."<br />";
-            echo "current : ".$current_version;
-             */
-            // do the check and display message if both versions differ
-            // we check also the size of latest version, or we get the message if it couldn't connect
-            if(strlen($latest_version) == 40 && $latest_version != $current_version) {
-                echo "<a class='errors' href='https://github.com/NicolasCARPi/elabftw#updating'>Update available !</a> ";
-            }
+        } else { //curl extension not loaded
+                echo "<a class='errors' href='https://github.com/NicolasCARPi/elabftw/wiki/Troubleshooting#manual-intervention-needed'>You need to install curl extension for php. Click this message.</a>";
         }
+
     } // end if git command exists
 
     // show admin panel link

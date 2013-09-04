30178134d3aee14a1da5be52020a45faae54ba29
Nicolas CARPi <nicolas.carpi@gmail.com>
2013-09-04 17:46:12 +0200

    fix install/index.php for new config file

--- install/index.php
+++ install/index.php
@@ -63,16 +63,16 @@ $fail = "<span style='color:red'>FAIL</span>";
 echo "<section class='item'>";
 echo "<center><img src='../img/logo.png' alt='elabftw' title='elabftw' /></center>";
 echo "<h2>Welcome the the install of eLabFTW</h2>";
-// INI
-echo "[°] Check for admin/config.ini file...";
-if(file_exists('../admin/config.ini')) {
-    $ini_arr = parse_ini_file('../admin/config.ini');
-    if ($ini_arr['lab_name'] == 'YOURLABNAME') {
-        die($fail." : Please edit admin/config.ini");
+// Config file
+echo "[°] Check for admin/config.php file...";
+if(file_exists('../admin/config.php')) {
+    require_once('../admin/config.php');
+    if (LAB_NAME == 'YOURLABNAME') {
+        die($fail." : Please edit admin/config.php");
     }
     echo $ok;
 } else {
-        die($fail." : Please copy admin/config-example.ini to admin/config.ini and edit it.");
+        die($fail." : Please copy admin/config.php-EXAMPLE to admin/config.php and edit it.");
 }
 
 
@@ -119,12 +119,12 @@ echo "<br />";
 echo "[°] Connection to database...";
 try
 {
-$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-$bdd = new PDO('mysql:host='.$ini_arr['db_host'].';dbname='.$ini_arr['db_name'], $ini_arr['db_user'], $ini_arr['db_password'], $pdo_options);
+    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+    $bdd = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD, $pdo_options);
 }
 catch(Exception $e)
 {
-    die($fail." : Could not connect to the database. ERROR : ".$e);
+    die('Error : '.$e->getMessage());
 }
 // check if user imported the structure
 $sql = "SHOW TABLES LIKE 'users'";
@@ -142,15 +142,10 @@ if ($res) {
 // CHECK PATH
 echo "<br />";
 echo "[°] Checking the path...";
-// first check if the user uncommented a path setting
-if (!isset($ini_arr['path'])) {
-    die($fail." : Path is not set. Please uncomment the corresponding line in admin/config.ini.");
-}
-
 // remove /install/index.php from the path
 $should_be_path = substr(realpath(__FILE__), 0, -18);
-if($ini_arr['path'] != $should_be_path) {
-    die($fail." : Path is not the same ! Change its value in admin/config.ini to <strong>".$should_be_path."</strong>");
+if(PATH != $should_be_path) {
+    die($fail." : Path is not the same ! Change its value in admin/config.php to <strong>".$should_be_path."</strong>");
 } else {
     echo $ok;
 }

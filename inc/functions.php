cadb61f54cd71959162b69355c0a5b094f086a2b
Nicolas CARPi <nicolas.carpi@gmail.com>
2014-03-04 16:03:30 +0100

    add div to whitelist

--- inc/functions.php
+++ inc/functions.php
@@ -470,7 +470,7 @@ function check_body($input)
     // Check BODY (sanitize only);
     if ((isset($input)) && (!empty($input))) {
         // we white list the allowed html tags
-        return strip_tags($input, "<br><br /><p><sub><img><sup><strong><b><em><u><a><s><font><span><ul><li><ol><blockquote><h1><h2><h3><h4><h5><h6><hr><table><tr><td>");
+        return strip_tags($input, "<div><br><br /><p><sub><img><sup><strong><b><em><u><a><s><font><span><ul><li><ol><blockquote><h1><h2><h3><h4><h5><h6><hr><table><tr><td>");
     } else {
         return '';
     }

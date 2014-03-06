a0b9f46750741db03537cafacfdcdc7920f19b9c
Nicolas CARPi <nicolas.carpi@gmail.com>
2014-03-06 17:56:23 +0100

    remove errors on install.php and remove httpd-ssl.conf

--- install/install.php
+++ install/install.php
@@ -24,6 +24,9 @@
 *                                                                               *
 ********************************************************************************/
 /* install/install.php to get an installation up and running */
+// we disable errors to avoid having notice and warning polluting our file
+error_reporting(E_ERROR);
+
 if (!isset($_SESSION)) {
     session_start();
 }
@@ -98,7 +101,6 @@ define('DB_PASSWORD', '".$db_password."');
 
 // we try to write content to file and propose the file for download if we can't write to it
 
-error_reporting(E_ERROR);
 if (file_put_contents($config_file, $config)) {
     $infos_arr = array();
     $infos_arr[] = 'Congratulations, you successfully installed eLabFTW, 

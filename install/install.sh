26af83baccaf5be81a3dfc3e8176e1d322a87b2d
NicolasCARPi <nicolas.carpi@curie.fr>
2015-02-23 20:58:56 +0100

    add proper header

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
-# little script that tries to figure out what PID/GID the webserver is running.
-# 20150209 v0.3 Roger Koot <rkoot at umcutrecht dot nl>
-# modified by Nicolas CARPi for elabftw.
+#/********************************************************************************
+#*                                                                               *
+#*   Copyright 2012 Nicolas CARPi (nicolas.carpi@gmail.com)                      *
+#*   http://www.elabftw.net/                                                     *
+#*                                                                               *
+#********************************************************************************/
+#
+#/********************************************************************************
+#*  This file is part of eLabFTW.                                                *
+#*                                                                               *
+#*    eLabFTW is free software: you can redistribute it and/or modify            *
+#*    it under the terms of the GNU Affero General Public License as             *
+#*    published by the Free Software Foundation, either version 3 of             *
+#*    the License, or (at your option) any later version.                        *
+#*                                                                               *
+#*    eLabFTW is distributed in the hope that it will be useful,                 *
+#*    but WITHOUT ANY WARRANTY; without even the implied                         *
+#*    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR                    *
+#*    PURPOSE.  See the GNU Affero General Public License for more details.      *
+#*                                                                               *
+#*    You should have received a copy of the GNU Affero General Public           *
+#*    License along with eLabFTW.  If not, see <http://www.gnu.org/licenses/>.   *
+#*                                                                               *
+#********************************************************************************/
 
 # show header because it's cool
-
 echo "       _       _      __ _            "
 echo "      | |     | |    / _| |           "
 echo "   ___| | __ _| |__ | |_| |___      __"

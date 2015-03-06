d2ac2fb20098ea1f71d7d73b11740780f0157c5f
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-03-06 16:01:42 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- inc/functions.php
+++ inc/functions.php
@@ -1105,11 +1105,11 @@ function rm_field($table, $field, $added)
  */
 function cURLcheckBasicFunctions()
 {
-  if( !function_exists("curl_init") &&
+    if( !function_exists("curl_init") &&
       !function_exists("curl_setopt") &&
       !function_exists("curl_exec") &&
       !function_exists("curl_close") ) return false;
-  else return true;
+    else return true;
 }
 
 /*
@@ -1120,27 +1120,27 @@ function cURLcheckBasicFunctions()
  */
 function cURLdownload($url, $file)
 {
-  if( !cURLcheckBasicFunctions() ) return "UNAVAILABLE: cURL Basic Functions";
-  $ch = curl_init();
-  if($ch)
-  {
+    if( !cURLcheckBasicFunctions() ) return "UNAVAILABLE: cURL Basic Functions";
+    $ch = curl_init();
+    if($ch)
+    {
     $fp = fopen($file, "w");
     if($fp)
     {
-      if( !curl_setopt($ch, CURLOPT_URL, $url) )
-      {
+        if( !curl_setopt($ch, CURLOPT_URL, $url) )
+        {
         fclose($fp); // to match fopen()
         curl_close($ch); // to match curl_init()
         return false;
-      }
-      if( !curl_setopt($ch, CURLOPT_FILE, $fp) ) return "FAIL: curl_setopt(CURLOPT_FILE)";
-      if( !curl_setopt($ch, CURLOPT_HEADER, 0) ) return "FAIL: curl_setopt(CURLOPT_HEADER)";
-      if( !curl_exec($ch) ) return "FAIL: curl_exec()";
-      curl_close($ch);
-      fclose($fp);
-      return true;
+        }
+        if( !curl_setopt($ch, CURLOPT_FILE, $fp) ) return "FAIL: curl_setopt(CURLOPT_FILE)";
+        if( !curl_setopt($ch, CURLOPT_HEADER, 0) ) return "FAIL: curl_setopt(CURLOPT_HEADER)";
+        if( !curl_exec($ch) ) return "FAIL: curl_exec()";
+        curl_close($ch);
+        fclose($fp);
+        return true;
     }
     else return false;
-  }
-  else return false;
+    }
+    else return false;
 }

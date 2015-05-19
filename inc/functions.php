72ef0bf6a975a32c8c24d1f24265a665804b9d75
NicolasCARPi <nicolas.carpi@curie.fr>
2015-05-19 13:29:52 +0200

    fix bug where item is not properly duplicated

--- inc/functions.php
+++ inc/functions.php
@@ -708,16 +708,18 @@ function duplicate_item($id, $type)
         // go pick what is the default status upon creating experiment
         // there should be only one because upon making a status default,
         // all the others are made not default
-        $sql = 'SELECT id FROM status WHERE is_default = true LIMIT 1';
+        $sql = 'SELECT id FROM status WHERE is_default = true AND team = :team LIMIT 1';
         $req = $pdo->prepare($sql);
+        $req->bindParam(':team', $_SESSION['team_id']);
         $req->execute();
         $status = $req->fetchColumn();
 
         // if there is no is_default status
         // we take the first status that come
         if (!$status) {
-            $sql = 'SELECT id FROM status LIMIT 1';
+            $sql = 'SELECT id FROM status WHERE team = :team LIMIT 1';
             $req = $pdo->prepare($sql);
+            $req->bindParam(':team', $_SESSION['team_id']);
             $req->execute();
             $status = $req->fetchColumn();
         }

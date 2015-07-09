c2eb46d491370d79fed264b0fb5a098cdd206a6d
NicolasCARPi <nicolas.carpi@curie.fr>
2015-07-09 13:07:31 +0200

    center mol editor, remove extraneous js loads

--- inc/editDB.php
+++ inc/editDB.php
@@ -136,12 +136,11 @@ if ($_SESSION['prefs']['chem_editor']) {
     ?>
         <div class='box chemdoodle'>
             <h3><?php echo _('Molecule drawer'); ?></h3>
-            <link rel="stylesheet" href="css/chemdoodle.css" type="text/css">
-            <script src="js/chemdoodle.js"></script>
-            <script src="js/chemdoodle-uis.js"></script>
-            <script>
-                var sketcher = new ChemDoodle.SketcherCanvas('sketcher', 550, 300, {oneMolecule:true});
-            </script>
+            <div class='center'>
+                <script>
+                    var sketcher = new ChemDoodle.SketcherCanvas('sketcher', 550, 300, {oneMolecule:true});
+                </script>
+            </div>
     </div>
     <?php
 }

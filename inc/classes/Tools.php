125b0e3105f5c8dbeb70fcb26070495d41fe8250
NicolasCARPi <nicolas.carpi@curie.fr>
2015-07-26 23:28:13 +0200

    improve logic in function, change var name, remove unused pdo

--- inc/classes/Tools.php
+++ inc/classes/Tools.php
@@ -25,89 +25,65 @@
 ********************************************************************************/
 namespace Elabftw\Elabftw;
 
-use \Elabftw\Elabftw\Db;
-
 class Tools
 {
-    private $pdo;
-
-    public function __construct()
-    {
-        $db = new \Elabftw\Elabftw\Db();
-        $this->pdo = $db->connect();
-    }
-
     /**
      * Converts the php.ini upload size setting to a numeric value in MB
      * Returns 2 if no value is found (using the default setting that was in there previously)
+     * It also checks for the post_max_size value and return the lowest value
      * @return int maximum size in MB of files allowed for upload
     */
     public function returnMaxUploadSize()
     {
         $max_size = trim(ini_get('upload_max_filesize'));
-
-        if (!isset($max_size)) {
-            return 2;
-        }
-
-        $unit = strtolower($max_size[strlen($max_size) - 1]);
-
-        // convert to Mb
-        switch ($unit) {
-            case 'g':
-                $max_size *= 1000;
-                break;
-            case 'k':
-                $max_size /= 1024;
-                break;
-        }
-
-        // check that post_max_size is greater than upload_max_filesize
-        // if not, use this value
         $post_max_size = trim(ini_get('post_max_size'));
 
-        if (!isset($post_max_size)) {
+        if (empty($max_size) || empty($post_max_size)) {
             return 2;
         }
 
-        $unit = strtolower($post_max_size[strlen($post_max_size) - 1]);
+        // assume they both have same unit to compare the values
+        if (intval($post_max_size) > intval($max_size)) {
+            $input = $max_size;
+        } else {
+            $input = $post_max_size;
+        }
+
+        // get unit
+        $unit = strtolower($input[strlen($input) - 1]);
 
         // convert to Mb
         switch ($unit) {
             case 'g':
-                $post_max_size *= 1000;
+                $input *= 1000;
                 break;
             case 'k':
-                $post_max_size /= 1024;
+                $input /= 1024;
                 break;
         }
 
-        if (intval($post_max_size) > intval($max_size)) {
-            return intval($max_size);
-        } else {
-            return intval($post_max_size);
-        }
+        return intval($input);
     }
 
     /**
      * Show the units in human format from bytes.
      *
-     * @param int $a_bytes size in bytes
+     * @param int $bytes size in bytes
      * @return string
      */
-    public function formatBytes($a_bytes)
+    public function formatBytes($bytes)
     {
         // nice display of filesize
-        if ($a_bytes < 1024) {
-            return $a_bytes . ' B';
-        } elseif ($a_bytes < 1048576) {
-            return round($a_bytes / 1024, 2) . ' KiB';
-        } elseif ($a_bytes < 1073741824) {
-            return round($a_bytes / 1048576, 2) . ' MiB';
-        } elseif ($a_bytes < 1099511627776) {
-            return round($a_bytes / 1073741824, 2) . ' GiB';
-        } elseif ($a_bytes < 1125899906842624) {
-            return round($a_bytes / 1099511627776, 2) . ' TiB';
+        if ($bytes < 1024) {
+            return $bytes . ' B';
+        } elseif ($bytes < 1048576) {
+            return round($bytes / 1024, 2) . ' KiB';
+        } elseif ($bytes < 1073741824) {
+            return round($bytes / 1048576, 2) . ' MiB';
+        } elseif ($bytes < 1099511627776) {
+            return round($bytes / 1073741824, 2) . ' GiB';
+        } elseif ($bytes < 1125899906842624) {
+            return round($bytes / 1099511627776, 2) . ' TiB';
         } else {
             return 'That is a very big file you have there my friend.';
         }

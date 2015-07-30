9c6b63167e6809f693a05ddbcc67c685fe5fcf69
NicolasCARPi <nicolas.carpi@curie.fr>
2015-07-30 22:06:42 +0200

    add 2 new classes : Import (abstract) and ImportCsv

--- app/import.php
+++ app/import.php
@@ -27,117 +27,30 @@ require_once '../inc/common.php';
 // it might take some time and we don't want to be cut in the middle, so set time_limit to ∞
 set_time_limit(0);
 
-$inserted = 0;
 $errflag = false;
 $msg_arr = array();
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    die();
-}
+try {
+    switch ($_POST['type']) {
+        case 'csv':
+            $import = new \Elabftw\Elabftw\ImportCsv();
+            break;
 
-// look at mime type. not a trusted source, but it can prevent dumb errors
-$mimes = array(null, 'application/vnd.ms-excel', 'text/plain', 'text/csv', 'text/tsv', 'application/zip', 'application/force-download');
-if (!in_array($_FILES['file']['type'], $mimes)) {
-    $errflag = true;
-    $msg_arr[] = sprintf(_("This doesn't look like a .%s file. Import aborted."), $type);
-}
-
-// try to read the file
-if (!is_readable($_FILES['file']['tmp_name'])) {
-    $errflag = true;
-    $msg_arr[] = _("Could not open the file.");
-}
-
-// get what type we want
-if (isset($_COOKIE['itemType']) && is_pos_int($_COOKIE['itemType'])) {
-    $itemType = $_COOKIE['itemType'];
-} else {
-    $errflag = true;
-    $msg_arr[] = _("No cookies found. Import aborted.");
-}
-
-// redirect user on error
-if ($errflag) {
-    $_SESSION['errors'] = $msg_arr;
-    header('Location: ../admin.php');
-    exit;
-}
-
-switch ($_POST['type']) {
-    case 'csv':
-        // CODE TO IMPORT CSV
-        $row = 0;
-        $column = array();
-
-        // open file
-        $handle = fopen($_FILES['file']['tmp_name'], 'r');
-        if ($handle === false) {
+        case 'zip':
+            $import = new \Elabftw\Elabftw\ImportZip();
+            break;
+        default:
             $errflag = true;
-        }
-
-        // loop the lines
-        while ($data = fgetcsv($handle, 0, ",")) {
-            $num = count($data);
-            // get the column names (first line)
-            if ($row == 0) {
-                for ($i = 0; $i < $num; $i++) {
-                    $column[] = $data[$i];
-                }
-                $row++;
-                continue;
-            }
-            $row++;
-
-            $title = $data[0];
-            $body = '';
-            $j = 0;
-            foreach ($data as $line) {
-                $body .= "<p><strong>" . $column[$j] . " :</strong> " . $line . '</p>';
-                $j++;
-            }
-            // clean the body
-            $body = str_replace('<p><strong> :</strong> </p>', '', $body);
-
-            // SQL for importing
-            $sql = "INSERT INTO items(team, title, date, body, userid, type) VALUES(:team, :title, :date, :body, :userid, :type)";
-            $req = $pdo->prepare($sql);
-            $result = $req->execute(array(
-                'team' => $_SESSION['team_id'],
-                'title' => $title,
-                'date' => kdate(),
-                'body' => $body,
-                'userid' => $_SESSION['userid'],
-                'type' => $itemType
-            ));
-            if ($result) {
-                $inserted++;
-            } else {
-                $errflag = true;
-            }
-        }
-        fclose($handle);
-        // END CODE TO IMPORT CSV
-        break;
-
-    case 'zip':
-        // CODE TO IMPORT ZIP
-        try {
-            $import = new \Elabftw\Elabftw\ImportZip($_FILES['file']['tmp_name'], $itemType);
-        } catch (Exception $e) {
-            $errflag = true;
-            $msg_arr[] = $e->getMessage();
-        }
-        $inserted = $import->inserted;
-        // END CODE TO IMPORT ZIP
-        break;
-    default:
-        $errflag = true;
+    }
+} catch (Exception $e) {
+    $errflag = true;
+    $msg_arr[] = $e->getMessage();
 }
 
 
 // REDIRECT
 if (!$errflag) {
-    $msg_arr[] = $inserted . ' ' . ngettext('item imported successfully.', 'items imported successfully.', $inserted);
+    $msg_arr[] = $import->inserted . ' ' . ngettext('item imported successfully.', 'items imported successfully.', $import->inserted);
     $_SESSION['infos'] = $msg_arr;
     header('Location: ../database.php');
 } else {

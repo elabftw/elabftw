4198003cc66ead083ee7197ec117adef60cfc60a
NicolasCARPi <nicolas.carpi@curie.fr>
2016-04-03 19:12:39 +0200

    better install/test.php

--- install/test.php
+++ install/test.php
@@ -1,44 +1,36 @@
 <?php
-/********************************************************************************
-*                                                                               *
-*   Copyright 2012 Nicolas CARPi (nicolas.carpi@gmail.com)                      *
-*   http://www.elabftw.net/                                                     *
-*                                                                               *
-********************************************************************************/
+/**
+ * index.php
+ *
+ * @author Nicolas CARPi <nicolas.carpi@curie.fr>
+ * @copyright 2012 Nicolas CARPi
+ * @see http://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
 
-/********************************************************************************
-*  This file is part of eLabFTW.                                                *
-*                                                                               *
-*    eLabFTW is free software: you can redistribute it and/or modify            *
-*    it under the terms of the GNU Affero General Public License as             *
-*    published by the Free Software Foundation, either version 3 of             *
-*    the License, or (at your option) any later version.                        *
-*                                                                               *
-*    eLabFTW is distributed in the hope that it will be useful,                 *
-*    but WITHOUT ANY WARRANTY; without even the implied                         *
-*    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR                    *
-*    PURPOSE.  See the GNU Affero General Public License for more details.      *
-*                                                                               *
-*    You should have received a copy of the GNU Affero General Public           *
-*    License along with eLabFTW.  If not, see <http://www.gnu.org/licenses/>.   *
-*                                                                               *
-********************************************************************************/
-/* install/testSQL.php to test if the SQL/email parameters are good */
+/**
+ * Check if we can connect to database
+ */
+namespace Elabftw\Elabftw;
 
-// Check if there is already a config file, die if yes.
-if (file_exists('../config.php')) {
-    die('Remove config file.');
-}
+use Exception;
+use PDO;
 
-// MYSQL
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['mysql'])) {
-    try {
+try {
+    // Check if there is already a config file
+    if (file_exists('../config.php')) {
+        throw new Exception('Remove config file.');
+    }
+
+    // MYSQL
+    if (isset($_POST['mysql'])) {
         $pdo_options = array();
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         $pdo = new PDO('mysql:host=' . $_POST['db_host'] . ';dbname=' . $_POST['db_name'], $_POST['db_user'], $_POST['db_password'], $pdo_options);
-    } catch (Exception $e) {
-        echo $e->getMessage();
-        exit();
+        echo 1;
     }
-    echo 1;
+
+} catch (Exception $e) {
+    echo $e->getMessage();
 }

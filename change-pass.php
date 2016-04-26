5e365a1f504c7f71588dc2490506b23561155851
NicolasCARPi <nicolas.carpi@curie.fr>
2016-04-26 15:36:49 +0200

    better change-pass.php

--- change-pass.php
+++ change-pass.php
@@ -19,9 +19,11 @@ use Exception;
 require_once 'inc/common.php';
 $page_title = _('Reset password');
 $selected_menu = null;
+require_once 'inc/head.php';
+
+$Auth = new Auth();
 
 try {
-    require_once 'inc/head.php';
 
     // get the unique key
     if (!isset($_GET['key']) ||
@@ -30,85 +32,82 @@ try {
 
         throw new Exception('Bad parameters in url.');
     }
-
-    $key = filter_var($_GET['key'], FILTER_SANITIZE_STRING);
-    $userid = filter_var($_GET['userid'], FILTER_VALIDATE_INT);
     ?>
 
     <section class='center'>
         <form method="post" class='loginform' action="app/reset.php">
             <p>
                 <!-- output the key and userid as hidden fields -->
-                <input type="hidden" name="key" value="<?php echo $key; ?>" />
-                <input type="hidden" name="userid" value="<?php echo $userid; ?>" />
+                <input type="hidden" name="key" value="<?= filter_var($_GET['key'], FILTER_SANITIZE_STRING) ?>" />
+                <input type="hidden" name="userid" value="<?= $_GET['userid'] ?>" />
 
-                <label class='block' for='passwordtxt'><?php echo _('New password'); ?></label>
-                <input name="password" type="password" title='<?php echo _('8 characters minimum'); ?>' id="password" pattern=".{8,}" required />
-                <label class='block' for='cpasswordtxt'><?php echo _('Type it again'); ?></label>
-                <input name="cpassword" type="password" title='<?php echo _('8 characters minimum'); ?>' id="cpassword" pattern=".{8,}" required />
-                <label class='block' for='complexity'><?php echo _('Complexity'); ?></label>
+                <label class='block' for='passwordtxt'><?= _('New password') ?></label>
+                <span class='smallgray'><?= $Auth::MIN_PASSWORD_LENGTH . " " . _('characters minimum') ?></span></label>
+                <input id='password' type='password' pattern='.{0}|.{<?= $Auth::MIN_PASSWORD_LENGTH ?>,}' value='' name='password' required />
+
+                <label class='block' for='cpasswordtxt'><?= _('Type it again') ?></label>
+                <input id='cpassword' type='password' pattern='.{0}|.{<?= $Auth::MIN_PASSWORD_LENGTH ?>,}' value='' name='cpassword' required />
+                <label class='block' for='complexity'><?= _('Complexity') ?></label>
                 <input id='complexity' disabled />
-                <div id="checkPasswordMatchDiv"><p><?php echo _('The passwords do not match!'); ?></p></div>
-
+                <div id="checkPasswordMatchDiv"><p><?= _('The passwords do not match!') ?></p></div>
             </p>
         </form>
     </section>
 
     <script>
-    // we check for password match here
     function checkPasswordMatch() {
-        var password = $("#password").val();
-        var confirmPassword = $("#cpassword").val();
+        password = $("#password").val();
+        confirmPassword = $("#cpassword").val();
 
         if (password != confirmPassword)
-            $("#checkPasswordMatchDiv").html("<p><?php echo _('The passwords do not match!'); ?></p>");
+            $("#checkPasswordMatchDiv").html("<p><?= _('The passwords do not match!') ?></p>");
         else
-            $("#checkPasswordMatchDiv").html("<button class='button' type='submit' name='Submit'><?php echo _('Save new password'); ?></button>");
+            $("#checkPasswordMatchDiv").html("<button class='button' type='submit' name='Submit'><?= _('Save new password') ?></button>");
     }
 
     $(document).ready(function () {
+        // check if passwords match
        $("#cpassword").keyup(checkPasswordMatch);
-        // give focus to the first field on page load
+        // give focus to the first input field
         document.getElementById("password").focus();
         // password complexity
         $("#password").complexify({}, function (valid, complexity){
             if (complexity < 20) {
                 $('#complexity').css({'background-color':'red'});
                 $('#complexity').css({'color':'white'});
-                $('#complexity').val('<?php echo _('Weak password'); ?>');
+                $('#complexity').val('<?= _('Weak password') ?>');
                 $('#complexity').css({'border-color' : '#e3e3e3'});
                 $('#complexity').css({'box-shadow': '0 0  yellow'});
                 $('#complexity').css({'-moz-box-shadow': '0 0 yellow'});
             } else if (complexity < 30) {
                 $('#complexity').css({'color':'#white'});
                 $('#complexity').css({'background-color':'orange'});
-                $('#complexity').val('<?php echo _('Average password'); ?>');
+                $('#complexity').val('<?= _('Average password') ?>');
                 $('#complexity').css({'box-shadow': '0 0  yellow'});
                 $('#complexity').css({'border-color' : '#e3e3e3'});
                 $('#complexity').css({'-moz-box-shadow': '0 0 yellow'});
             } else if (complexity < 50) {
                 $('#complexity').css({'color':'white'});
-                $('#complexity').val('<?php echo _('Good password'); ?>');
+                $('#complexity').val('<?= _('Good password') ?>');
                 $('#complexity').css({'background-color':'green'});
                 $('#complexity').css({'box-shadow': '0 0  yellow'});
                 $('#complexity').css({'-moz-box-shadow': '0 0 yellow'});
                 $('#complexity').css({'border-color' : '#e3e3e3'});
             } else if (complexity < 99) {
                 $('#complexity').css({'color':'black'});
-                $('#complexity').val('<?php echo _('Strong password'); ?>');
+                $('#complexity').val('<?= _('Strong password') ?>');
                 $('#complexity').css({'background-color':'#ffd700'});
                 $('#complexity').css({'box-shadow': '0px 0px 15px 5px #ffd700'});
                 $('#complexity').css({'border' : 'none'});
                 $('#complexity').css({'-moz-box-shadow': '0px 0px 15px 5px #ffd700'});
             } else {
                 $('#complexity').css({'color':'#797979'});
-                $('#complexity').val('<?php echo _('No way that is your real password!'); ?>');
+                $('#complexity').val('<?= _('No way that is your real password!') ?>');
                 $('#complexity').css({'background-color':'#e3e3e3'});
                 $('#complexity').css({'box-shadow': '0 0  yellow'});
                 $('#complexity').css({'-moz-box-shadow': '0 0 yellow'});
                 $('#complexity').css({'border-color' : '#e3e3e3'});
             }
-            //$("#complexity").html(Math.round(complexity) + '%');
         });
     });
     </script>

c2fb5cc935e6416095fce6f0e243e3c0fb3a7a71
Nicolas CARPi <nicolas.carpi@curie.fr>
2016-07-28 22:59:14 +0200

    try circleci.yml with database

--- tests/config.php
+++ tests/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'phpunit');
-define('DB_USER', 'phpunit');
-define('DB_PASSWORD', 'phpunit');
+define('DB_NAME', 'circle_test');
+define('DB_USER', 'ubuntu');
+define('DB_PASSWORD', '');
 define('ELAB_ROOT', substr(realpath(__FILE__), 0, -16));
 define('SECRET_KEY', 'def00000f236867588a2b27e58196fc45c7d740b2a54685a63adc5820f23a8b08f0992749a910788421870ec4c638592a32fa929d7d6271a30d890b8055b35948edd43e1');

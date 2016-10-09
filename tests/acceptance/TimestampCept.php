482794726307833c1c303e1a27ea323cda6df40a
Nicolas CARPi <nicolas.carpi@curie.fr>
2016-10-09 22:06:11 +0200

    fix timestamp test with better css selector

--- tests/acceptance/TimestampCept.php
+++ tests/acceptance/TimestampCept.php
@@ -4,6 +4,6 @@ $I->wantTo('Timestamp an experiment');
 testLogin($I);
 $I->amOnPage('experiments.php?mode=view&id=1');
 $I->click('a.elab-tooltip:nth-child(8) > img:nth-child(2)');
-$I->click('.ui-dialog-buttonset > button:nth-child(1)');
+$I->click('body > div.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.ui-dialog-buttons.ui-draggable > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button:nth-child(1)');
 $I->wait(2);
 $I->see('Experiment was timestamped');

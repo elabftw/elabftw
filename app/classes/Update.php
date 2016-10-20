29acc68abbb745181a8207d4abea265ddc436049
Nicolas CARPi <nicolas.carpi@curie.fr>
2016-10-20 13:01:37 +0200

    bump to 1.3.1

--- app/classes/Update.php
+++ app/classes/Update.php
@@ -51,7 +51,7 @@ class Update
      * AND package.json
      * ///////////////////////////
      */
-    const INSTALLED_VERSION = '1.3.0';
+    const INSTALLED_VERSION = '1.3.1';
 
     /**
      * /////////////////////////////////////////////////////

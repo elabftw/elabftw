f64cc124258e92c4c5ced00e795cceb0873ac63d
Nicolas CARPi <nicolas.carpi@curie.fr>
2016-10-26 16:52:22 +0200

    add #autocomplete feature to help menu

--- app/head.inc.php
+++ app/head.inc.php
@@ -110,6 +110,7 @@ if (isset($_SESSION['auth'])) {
         <li class='tip'><?= sprintf(_('There is a manual available %shere%s.'), "<a href='https://elabftw.readthedocs.io/en/stable/manual.html'>", "</a>") ?></li>
         <li class='tip'><?= _("You can use a TODOlist by pressing 't'.") ?></li>
         <li class='tip'><?= sprintf(_('You can have experiments templates (%sControl Panel%s).'), "<a href='ucp.php?tab=3'>", "</a>") ?></li>
+        <li class='tip'><?= _('You can make links inside the text by typing # and an autocomplete menu will spawn with a list of Experiments/Items.') ?></li>
         <li class='tip'><?= sprintf(_('The admin of a team can edit the status and the types of items available (%sAdmin Panel%s).'), "<a href='admin.php?tab=4'>", "</a>") ?></li>
         <li class='tip'><?= _('If you press Ctrl Shift D in the editor, the date will appear under the cursor.') ?></li>
         <li class='tip'><?= sprintf(_('Custom shortcuts are available (%sControl Panel%s).'), "<a href='ucp.php?tab=1'>", "</a>") ?></li>

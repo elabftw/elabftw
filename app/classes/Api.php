435364c039e5c2a96b0c2c786195de1a9d07aee3
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-01-20 22:27:21 +0100

    remove json_encode

--- app/classes/Api.php
+++ app/classes/Api.php
@@ -123,7 +123,7 @@ class Api
         }
 
         if ($this->Entity->update($_POST['title'], $_POST['date'], $_POST['body'])) {
-            return json_encode(array('result', 'success'));
+            return array('result', 'success');
         }
 
         return array('Result', Tools::error());

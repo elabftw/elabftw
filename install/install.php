a3e019bfbe262530961ce575d2fca8e5833631c2
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-01-25 00:20:06 +0100

    remove some die

--- install/install.php
+++ install/install.php
@@ -29,98 +29,94 @@ use Exception;
 use PDO;
 use \Defuse\Crypto\Key as Key;
 
-/* install/index.php to get an installation up and running */
-session_start();
-require_once '../vendor/autoload.php';
-require_once '../app/functions.inc.php';
-
-/* install/install.php to get an installation up and running */
-/* this script will :
- * 1. read the sql infos given through POST
- * 2. import the SQL structure (+ default values)
- * 3. write the config file (or propose to download it)
- */
-
-// we disable errors to avoid having notice and warning polluting our file
-error_reporting(E_ERROR);
-
-// Check if there is already a config file, redirect to index if yes.
-if (file_exists('../config.php')) {
-    header('Location: ../install/index.php');
-    exit;
-}
-
-// POST data
-if (isset($_POST['db_host']) && !empty($_POST['db_host'])) {
-    $db_host = $_POST['db_host'];
-} else {
-    die('Bad POST data');
-}
-
-if (isset($_POST['db_name']) && !empty($_POST['db_name'])) {
-    $db_name = $_POST['db_name'];
-} else {
-    die('Bad POST data');
-}
-
-if (isset($_POST['db_user']) && !empty($_POST['db_user'])) {
-    $db_user = $_POST['db_user'];
-} else {
-    die('Bad POST data');
-}
-
-// the db pass can be empty on mac and windows install
-if (isset($_POST['db_password']) && !empty($_POST['db_password'])) {
-    $db_password = $_POST['db_password'];
-}
-// connect to DB
 try {
+    /* install/index.php to get an installation up and running */
+    session_start();
+    require_once '../vendor/autoload.php';
+    require_once '../app/functions.inc.php';
+
+    /* install/install.php to get an installation up and running */
+    /* this script will :
+     * 1. read the sql infos given through POST
+     * 2. import the SQL structure (+ default values)
+     * 3. write the config file (or propose to download it)
+     */
+
+    // we disable errors to avoid having notice and warning polluting our file
+    error_reporting(E_ERROR);
+
+    // Check if there is already a config file, redirect to index if yes.
+    if (file_exists('../config.php')) {
+        header('Location: ../install/index.php');
+        exit;
+    }
+
+    // POST data
+    if (isset($_POST['db_host']) && !empty($_POST['db_host'])) {
+        $db_host = $_POST['db_host'];
+    } else {
+        throw new Exception('Bad POST data');
+    }
+
+    if (isset($_POST['db_name']) && !empty($_POST['db_name'])) {
+        $db_name = $_POST['db_name'];
+    } else {
+        throw new Exception('Bad POST data');
+    }
+
+    if (isset($_POST['db_user']) && !empty($_POST['db_user'])) {
+        $db_user = $_POST['db_user'];
+    } else {
+        throw new Exception('Bad POST data');
+    }
+
+    // the db pass can be empty on mac and windows install
+    if (isset($_POST['db_password']) && !empty($_POST['db_password'])) {
+        $db_password = $_POST['db_password'];
+    }
+    // connect to DB
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
     $pdo = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name, $db_user, $db_password, $pdo_options);
-} catch (Exception $e) {
-    die('Error : ' . $e->getMessage());
-}
 
-// now import the structure
-try {
+    // now import the structure
     import_sql_structure();
-} catch (Exception $e) {
-    die('Error importing the SQL structure: ' . $e->getMessage());
-}
 
-// BUILD CONFIG FILE
+    // BUILD CONFIG FILE
 
-// the new file to write to
-$config_file = '../config.php';
-$elab_root = substr(realpath(__FILE__), 0, -20) . '/';
-// make a new secret key
-$new_key = Key::createNewRandomKey();
+    // the new file to write to
+    $config_file = '../config.php';
+    $elab_root = substr(realpath(__FILE__), 0, -20) . '/';
+    // make a new secret key
+    $new_key = Key::createNewRandomKey();
 
-// what we will write in the file
-$config = "<?php
-define('DB_HOST', '" . $db_host . "');
-define('DB_NAME', '" . $db_name . "');
-define('DB_USER', '" . $db_user . "');
-define('DB_PASSWORD', '" . $db_password . "');
-define('ELAB_ROOT', '" . $elab_root . "');
-define('SECRET_KEY', '" . $new_key->saveToAsciiSafeString() . "');
-";
+    // what we will write in the file
+    $config = "<?php
+    define('DB_HOST', '" . $db_host . "');
+    define('DB_NAME', '" . $db_name . "');
+    define('DB_USER', '" . $db_user . "');
+    define('DB_PASSWORD', '" . $db_password . "');
+    define('ELAB_ROOT', '" . $elab_root . "');
+    define('SECRET_KEY', '" . $new_key->saveToAsciiSafeString() . "');
+    ";
 
-// we try to write content to file and propose the file for download if we can't write to it
-if (file_put_contents($config_file, $config)) {
-    // it's cool, we managed to write the config file
-    // let's put restricting permissions on it as discussed in #129
-    if (is_writable($config_file)) {
-        chmod($config_file, 0400);
+    // we try to write content to file and propose the file for download if we can't write to it
+    if (file_put_contents($config_file, $config)) {
+        // it's cool, we managed to write the config file
+        // let's put restricting permissions on it as discussed in #129
+        if (is_writable($config_file)) {
+            chmod($config_file, 0400);
+        }
+        $infos_arr = array();
+        $infos_arr[] = 'Congratulations, you successfully installed eLabFTW, 
+        now you need to <strong>register</strong> your account (you will have full admin rights).';
+        $_SESSION['ok'] = $infos_arr;
+        header('Location: ../register.php');
+
+    } else {
+        header('Content-Type: text/x-delimtext; name="config.php"');
+        header('Content-disposition: attachment; filename=config.php');
+        echo $config;
     }
-    $infos_arr = array();
-    $infos_arr[] = 'Congratulations, you successfully installed eLabFTW, 
-    now you need to <strong>register</strong> your account (you will have full admin rights).';
-    $_SESSION['ok'] = $infos_arr;
-    header('Location: ../register.php');
-
-} else {
-    header('Content-Type: text/x-delimtext; name="config.php"');
-    header('Content-disposition: attachment; filename=config.php');
-    echo $config;
+} catch (Exception $e) {
+    display_message('ko', 'Error: ' . $e->getMessage());
 }

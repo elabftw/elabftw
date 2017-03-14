5e853917de783751bb183b1e96656fba4ac3b06a
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-03-14 16:41:59 +0100

    issue is 362 not 364

--- app/classes/Tools.php
+++ app/classes/Tools.php
@@ -434,7 +434,7 @@ class Tools
     /**
      * When you want to know the server port used
      * We cannot rely on SERVER_PORT because it'll always be 443 inside Docker
-     * See issue #364
+     * See issue #362
      * If the port is standard (443), it will not appear in HTTP_HOST, but otherwise it'll be there
      * so we can get the custom port from here
      *

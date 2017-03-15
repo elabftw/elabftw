6a5eb37d09d1f9e832c2a52e83bb61198c739a19
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-03-15 15:18:57 +0100

    1.5.4

--- app/classes/Update.php
+++ app/classes/Update.php
@@ -48,7 +48,7 @@ class Update
      * UPDATE IT ALSO IN package.json
      * ///////////////////////////
      */
-    const INSTALLED_VERSION = '1.5.3';
+    const INSTALLED_VERSION = '1.5.4';
 
     /**
      * /////////////////////////////////////////////////////

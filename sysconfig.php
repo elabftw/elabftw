d41f68cf23bd069cfacfc2c6657713931687787d
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-08-24 16:13:34 +0200

    remove all output from sysconfig.php -> all in tpl

--- sysconfig.php
+++ sysconfig.php
@@ -24,48 +24,18 @@ try {
     require_once 'app/head.inc.php';
 
     if ($Session->get('is_sysadmin') != 1) {
-        throw new Exception(_('This section is out of your reach.'));
+        throw new Exception(Tools::error(true));
     }
 
     $Auth = new Auth();
     $Idps = new Idps();
+    $idpsArr = $Idps->readAll();
     $Logs = new Logs();
+    $logsArr = $Logs->readAll();
     $TeamsView = new TeamsView(new Teams());
     $teamsArr = $TeamsView->Teams->readAll();
     $usersArr = $Users->readAll();
     $ReleaseCheck = new ReleaseCheck($Config);
-    if (!$ReleaseCheck->getUpdatesIni()) {
-        $message = 'Error getting latest version information from server! Check the proxy setting.';
-        echo Tools::displayMessage($message, 'ko');
-    }
-
-    // display current and latest version
-    echo "<p>" . _('Installed version:') . " " . $ReleaseCheck::INSTALLED_VERSION . " ";
-    if ($ReleaseCheck->success === true) {
-        // show a little green check if we have latest version
-        if (!$ReleaseCheck->updateIsAvailable()) {
-            echo "<img src='app/img/check.png' width='16px' length='16px' title='latest' style='position:relative;bottom:2px' alt='OK' />";
-        }
-        // display latest version
-        echo "<br>" . _('Latest version:') . " " . $ReleaseCheck->getLatestVersion() . "</p>";
-
-        // if we don't have the latest version, show button redirecting to doc
-        if ($ReleaseCheck->updateIsAvailable()) {
-            $message = $ReleaseCheck->getReleaseDate() . " - " .
-                _('A new version is available!') . " <a href='https://doc.elabftw.net/how-to-update.html'>
-                <button class='button'>Update elabftw</button></a>
-                <a href='" . $ReleaseCheck->getChangelogLink() . "'><button class='button'>Read changelog</button></a>";
-            echo Tools::displayMessage($message, 'warning');
-        }
-    } else {
-        echo "</p>";
-    }
-
-    if ($Config->configArr['mail_from'] === 'notconfigured@example.com') {
-        $message = sprintf(_('Please finalize install : %slink to documentation%s.'), "<a href='https://doc.elabftw.net/postinstall.html#setting-up-email'>", "</a>");
-        echo Tools::displayMessage($message, 'ko');
-    }
-
     $langsArr = Tools::getLangsArr();
 
     switch ($Config->configArr['mail_method']) {
@@ -92,13 +62,10 @@ try {
 
     $phpInfos = array(PHP_OS, PHP_VERSION, PHP_INT_MAX, PHP_SYSCONFDIR);
 
-    $logsArr = $Logs->readAll();
-
-    $idpsArr = $Idps->readAll();
-
     echo $Twig->render('sysconfig.html', array(
         'Auth' => $Auth,
         'Config' => $Config,
+        'ReleaseCheck' => $ReleaseCheck,
         'TeamsView' => $TeamsView,
         'langsArr' => $langsArr,
         'disable_sendmail' => $disable_sendmail,

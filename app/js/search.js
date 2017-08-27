360737fa8ea1dd4cb75b019d305e55a2bad4c12f
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-08-27 18:31:46 +0200

    fix search tag list display

--- app/js/search.js
+++ app/js/search.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     // DATEPICKER
     $( ".datepicker" ).datepicker({dateFormat: 'yymmdd'});
-    var getParams = getGetParameters();
-    if (getParams.type === 'experiments') {
+    if ($('#searchin').val() === 'experiments') {
         $("#tag_db").hide();
     } else {
         $("#tag_exp").hide();
     }
 
     // scroll to anchor if there is a search
+    var getParams = getGetParameters();
     if (getParams.type) {
         window.location.hash = "#anchor";
     }

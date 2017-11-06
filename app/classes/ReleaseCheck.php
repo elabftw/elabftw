e9b1c9946f3ec2ea9e71c9b29c0d9232d9aa4a78
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-11-06 16:45:32 +0100

    1.7.7

--- app/classes/ReleaseCheck.php
+++ app/classes/ReleaseCheck.php
@@ -43,7 +43,7 @@ class ReleaseCheck
      * UPDATE IT ALSO IN package.json
      * ///////////////////////////
      */
-    const INSTALLED_VERSION = '1.7.6';
+    const INSTALLED_VERSION = '1.7.7';
 
     /**
      * Fetch the update info on object creation

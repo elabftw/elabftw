7967a8fd310a458dd578e76074c50a4853831a26
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-02-28 02:24:30 +0100

    fix the circleci config file

--- tests/config-circleci.php
+++ tests/config-circleci.php
@@ -3,5 +3,5 @@ define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'phpunit');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('ELAB_ROOT', '/home/ubuntu/elabftw/');
+define('ELAB_ROOT', '/home/circleci/elabftw/elabftw/');
 define('SECRET_KEY', 'def00000f236867588a2b27e58196fc45c7d740b2a54685a63adc5820f23a8b08f0992749a910788421870ec4c638592a32fa929d7d6271a30d890b8055b35948edd43e1');

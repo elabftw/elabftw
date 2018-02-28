248d577383abd315faad64bc042f6d8c25bce688
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-02-28 16:39:20 +0100

    also check for the FAILED string. see #591

--- app/classes/TrustedTimestamps.php
+++ app/classes/TrustedTimestamps.php
@@ -410,7 +410,7 @@ class TrustedTimestamps extends AbstractMake
             if (stripos($retline, "message imprint mismatch") !== false) {
                 return false;
             }
-            if (stripos($retline, "TS_CHECK_SIGNING_CERTS")) {
+            if (stripos($retline, "TS_CHECK_SIGNING_CERTS") || stripos($retline, "FAILED")) {
                 // we are facing the OpenSSL bug discussed here:
                 // https://github.com/elabftw/elabftw/issues/242#issuecomment-212382182
                 return $this->validateWithJava();

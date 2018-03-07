22d2d3841c052c8a347e1d571e848327e869eaad
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-03-07 16:38:50 +0100

    use moments.js for revisions too

---
+++ web/app/js/src/relative-moment.js
@@ -0,0 +1,14 @@
+// i18n for moment.js
+moment.locale($('#entityInfos').data('locale'));
+// DISPLAY COMMENT TIME RELATIVE TO NOW
+function relativeMoment() {
+    $.each($('.relative-moment'), function(i, el) {
+        let commentTime = el.title;
+        let relMom = moment(commentTime, 'YYYY-MM-DD H:m:s').fromNow();
+        el.textContent = relMom;
+    });
+}
+
+$(document).ready(function() {
+    relativeMoment();
+});

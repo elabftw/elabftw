eb376c37bb9307d4366144821e3ddcdb7cc724b0
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-03-07 02:29:34 +0100

    remove comments.png

--- src/views/UploadsView.php
+++ src/views/UploadsView.php
@@ -132,7 +132,7 @@ class UploadsView
         // your are in view mode
 
         if ($mode === 'edit' || ($upload['comment'] != 'Click to add a comment')) {
-            $comment = "<img src='app/img/comment.png' alt='comment' />
+            $comment = "<i class='fas fa-comments'></i>
                         <p class='editable inline' id='filecomment_" . $upload['id'] . "'>" .
             $upload['comment'] . "</p>";
             $html .= $comment;

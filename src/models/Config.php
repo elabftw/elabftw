7c86ed1212e06427ed8fb26efa069190e39e1c22
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-07-16 15:52:07 +0200

    fix notice in sysconfig

--- src/models/Config.php
+++ src/models/Config.php
@@ -104,10 +104,6 @@ class Config
         // encrypt password
         if (isset($post['smtp_password']) && !empty($post['smtp_password'])) {
             $post['smtp_password'] = Crypto::encrypt($post['smtp_password'], Key::loadFromAsciiSafeString(SECRET_KEY));
-            // we might receive a set but empty smtp_password, so ignore it
-            // unless it is null, then it's because we want to clear it
-        } elseif (empty($post['smtp_password']) && $post['smtp_password'] !== null) {
-            unset($post['smtp_password']);
         }
 
         // loop the array and update config

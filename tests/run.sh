475c8aeaf0e6e80cc375ae8aa6563e2c41d0c20d
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-07-17 12:04:18 +0200

    now properly swapping the config files

--- tests/run.sh
+++ tests/run.sh
@@ -8,9 +8,16 @@ set -eu
 # kill chromedriver on exit
 function cleanup {
     killall chromedriver
+    sudo cp config.php.docker config.php
+    sudo chown 100:101 config.php
 }
 trap cleanup EXIT
 
+# swap config file for docker with the one for localhost
+# sudo is needed because config file for docker is owned by 100:101
+sudo cp config.php config.php.docker
+sudo cp tests/config-home.php config.php
+sudo chmod +r config.php
 # start chromedriver
 chromedriver --url-base=/wd/hub &
 # create a fresh phpunit database

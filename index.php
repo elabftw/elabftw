86d30e29626fedce660eab1ee81fbef86e10e2e0
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-08-01 14:41:44 +0200

    add index.php in old root for users not reading upgrade guide

---
+++ index.php
@@ -0,0 +1,5 @@
+<h1>Hello!</h1>
+
+<p>The path of the served files is now web/. You need to edit your webserver config.</p>
+
+<p>Have a look at <a href='https://elabftw.readthedocs.io/en/next/how-to-update.html#complete-upgrade-guide-from-1-8-x-to-2-0-0'>the upgrade guide</a>.</p>

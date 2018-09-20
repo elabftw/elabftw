57afa1ccfd60b65c29d1afe429cb2926f4dda042
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-09-20 15:24:24 +0200

    2.0.3

--- src/classes/ReleaseCheck.php
+++ src/classes/ReleaseCheck.php
@@ -43,7 +43,7 @@ class ReleaseCheck
      * UPDATE THIS AFTER RELEASING
      * ///////////////////////////
      */
-    public const INSTALLED_VERSION = '2.0.2';
+    public const INSTALLED_VERSION = '2.0.3';
 
     /**
      * Fetch the update info on object creation

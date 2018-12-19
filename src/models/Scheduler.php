0835e63f15f780a86c6cd996584893a6037d6220
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-12-19 16:28:45 +0100

    fix use

--- src/models/Scheduler.php
+++ src/models/Scheduler.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace Elabftw\Elabftw;
 
+use Elabftw\Exceptions\DatabaseErrorException;
 use PDO;
 
 /**
--- web/app/controllers/StatusController.php
+++ web/app/controllers/StatusController.php
@@ -14,6 +14,7 @@ use Elabftw\Exceptions\DatabaseErrorException;
 use Elabftw\Exceptions\FilesystemErrorException;
 use Elabftw\Exceptions\IllegalActionException;
 use Elabftw\Exceptions\ImproperActionException;
+use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**

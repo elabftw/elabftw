927dcbe527d53b9ad2350ece5f05ca7b0992e2fc
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-01-02 15:51:54 +0100

    add acceptance tests

--- tests/acceptance/ChangeVisibilityExperimentCept.php
+++ tests/acceptance/ChangeVisibilityExperimentCept.php
@@ -1,14 +1,7 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Test creating an experiment');
-testLogin($I);
-$I->amOnPage('experiments.php');
-$I->see('Experiments');
-$I->amOnPage('experiments.php?create=true');
-$I->see('Tags');
-$I->see('Date');
-$I->see('Title');
 $I->wantTo('Change the visibility to user');
+testLogin($I);
 $I->amOnPage('experiments.php?mode=edit&id=1');
 $I->selectOption('#visibility_select', 'Only me');
 $I->waitForJs('return jQuery.active == 0', 10);

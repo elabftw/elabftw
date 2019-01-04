033107363a1f697e3db647059b0ff88aef458e71
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-01-04 18:58:07 +0100

    better api controller

--- web/app/controllers/ApiController.php
+++ web/app/controllers/ApiController.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace Elabftw\Elabftw;
 
 use Elabftw\Controllers\ApiController;
+use Elabftw\Elabftw\Tools;
 use Elabftw\Exceptions\DatabaseErrorException;
 use Elabftw\Exceptions\FilesystemErrorException;
 use Elabftw\Exceptions\IllegalActionException;
@@ -32,6 +33,8 @@ use Symfony\Component\HttpFoundation\Response;
 require_once \dirname(__DIR__, 3) . '/config.php';
 require_once \dirname(__DIR__, 3) . '/vendor/autoload.php';
 
+$Response = new JsonResponse(array('error' => Tools::error()));
+
 try {
     // create Request object
     $Request = Request::createFromGlobals();
@@ -40,116 +43,25 @@ try {
     $ApiController = new ApiController($Request);
     $Response = $ApiController->getResponse();
 
-    // verify the key and load user infos
-    $Users = new Users();
-    $ApiKeys = new ApiKeys($Users);
-    $keyArr = $ApiKeys->readFromApiKey($Request->server->get('HTTP_AUTHORIZATION'));
-    $Users->setId((int) $keyArr['userid']);
-    $canWrite = (bool) $keyArr['canWrite'];
-
-    // parse args
-    $args = explode('/', rtrim($Request->query->get('req'), '/'));
-
-    // assign the id if there is one
-    $id = null;
-    if (Tools::checkId((int) end($args)) !== false) {
-        $id = (int) end($args);
-    }
-
-    // assign the endpoint
-    $endpoint = array_shift($args);
-
-    // load Entity
-    if ($endpoint === 'uploads') {
-        if ($id === null) {
-            throw new ImproperActionException('You need to specify an ID');
-        }
-        $Entity = new Uploads();
-        $uploadData = $Entity->readFromId($id);
-        // check user owns the file
-        // we could also check if user has read access to the item
-        // but for now let's just restrict downloading file via API to owned files
-        if ($uploadData['userid'] !== $Users->userData['userid']) {
-            throw new IllegalActionException('User tried to download file without permission.');
-        }
-        $filePath = \dirname(__DIR__, 3) . '/uploads/' . $uploadData['long_name'];
-        $Response = new Response(\file_get_contents($filePath));
-
-    } elseif ($endpoint === 'experiments') {
-        $Entity = new Experiments($Users, $id);
-    } elseif ($endpoint === 'items') {
-        $Entity = new Database($Users, $id);
-    } else {
-        throw new IllegalActionException('Bad endpoint.');
-    }
-
-    if ($Entity instanceof Experiments || $Entity instanceof Database) {
-        $Api = new Api($Entity);
-
-        // a simple GET
-        if ($Request->server->get('REQUEST_METHOD') === 'GET') {
-            $content = $Api->getEntity();
-
-        // POST request
-        } else {
-
-            if (!$canWrite) {
-                throw new ImproperActionException('Cannot use readonly key with POST method!');
-            }
-            // FILE UPLOAD
-            if ($Request->files->count() > 0) {
-                $content = $Api->uploadFile($Request);
-
-            // TITLE DATE BODY UPDATE
-            } elseif ($Request->request->has('title')) {
-                $content = $Api->updateEntity(
-                    $Request->request->get('title'),
-                    $Request->request->get('date'),
-                    $Request->request->get('body')
-                );
-
-            // ADD TAG
-            } elseif ($Request->request->has('tag')) {
-                $content = $Api->addTag($Request->request->get('tag'));
-
-            // ADD LINK
-            } elseif ($Request->request->has('link')) {
-                $content = $Api->addLink((int) $Request->request->get('link'));
-
-
-            // CREATE AN EXPERIMENT
-            } else {
-                if ($endpoint === 'experiments') {
-                    $content = $Api->createExperiment();
-                } else {
-                    throw new ImproperActionException('Creating database items is not supported.');
-                }
-            }
-        }
-    }
-    if ($Response instanceof JsonResponse) {
-        $Response->setData($content);
-    }
-
 } catch (ImproperActionException $e) {
     $Response->setData(array(
         'error' => $e->getMessage()
     ));
 
 } catch (IllegalActionException $e) {
-    $Log->notice('', array(array('userid' => $App->Session->get('userid')), array('IllegalAction', $e)));
+    $Log->notice('', array('IllegalAction', $e));
     $Response->setData(array(
         'error' => Tools::error(true)
     ));
 
 } catch (DatabaseErrorException | FilesystemErrorException $e) {
-    $Log->error('', array(array('userid' => $App->Session->get('userid')), array('Error', $e)));
+    $Log->error('', array('Error', $e));
     $Response->setData(array(
         'error' => $e->getMessage()
     ));
 
 } catch (Exception $e) {
-    $Log->error('', array(array('userid' => $App->Session->get('userid')), array('Exception' => $e)));
+    $Log->error('', array('Exception' => $e));
 
 } finally {
     $Response->send();

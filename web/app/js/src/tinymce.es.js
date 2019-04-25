44bb131a661466daf3ee20332b6b085526871d15
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-04-25 14:50:01 +0200

    sort alphabetically in tinymce

--- web/app/js/src/tinymce.es.js
+++ web/app/js/src/tinymce.es.js
@@ -6,20 +6,20 @@
  * @package elabftw
  */
 import tinymce from 'tinymce/tinymce';
-import 'tinymce/themes/silver/theme';
-import 'tinymce/plugins/table';
-import 'tinymce/plugins/searchreplace';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/fullscreen';
-import 'tinymce/plugins/insertdatetime';
-import 'tinymce/plugins/paste';
-import 'tinymce/plugins/charmap';
-import 'tinymce/plugins/lists';
 import 'tinymce/plugins/advlist';
-import 'tinymce/plugins/save';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/hr';
 import 'tinymce/plugins/image';
 import 'tinymce/plugins/imagetools';
+import 'tinymce/plugins/insertdatetime';
 import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
 import 'tinymce/plugins/pagebreak';
-import 'tinymce/plugins/codesample';
-import 'tinymce/plugins/hr';
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/table';
+import 'tinymce/themes/silver/theme';

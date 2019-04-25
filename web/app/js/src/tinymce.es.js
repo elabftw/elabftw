e6df30249ffd4bd62ce6d30f624af6184f33d76f
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-04-25 14:35:44 +0200

    upgrade to tinymce 5.0

---
+++ web/app/js/src/tinymce.es.js
@@ -0,0 +1,25 @@
+/**
+ * @author Nicolas CARPi <nicolas.carpi@curie.fr>
+ * @copyright 2012 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+import tinymce from 'tinymce/tinymce';
+import 'tinymce/themes/silver/theme';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/imagetools';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/pagebreak';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/hr';

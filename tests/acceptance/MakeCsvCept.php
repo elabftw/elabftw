59606385f43a1758ae64ed1f0563600bf33cf48c
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-04-26 14:54:25 +0200

    fix the duplicate experiment acceptance test

--- tests/acceptance/MakeCsvCept.php
+++ tests/acceptance/MakeCsvCept.php
@@ -1,8 +1,6 @@
 <?php
-use \Codeception\Util\HttpCode;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Test creating a CSV file');
 testLogin($I);
 $I->amOnPage('/make.php?what=csv&id=1&type=experiments');
-//$I->seeHttpHeader('Content-Type', 'text-csv; charset=UTF-8');
-//$I->seeResponseCodeIs(HttpCode::OK);
+// we can't see the headers

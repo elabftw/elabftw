908aa18a0c912419fbbc1b1a2cef506f0bef8eca
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-05-06 17:00:49 +0200

    3.1.1

--- src/classes/ReleaseCheck.php
+++ src/classes/ReleaseCheck.php
@@ -40,7 +40,7 @@ class ReleaseCheck
     private const URL_HTTP = 'http://get.elabftw.net/updates.ini';
 
     /** @var string INSTALLED_VERSION the current version of elabftw */
-    public const INSTALLED_VERSION = '3.1.0';
+    public const INSTALLED_VERSION = '3.1.1';
 
     /**
      * Fetch the update info on object creation

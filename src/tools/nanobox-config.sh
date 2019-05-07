59ea07fa8c4acce9871e7035aadb1b63abf59cd0
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-05-07 17:52:03 +0200

    retab nanobox script

--- src/tools/nanobox-config.sh
+++ src/tools/nanobox-config.sh
@@ -1,19 +1,20 @@
+#!/usr/bin/env bash
 getEnv() {
-	db_host=${DATA_MYSQL_HOST:-localhost}
-	db_password=${DATA_MYSQL_ROOT_PASS}
+    db_host=${DATA_MYSQL_HOST:-localhost}
+    db_password=${DATA_MYSQL_ROOT_PASS}
 }
 
 writeConfigFile() {
-	# write config file from env var
+    # write config file from env var
     config_path="/app/config.php"
     secret_key=$(curl -L https://demo.elabftw.net/install/generateSecretKey.php)
-	config="<?php
-	define('DB_HOST', '${db_host}');
-	define('DB_NAME', 'elabftw');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '${db_password}');
+    config="<?php
+    define('DB_HOST', '${db_host}');
+    define('DB_NAME', 'elabftw');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '${db_password}');
     define('SECRET_KEY', '${secret_key}');"
-	echo "$config" > "$config_path"
+    echo "$config" > "$config_path"
 }
 getEnv
 writeConfigFile

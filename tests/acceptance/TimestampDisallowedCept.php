838f22ee5918183dc4d941807734475e770f939f
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-05-28 00:30:32 +0200

    more little fixes

--- tests/acceptance/TimestampDisallowedCept.php
+++ tests/acceptance/TimestampDisallowedCept.php
@@ -10,5 +10,5 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('Timestamp an experiment (timestamping disallowed)');
 testLogin($I);
 $I->amOnPage('experiments.php?mode=view&id=1');
-# not working FIXME
-#$I->dontSeeElement('#confirmTimestamp');
+// not working FIXME
+//$I->dontSeeElement('#confirmTimestamp');

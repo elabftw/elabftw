4cfc225222819cf63ae571ef4c2021acdc4c9c6c
Sherjeel Shabih <shabihsherjeel@gmail.com>
2019-11-08 17:05:19 +0100

    Added support to switch to other views on JSON editor

--- web/app/js/src/jsoneditor-loader.js
+++ web/app/js/src/jsoneditor-loader.js
@@ -3,28 +3,32 @@ const container = document.getElementById('jsoneditor');
 
 const options = {onChangeJSON:function(){
   $('.jsonSaver').removeAttr('disabled', 0).text('Save').css('cursor','pointer');
-}};
+},
+modes:['tree','code','view','form'],
+onModeChange:function(newMode, oldMode){
+  if(newMode==='code'){$('#jsoneditor').height('800px');}
+  else{$('#jsoneditor').removeAttr('style');}
+}
+};
 
 var JSONEditor;
 const editor = new JSONEditor(container, options);
 $('.jsonSaver').attr('disabled', 1).text('Saved').css('cursor','default');
 $('.jsonEditorDiv').hide();
 
-var currentFileType;
 var currentFileUploadID;
 var currentFileItemID;
 
 key.filter = function(event){
   var tagName = (event.target || event.srcElement).tagName;
   return !(tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA' || (event.target || event.srcElement).hasAttribute('contenteditable'));
-}
+};
 
 $(document).on('click', '.jsonLoader', function(){
   $.get('app/download.php', {f:$(this).data('link')}).done(function(data){
     editor.set(JSON.parse(data));
     $('.jsonEditorDiv').show();
   });
-  currentFileType = $(this).data('type');
   currentFileUploadID = $(this).data('id');
   currentFileItemID = $(this).data('uploadid');
 });
@@ -43,6 +47,6 @@ $(document).on('click', '.jsonSaver', function(){
   request.open('POST', 'app/controllers/EntityController.php');
   request.onload = function () {
     $('.jsonSaver').attr('disabled', 1).text('Saved').css('cursor','default');
-};
+  };
   request.send(formData);
 });

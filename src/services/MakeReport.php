fbfa792dc629d5fd7ce321fc54d95133177d982a
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-12-05 20:56:56 +0100

    fix make report missing column name

--- src/services/MakeReport.php
+++ src/services/MakeReport.php
@@ -70,6 +70,7 @@ class MakeReport
             'usergroup',
             'archived',
             'last_login',
+            'full_name',
             'team_name',
             'diskusage_in_bytes',
             'diskusage_formatted',

7b629e80753af44d915538325ccd3f1c5d2c1e2f
Nicolas CARPi <nicolas.carpi@curie.fr>
2020-02-13 17:09:32 +0100

    fix schema update

--- src/sql/schema52.sql
+++ src/sql/schema52.sql
@@ -24,7 +24,7 @@ START TRANSACTION;
     ALTER TABLE `users2teams` ADD CONSTRAINT `fk_users2teams_teams_id` FOREIGN KEY (`teams_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
     ALTER TABLE `users2teams` ADD CONSTRAINT `fk_users2teams_users_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
     ALTER TABLE `api_keys` ADD `team` int(10) UNSIGNED NOT NULL;
-    UPDATE `api_keys` SET `team` = (SELECT `team` FROM `users` WHERE `users.userid` = `api_keys.userid`);
+    UPDATE `api_keys` SET `team` = (SELECT `team` FROM `users` WHERE users.userid = api_keys.userid);
     ALTER TABLE `api_keys` ADD CONSTRAINT `fk_api_keys_teams_id` FOREIGN KEY (`team`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
     ALTER TABLE `users` DROP `team`;
     UPDATE config SET conf_value = 52 WHERE conf_name = 'schema';

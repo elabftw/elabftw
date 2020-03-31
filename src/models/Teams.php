fed99c201673712677d58ee7db5328d36ef26d03
Nicolas CARPi <nicolas.carpi@curie.fr>
2020-03-31 14:48:20 +0200

    correctly cast to string and use true as third param for in_array
    this bug was brought back by phpcs!
    Lesson of the day, always check the diff after an automatic fix!!!
    
    Fix #1852

--- src/models/Teams.php
+++ src/models/Teams.php
@@ -424,7 +424,7 @@ class Teams implements CrudInterface
     {
         $UsersHelper = new UsersHelper();
         $teams = $UsersHelper->getTeamsIdFromUserid($userid);
-        return in_array($team, $teams, true);
+        return in_array((string) $team, $teams, true);
     }
 
     public function isUserInTeam(int $userid, int $team): bool

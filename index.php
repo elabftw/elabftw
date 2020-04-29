e9ba900fe768cd86e9adc235844282ef080d9757
Nicolas CARPi <nico-git@deltablot.email>
2020-04-29 03:55:16 +0200

    remove readthedocs mentions

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 <p>The path of the served files is now web/. You need to edit your webserver config.</p>
 
-<p>Have a look at <a href='https://elabftw.readthedocs.io/en/next/how-to-update.html#complete-upgrade-guide-from-1-8-x-to-2-0-0'>the upgrade guide</a>.</p>
+<p>Have a look at <a href='https://doc.elabftw.net/how-to-update.html#complete-upgrade-guide-from-1-8-x-to-2-0-0'>the upgrade guide</a>.</p>

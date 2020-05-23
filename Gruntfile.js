6677d967192eea367bb0393f00c3a8013aa33219
Marcel <65481677+MarcelBolten@users.noreply.github.com>
2020-05-22 23:56:02 -0700

    bring back in sync to elabftw

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function(grunt) {
       },
       tinymce: {
         // copy the mobile font file
-        command: 'cpy-cli node_modules/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff web/app/css/tinymce/fonts/tinymce-mobile.woff'
+        command: 'cp node_modules/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff web/app/css/tinymce/fonts/tinymce-mobile.woff'
       }
     }
   });

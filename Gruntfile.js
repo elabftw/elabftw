6909e043ff8c4f04ec2aad875a8e65c8d1bff2b9
Marcel <65481677+MarcelBolten@users.noreply.github.com>
2020-05-24 23:32:04 -0700

    Fix cpy command

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function(grunt) {
       },
       tinymce: {
         // copy the mobile font file
-        command: 'cpy-cli node_modules/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff web/app/css/tinymce/fonts/tinymce-mobile.woff'
+        command: 'yarn cpy node_modules/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff web/app/css/tinymce/fonts/'
       }
     }
   });

be769d259cdf0e3803facd10df5c97e322c22e3f
Emmanuel Dreyfus <manu@netbsd.org>
2020-06-30 14:13:29 +0200

    Fix schema55 version, which is actually 55.

--- src/sql/schema55.sql
+++ src/sql/schema55.sql
@@ -1,4 +1,4 @@
--- Schema 53
+-- Schema 55
 START TRANSACTION;
     INSERT INTO config (conf_name, conf_value) VALUES ('extauth_remote_user', '');
     INSERT INTO config (conf_name, conf_value) VALUES ('extauth_firstname', '');
@@ -6,5 +6,5 @@ START TRANSACTION;
     INSERT INTO config (conf_name, conf_value) VALUES ('extauth_email', '');
     INSERT INTO config (conf_name, conf_value) VALUES ('extauth_teams', '');
 
-    UPDATE config SET conf_value = 53 WHERE conf_name = 'schema';
+    UPDATE config SET conf_value = 55 WHERE conf_name = 'schema';
 COMMIT;

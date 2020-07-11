d6d9c714d6f5b9c518f90b2c668368b497f4e2b5
Nicolas CARPi <nico-git@deltablot.email>
2020-07-12 01:33:58 +0200

    don't use compact for babel => no warnings

--- builder.js
+++ builder.js
@@ -112,7 +112,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            compact: false,
           }
         }
       },

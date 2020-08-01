b51da78c7a506e757ca790aeed461fdda4f132c8
Marcel <65481677+MarcelBolten@users.noreply.github.com>
2020-08-01 00:25:25 -0700

    Don't increase failed attempts during MFA if user has auth

--- web/app/controllers/MfaController.php
+++ web/app/controllers/MfaController.php
@@ -45,7 +45,7 @@ try {
             $location = $App->Session->get('mfa_redirect');
             $App->Session->remove('mfa_redirect');
             $App->Session->remove('mfa_secret');
-        } elseif (!$verifyMFACode) {
+        } elseif (!$verifyMFACode && !$App->Session->has('auth')) {
             $Auth->increaseFailedAttempt();
         }
     }

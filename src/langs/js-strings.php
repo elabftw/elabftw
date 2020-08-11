4306cbec8db193a0e5a8955bd506c70b8b340c22
Nicolas CARPi <nico-git@deltablot.email>
2020-08-11 03:41:41 +0200

    add findUnusedVariablesAndParams to psalm

--- src/langs/js-strings.php
+++ src/langs/js-strings.php
@@ -4,23 +4,33 @@
  * These strings are used in JS and appear nowhere else in the codebase
  *
  */
+/** @psalm-suppress UnusedFunctionCall */
 // comment-add
 _('Add a comment');
+/** @psalm-suppress UnusedFunctionCall */
 // dropzone-upload-area
 _('Drop files here to upload');
+/** @psalm-suppress UnusedFunctionCall */
 // entity-default-title
 _('Untitled');
+/** @psalm-suppress UnusedFunctionCall */
 // entity-delete-warning
 _('The selected item(s) and all associated data will be permanently deleted. This cannot be undone. Are you sure?');
+/** @psalm-suppress UnusedFunctionCall */
 // generic-delete-warning
 _('Delete this?');
+/** @psalm-suppress UnusedFunctionCall */
 // link-delete-warning
 _('Delete this link?');
+/** @psalm-suppress UnusedFunctionCall */
 // please-wait
 _('Please wait…');
+/** @psalm-suppress UnusedFunctionCall */
 // step-delete-warning
 _('Delete this step?');
+/** @psalm-suppress UnusedFunctionCall */
 // tag-delete-warning
 _('Delete this tag?');
+/** @psalm-suppress UnusedFunctionCall */
 // upload-file-comment
 _('File comment');

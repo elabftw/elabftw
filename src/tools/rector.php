5a6459c881d35a580293a44a6b4d7dff51d263a7
Marcel Bolten <marcel.bolten@msl.ubc.ca>
2020-08-10 19:34:41 -0700

    phpcs

--- src/tools/rector.php
+++ src/tools/rector.php
@@ -9,7 +9,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set('php_version_features', '7.3');
 
-    $parameters->set('sets', ['code-quality', 'twig-underscore-to-namespace', 'dead-code', 'phpstan']);
+    $parameters->set('sets', array('code-quality', 'twig-underscore-to-namespace', 'dead-code', 'phpstan'));
 
-    $parameters->set('exclude_rectors', ['Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector', 'Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector', 'Rector\SOLID\Rector\ClassMethod\UseInterfaceOverImplementationInConstructorRector', 'Rector\DeadCode\Rector\Concat\RemoveConcatAutocastRector', 'Rector\PHPStan\Rector\Cast\RecastingRemovalRector']);
+    $parameters->set('exclude_rectors', array('Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector', 'Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector', 'Rector\SOLID\Rector\ClassMethod\UseInterfaceOverImplementationInConstructorRector', 'Rector\DeadCode\Rector\Concat\RemoveConcatAutocastRector', 'Rector\PHPStan\Rector\Cast\RecastingRemovalRector'));
 };

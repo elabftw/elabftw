be4816288f4f305ec03832293890a2cc9f38d18d
Sherjeel Shabih <shabihsherjeel@gmail.com>
2020-08-25 14:03:55 +0200

    Add "save as" option for JSON editor (#2108)

--- src/ts/jsoneditor.ts
+++ src/ts/jsoneditor.ts
@@ -8,6 +8,7 @@
 declare let key: any;
 
 import { notif } from './misc';
+import i18next from 'i18next';
 import JSONEditor from 'jsoneditor';
 
 // editor div
@@ -41,21 +42,21 @@ $(document).ready(function() {
       return !(tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA' || (event.target || event.srcElement).hasAttribute('contenteditable'));
     };
 
-    let currentFileUploadID: string;
     let currentFileItemID: string;
+    let itemID: string;
 
     // the loader action appears under .json uploaded files
     $(document).on('click', '.jsonLoader', function() {
+      // add the filename as a title
+      $('#jsonEditorTitle').html(i18next.t('filename') + ': ' + $(this).data('name'));
       $.get('app/download.php', {
         f: $(this).data('link')
       }).done(function(data) {
         try {
           editor.set(JSON.parse(data));
-          $('#jsonEditorDiv').collapse('toggle');
+          $('#jsonEditorDiv').collapse('show');
           if ($('.jsonEditorPlusMinusButton').html() === '+') {
             $('.jsonEditorPlusMinusButton').html('-').addClass('btn-neutral').removeClass('btn-primary');
-          } else {
-            $('.jsonEditorPlusMinusButton').html('+').removeClass('btn-neutral').addClass('btn-primary');
           }
         } catch(e) {
           // If it is just a parsing error, then we let the user edit the file.
@@ -67,28 +68,43 @@ $(document).ready(function() {
             notif({'res': false, 'msg':'JSON Editor: ' + e.message});
           }
         }
+        $('#jsonEditorContainer')[0].scrollIntoView();
       });
-      currentFileUploadID = $(this).data('id');
       currentFileItemID = $(this).data('uploadid');
+      itemID = $(this).data('id');
     });
 
-    $(document).on('click', '.jsonSaver', function(){
-      if (typeof currentFileUploadID === 'undefined') {
+    // Clear the JSONEditor and unload the file
+    $(document).on('click', '.jsonClear', function() {
+      currentFileItemID = undefined;
+      editor.set({});
+      $('#jsonEditorTitle').html('File was unloaded.');
+    });
+
+    // The save function is now defined separately
+    const saveJsonFile = function(){
+      if (typeof currentFileItemID === 'undefined') {
         // we are creating a new file
-        const realName = prompt('Enter name of the file');
+        let realName = prompt(i18next.t('request-filename'));
         if (realName == null) {
           return;
         }
-        const id = $('#main_form').find('input[name="id"]').attr('value');
+        // strip the filename of the .json extension from the name if available
+        if (realName.slice(-5).includes('.json')) {
+          realName = realName.slice(0, -5);
+        }
+        // add the new name for the file as a title
+        $('#jsonEditorTitle').html(i18next.t('filename') + ': ' + realName + '.json');
         $.post('app/controllers/EntityAjaxController.php', {
           addFromString: true,
           type: 'experiments',
-          id: id,
+          id: itemID,
           realName: realName,
           fileType: 'json',
           string: JSON.stringify(editor.get())
         }).done(function(json) {
-          $('#filesdiv').load('experiments.php?mode=edit&id=' + id + ' #filesdiv');
+          $('#filesdiv').load(window.location.href + ' #filesdiv');
+          currentFileItemID = String(json.uploadId);
           notif(json);
         });
       } else {
@@ -97,7 +113,7 @@ $(document).ready(function() {
         const blob = new Blob([JSON.stringify(editor.get())], { type: 'application/json' });
         formData.append('replace', 'true');
         formData.append('upload_id', currentFileItemID);
-        formData.append('id', currentFileUploadID);
+        formData.append('id', itemID);
         formData.append('type', 'experiments');
         formData.append('file', blob);
 
@@ -111,6 +127,14 @@ $(document).ready(function() {
           }
         });
       }
+    };
+
+    // Add support for 'Save as' by resetting the currentFileItemID to undefined
+    $(document).on('click', '.jsonSaveAs', function () {
+      currentFileItemID = undefined;
+      saveJsonFile();
     });
+
+    $(document).on('click', '.jsonSaver', saveJsonFile);
   }
 });

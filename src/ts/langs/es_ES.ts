c14a6889014488233a047bd0b9c3d43f6264a3a8
Sherjeel Shabih <shabihsherjeel@gmail.com>
2020-08-25 13:42:39 +0200

    Alphabetical order has been restored

--- src/ts/langs/es_ES.ts
+++ src/ts/langs/es_ES.ts
@@ -5,14 +5,14 @@ const t = {
     "dropzone-upload-area": "Deje sus archivos aquí para subirlos",
     "entity-default-title": "Sin título",
     "entity-delete-warning": "Los item(s) seleccionados y todo los datos asociados serán permanentemente elilminados. Esto no puede deshacerse. Está seguro?",
+    "filename": "Nombre del archivo",
     "generic-delete-warning": "¿Eliminar esto?",
     "link-delete-warning": "¿Eliminar este enlace?",
     "please-wait": "Por favor espera",
+    "request-filename": "Ingrese el nombre del archivo",
     "step-delete-warning": "¿Eliminar este paso?",
     "tag-delete-warning": "Eliminar esta etiqueta?",
     "upload-file-comment": "Comentario de archivo",
-    "filename": "Nombre del archivo",
-    "request-filename": "Ingrese el nombre del archivo",
   }
 };
 

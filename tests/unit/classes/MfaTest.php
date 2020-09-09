a2deb5e4c855d911c49ebf4e6ea248b16c602df5
Marcel Bolten <marcel.bolten@msl.ubc.ca>
2020-09-08 20:17:54 -0700

    improve unit tests

--- tests/unit/classes/MfaTest.php
+++ tests/unit/classes/MfaTest.php
@@ -20,76 +20,57 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class MfaTest extends \PHPUnit\Framework\TestCase
 {
     /** @var Session $Session Current session */
-    public $Session;
+    private $Session;
 
     /** @var Request $Request Current request */
     private $Request;
 
+    /** @var string $testPath Some file path */
+    private $testPath;
+
+    /** @var string $secret The 2FA test secret */
+    private $secret;
+
     protected function setUp(): void
     {
-        $this->Request = Request::createFromGlobals();
+        $this->testPath = 'path/to/some/file.php';
+        $this->secret = 'EXAMPLE2FASECRET234567ABCDEFGHIJ';
 
+        $this->Request = Request::createFromGlobals();
         $this->Session = new Session();
 
         $this->Request->setSession($this->Session);
         $this->Mfa = new Mfa($this->Request, $this->Session);
     }
 
-    public function testNeedVerification()
+    protected function tearDown(): void
     {
-        $path = 'path/to/some/file.php';
-        $this->assertFalse($this->Mfa->needVerification(2, $path));
-        $this->assertFalse($this->Session->has('mfa_secret'));
-        $this->assertFalse($this->Session->has('mfa_redirect'));
-
-        $secret = 'EXAMPLE2FASECRET234567ABCDEFGHIJ';
-        $this->assertTrue($this->Mfa->needVerification(1, $path));
-        $this->assertEquals($this->Session->get('mfa_redirect'), $path);
-        $this->assertEquals($this->Session->get('mfa_secret'), $secret);
-    }
-
-    public function testVerifyCode()
-    {
-        $secret = 'EXAMPLE2FASECRET234567ABCDEFGHIJ';
-        $TwoFactorAuth = new TwoFactorAuth('eLabFTW', 6, 30, 'sha1', new MpdfQrProvider());
-        $code = $TwoFactorAuth->getCode($secret);
-
-        $this->Request->request->set('mfa_code', $code);
-        $this->Session->set('mfa_secret', $secret);
-        $this->assertTrue($this->Mfa->verifyCode());
-        $this->assertTrue($this->Session->has('mfa_verified'));
-        $this->Session->remove('mfa_verified');
-
-        $this->Request->request->set('mfa_code', '123456');
-        $this->assertFalse($this->Mfa->verifyCode());
-        $this->assertFalse($this->Session->has('mfa_verified'));
+        $this->Session->invalidate();
     }
 
     public function testEnable()
     {
-        $path = 'path/to/some/file.php';
-        $this->assertEquals($this->Mfa->enable($path), '../../login.php');
-        $this->assertEquals($this->Session->get('mfa_redirect'), $path);
-        $this->assertEquals(strlen($this->Session->get('mfa_secret')), 32);
+        $this->assertEquals('../../login.php', $this->Mfa->enable($this->testPath));
+        $this->assertEquals($this->testPath, $this->Session->get('mfa_redirect'));
+        $this->assertEquals(32, strlen($this->Session->get('mfa_secret')));
         $this->assertTrue($this->Session->get('enable_mfa'));
-
-        $this->Session->remove('mfa_redirect');
-        $this->Session->remove('mfa_secret');
-        $this->Session->remove('enable_mfa');
     }
 
-    public function testCleanup()
+    public function testCleanupFalse()
     {
-        $path = 'path/to/some/file.php';
-        $this->Mfa->enable($path);
-        $this->assertEquals($this->Mfa->cleanup(), $path);
+        $this->Mfa->enable($this->testPath);
+
+        $this->assertEquals($this->Mfa->cleanup(), $this->testPath);
         $this->assertTrue($this->Session->has('enable_mfa'));
         $this->assertFalse($this->Session->has('mfa_secret'));
         $this->assertFalse($this->Session->has('mfa_redirect'));
-        $this->Session->remove('enable_mfa');
+    }
 
-        $this->Mfa->enable($path);
-        $this->assertEquals($this->Mfa->cleanup(true), $path);
+    public function testCleanupTrue()
+    {
+        $this->Mfa->enable($this->testPath);
+
+        $this->assertEquals($this->Mfa->cleanup(true), $this->testPath);
         $this->assertFalse($this->Session->has('enable_mfa'));
         $this->assertFalse($this->Session->has('mfa_secret'));
         $this->assertFalse($this->Session->has('mfa_redirect'));
@@ -97,13 +78,48 @@ class MfaTest extends \PHPUnit\Framework\TestCase
 
     public function testSaveSecret()
     {
-        $path = 'path/to/some/file.php';
         $this->Session->set('userid', 1);
-        $this->Mfa->enable($path);
+        $this->Session->set('mfa_secret', $this->secret);
+        $this->Session->set('enable_mfa', true);
+        $this->Session->set('mfa_redirect', $this->testPath);
 
-        $this->assertEquals($this->Mfa->saveSecret(), $path);
+        $this->assertEquals($this->testPath, $this->Mfa->saveSecret());
         $this->assertFalse($this->Session->has('enable_mfa'));
         $this->assertFalse($this->Session->has('mfa_secret'));
         $this->assertFalse($this->Session->has('mfa_redirect'));
+    }
+
+    public function testNeedVerificationFalse()
+    {
+        $this->assertFalse($this->Mfa->needVerification(2, $this->testPath));
+        $this->assertFalse($this->Session->has('mfa_secret'));
+        $this->assertFalse($this->Session->has('mfa_redirect'));
+    }
+
+    public function testNeedVerificationTrue()
+    {
+        $this->assertTrue($this->Mfa->needVerification(1, $this->testPath));
+        $this->assertEquals($this->testPath, $this->Session->get('mfa_redirect'));
+        $this->assertEquals($this->secret, $this->Session->get('mfa_secret'));
+    }
+
+    public function testVerifyCodeFalse()
+    {
+        $TwoFactorAuth = new TwoFactorAuth('eLabFTW', 6, 30, 'sha1', new MpdfQrProvider());
+        $this->Request->request->set('mfa_code', $TwoFactorAuth->getCode($this->secret));
+        $this->Session->set('mfa_secret', $this->secret);
+
+        $this->assertTrue($this->Mfa->verifyCode());
+        $this->assertTrue($this->Session->has('mfa_verified'));
+    }
+
+    public function testVerifyCodeTrue()
+    {
+        $TwoFactorAuth = new TwoFactorAuth('eLabFTW', 6, 30, 'sha1', new MpdfQrProvider());
+        $this->Request->request->set('mfa_code', '123456');
+        $this->Session->set('mfa_secret', $this->secret);
+
+        $this->assertFalse($this->Mfa->verifyCode());
+        $this->assertFalse($this->Session->has('mfa_verified'));
     }
 }

1ba910c0324d87098a38029caf6f3a64b5908d52
Nicolas CARPi <nico-git@deltablot.email>
2020-09-09 20:11:39 +0200

    fix bad bugfix

--- src/models/Pins.php
+++ src/models/Pins.php
@@ -85,6 +85,19 @@ class Pins
     }
 
     /**
+     * Remove all traces of that entity because it has been destroyed
+     */
+    public function cleanup(): void
+    {
+        $sql = 'DELETE FROM pin2users WHERE entity_id = :entity_id AND type = :type';
+        $req = $this->Db->prepare($sql);
+        $req->bindParam(':entity_id', $this->Entity->id, PDO::PARAM_INT);
+        $req->bindParam(':type', $this->Entity->type);
+
+        $this->Db->execute($req);
+    }
+
+    /**
      * Remove current entity from pinned of current user
      * The method is public because it is called on entity destroy
      *
@@ -94,8 +107,9 @@ class Pins
     {
         $this->Entity->canOrExplode('read');
 
-        $sql = 'DELETE FROM pin2users WHERE entity_id = :entity_id AND type = :type';
+        $sql = 'DELETE FROM pin2users WHERE entity_id = :entity_id AND users_id = :users_id AND type = :type';
         $req = $this->Db->prepare($sql);
+        $req->bindParam(':users_id', $this->Entity->Users->userData['userid']);
         $req->bindParam(':entity_id', $this->Entity->id, PDO::PARAM_INT);
         $req->bindParam(':type', $this->Entity->type);
 

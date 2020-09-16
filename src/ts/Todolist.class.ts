26d80afc7d9b7a9c27962e1b7f9c16af2192507f
Marcel Bolten <marcel.bolten@msl.ubc.ca>
2020-09-16 12:49:57 -0700

    jslint

--- src/ts/Todolist.class.ts
+++ src/ts/Todolist.class.ts
@@ -61,7 +61,7 @@ export default class Todolist {
       let html = '';
       for (const exp of json.msg) {
         html += `<li><h3><a href='experiments.php?mode=view&id=${exp.id}'>${exp.title}</a></h3>`;
-        for (let stepsData of Object.entries(exp.steps)) {
+        for (const stepsData of Object.entries(exp.steps)) {
           const stepId = stepsData[1][0];
           const stepBody = stepsData[1][1];
           html += `<div><input type='checkbox' class='stepbox mr-1' id='todo_step_${stepId}' data-id='${exp.id}' data-type='experiments' data-stepid='${stepId}' />${stepBody}</div>`;

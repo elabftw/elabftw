29a9f7f6fa7b1208698b3708e0f22d8bb458b500
Nicolas CARPi <nico-git@deltablot.email>
2020-10-30 00:06:05 +0100

    finish usershelper tests

--- tests/unit/services/UsersHelperTest.php
+++ tests/unit/services/UsersHelperTest.php
@@ -20,4 +20,22 @@ class UsersHelperTest extends \PHPUnit\Framework\TestCase
     {
         $this->assertTrue($this->UsersHelper->hasExperiments());
     }
+
+    public function testGetTeamsFromUserid()
+    {
+        $expected = array(array('id' => '1', 'name' => 'Alpha'));
+        $this->assertEquals($expected, $this->UsersHelper->getTeamsFromUserid());
+    }
+
+    public function testGetPermissions()
+    {
+        $expected = array('is_admin' => '1', 'is_sysadmin' => '1', 'can_lock' => '0');
+        $this->assertEquals($expected, $this->UsersHelper->getPermissions());
+    }
+
+    public function testgetTeamsNameFromUserid()
+    {
+        $expected = array('Alpha');
+        $this->assertEquals($expected, $this->UsersHelper->getTeamsNameFromUserid());
+    }
 }

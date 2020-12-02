10a872b4c37c609c815d54b3364ab6bad6ac40fb
Marcel Bolten <marcel.bolten@msl.ubc.ca>
2020-12-02 02:19:40 -0800

    update yarn.lock, fix breaking changes due to webpack 5
    yarn output:
    WARNING in ./node_modules/sax/lib/sax.js 166:13-37
    Module not found: Error: Can't resolve 'stream' in '/var/www/html/elabftw/node_modules/sax/lib'
    
    BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules by default.
    This is no longer the case. Verify if you need this module and configure a polyfill for it.
    
    If you want to include a polyfill, you need to:
            - add a fallback 'resolve.fallback: { "stream": require.resolve("stream-browserify") }'
            - install 'stream-browserify'
    If you don't want to include a polyfill, you can use an empty module like this:
            resolve.fallback: { "stream": false }
     @ ./node_modules/xml2js/lib/parser.js 36:8-22
     @ ./node_modules/xml2js/lib/xml2js.js 27:11-30
     @ ./node_modules/bio-parsers/src/parsers/snapgeneToJson.js 26:0-28 414:4-22
     @ ./node_modules/bio-parsers/src/parsers/anyToJson.js 9:0-46 70:45-59
     @ ./src/ts/ove.js
     @ ./src/ts/uploads.ts 5:0-45 14:4-24 75:24-44
    
    ERROR in ./node_modules/@gmod/gff/dist/api.js 100:15-32
    Module not found: Error: Can't resolve 'stream' in '/var/www/html/elabftw/node_modules/@gmod/gff/dist'
    
    BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules by default.
    This is no longer the case. Verify if you need this module and configure a polyfill for it.
    
    If you want to include a polyfill, you need to:
            - add a fallback 'resolve.fallback: { "stream": require.resolve("stream-browserify") }'
            - install 'stream-browserify'
    If you don't want to include a polyfill, you can use an empty module like this:
            resolve.fallback: { "stream": false }
     @ ./node_modules/@gmod/gff/dist/index.js 7:11-27
     @ ./node_modules/bio-parsers/src/parsers/gffToJson.js 1:0-28 6:22-41
     @ ./node_modules/bio-parsers/src/parsers/anyToJson.js 11:0-36 94:14-23 135:20-29
     @ ./src/ts/ove.js
     @ ./src/ts/uploads.ts 5:0-45 14:4-24 75:24-44
    
    ERROR in ./node_modules/xml2js/lib/parser.js 40:17-47
    Module not found: Error: Can't resolve 'timers' in '/var/www/html/elabftw/node_modules/xml2js/lib'
    
    BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules by default.
    This is no longer the case. Verify if you need this module and configure a polyfill for it.
    
    If you want to include a polyfill, you need to:
            - add a fallback 'resolve.fallback: { "timers": require.resolve("timers-browserify") }'
            - install 'timers-browserify'
    If you don't want to include a polyfill, you can use an empty module like this:
            resolve.fallback: { "timers": false }
     @ ./node_modules/xml2js/lib/xml2js.js 27:11-30
     @ ./node_modules/bio-parsers/src/parsers/snapgeneToJson.js 26:0-28 414:4-22
     @ ./node_modules/bio-parsers/src/parsers/anyToJson.js 9:0-46 70:45-59
     @ ./src/ts/ove.js
     @ ./src/ts/uploads.ts 5:0-45 14:4-24 75:24-44

--- builder.js
+++ builder.js
@@ -130,5 +130,11 @@ module.exports = {
         },
       }
     ]
-  }
+  },
+  resolve: {
+    fallback: {
+      "stream": require.resolve("stream-browserify"),
+      "timers": require.resolve("timers-browserify"),
+    },
+  },
 };

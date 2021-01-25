d8f110b5e0e5708aea973a40c0b19e4bc6f5116c
Marcel Bolten <marcel.bolten@msl.ubc.ca>
2021-01-25 00:11:16 -0800

    add title to to-do list delete button

--- src/ts/Todolist.class.ts
+++ src/ts/Todolist.class.ts
@@ -7,6 +7,8 @@
  */
 import Crud from './Crud.class';
 import { relativeMoment, makeSortableGreatAgain } from './misc';
+import i18next from 'i18next';
+
 
 export default class Todolist extends Crud {
 
@@ -44,7 +46,7 @@ export default class Todolist extends Crud {
       let html = '<ul id="todoItems-list" class="sortable" data-axis="y" data-table="todolist">';
       for (const entry of json.msg) {
         html += `<li id='todoItem_${entry.id}'>
-        <a class='clickable align-right destroyTodoItem' data-id='${entry.id}'>
+        <a class='clickable align-right destroyTodoItem' data-id='${entry.id}' title='` + i18next.t('generic-delete-warning') + `'>
           <i class='fas fa-trash-alt'></i>
         </a>
         <span style='font-size:90%;display:block;'><span class='draggable sortableHandle'><i class='fas fa-sort'></i></span> <span class='relative-moment' title='${entry.creation_time}'></span></span>

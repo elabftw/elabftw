538e509f0d3e849056dd25018a73a4b0222dbc41
Max Schröder <max.schroeder@uni-rostock.de>
2021-03-02 00:51:51 +0100

    Add configurable minimum delta for revisions (#2514)
    * Add configurable minimum delta for revisions

--- src/sql/schema57.sql
+++ src/sql/schema57.sql
@@ -6,6 +6,7 @@ START TRANSACTION;
     ALTER TABLE `users` ADD `password_hash` VARCHAR(255) NULL DEFAULT NULL AFTER `password`;
     INSERT INTO config (conf_name, conf_value) VALUES ('devmode', '0');
     INSERT INTO config (conf_name, conf_value) VALUES ('autologout_time', '0');
+    INSERT INTO config (conf_name, conf_value) VALUES ('min_delta_revisions', '100');
     ALTER TABLE `items` ADD `elabid` VARCHAR(255) NOT NULL;
     ALTER TABLE `experiments` CHANGE `datetime` `datetime` TIMESTAMP NULL DEFAULT NULL;
     ALTER TABLE `experiments` ADD `metadata` JSON NULL DEFAULT NULL;

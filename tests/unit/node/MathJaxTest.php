968c6287dd6451a796b042feb5fd548b129f2ed0
Nicolas CARPi <nico-git@deltablot.email>
2021-04-16 16:01:35 +0200

    fix the unit tests static analysis

--- tests/unit/node/MathJaxTest.php
+++ tests/unit/node/MathJaxTest.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace Elabftw\Services;
 
+use Elabftw\Exceptions\FilesystemErrorException;
 use Elabftw\Exceptions\ProcessFailedException;
 use Elabftw\Models\Experiments;
 use Elabftw\Models\Users;
@@ -17,14 +18,17 @@ use function file_get_contents;
 
 class MathJaxTest extends \PHPUnit\Framework\TestCase
 {
+    private Experiments $Entity;
+
+    private MakePdf $MakePdf;
+
     protected function setUp(): void
     {
-        $this->Users = new Users(1);
-        $this->Entity = new Experiments($this->Users);
+        $this->Entity = new Experiments(new Users(1, 1));
         $this->MakePdf = new MakePdf($this->Entity, true);
     }
 
-    public function testNoMathJax()
+    public function testNoMathJax(): void
     {
         $mpdf = $this->MakePdf->initializeMpdf();
         $mathJaxHtml = '<html><head></head><body>No Tex here</body></html>';
@@ -33,20 +37,29 @@ class MathJaxTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($mathJaxHtml, $mathJaxOut);
     }
 
-    public function testMathJax()
+    public function testMathJax(): void
     {
         $mpdf = $this->MakePdf->initializeMpdf();
         $mathJaxHtml = file_get_contents(dirname(__DIR__, 2) . '/_data/mathjax.html');
+        if (!is_string($mathJaxHtml)) {
+            throw new FilesystemErrorException('Error loading test file!');
+        }
         $mathJaxOut = $this->MakePdf->tex2svg($mpdf, $mathJaxHtml);
         $mathJaxOutExpect = file_get_contents(dirname(__DIR__, 2) . '/_data/mathjax.out.html');
+        if (!is_string($mathJaxOutExpect)) {
+            throw new FilesystemErrorException('Error loading test file!');
+        }
 
         $this->assertEquals($mathJaxOutExpect, $mathJaxOut);
     }
 
-    public function testMathJaxFail()
+    public function testMathJaxFail(): void
     {
         $mpdf = $this->MakePdf->initializeMpdf();
         $mathJaxHtml = file_get_contents(dirname(__DIR__, 2) . '/_data/mathjaxFail.html');
+        if (!is_string($mathJaxHtml)) {
+            throw new FilesystemErrorException('Error loading test file!');
+        }
         $this->expectException(ProcessFailedException::class);
         $mathJaxOut = $this->MakePdf->tex2svg($mpdf, $mathJaxHtml);
     }

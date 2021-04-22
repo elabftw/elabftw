5271342f01a2af4900f7fd5251009b5c53739635
Marcel Bolten <marcel.bolten@msl.ubc.ca>
2021-04-22 00:02:17 -0700

    reload after tag addition

--- src/ts/tags.ts
+++ src/ts/tags.ts
@@ -80,6 +80,22 @@ $(document).ready(function() {
         TagC.create($('#createTagInputMultiple').val() as string, checked[index]['id']);
       });
       $(this).val('');
+
+      // we need to wait a bit for the ajax return
+      setTimeout(() => {
+        if ($('#itemList').length) {
+          $.get(window.location.href, function(data) {
+            $('#itemList').html($(data).find('#itemList').html());
+            $('#pinned-entities').html($(data).find('#pinned-entities').html());
+          }, 'html');
+        }
+        if ($('#item-table').length) {
+          $.get(window.location.href, function(data) {
+            $('#item-table').html($(data).find('#item-table').html());
+            $('#pinned-entities').html($(data).find('#pinned-entities').html());
+          }, 'html');
+        }
+      }, 100);
     }
   });
 

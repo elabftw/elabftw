cc60ad0b601bdeb420d7e1f8413dae66cffda6ff
Nicolas CARPi <nico-git@deltablot.email>
2021-07-05 18:10:48 +0200

    add quick filter links for users and categories in show mode
    also add an email column on team page. fix #2763

--- src/controllers/AbstractEntityController.php
+++ src/controllers/AbstractEntityController.php
@@ -69,6 +69,11 @@ abstract class AbstractEntityController implements ControllerInterface
         if (Check::id((int) $this->App->Request->query->get('cat')) !== false) {
             $this->Entity->addFilter('categoryt.id', $this->App->Request->query->getDigits('cat'));
         }
+        // OWNER (USERID) FILTER
+        if ($this->App->Request->query->has('owner')) {
+            $owner = (int) $this->App->Request->query->get('owner');
+            $this->Entity->addFilter('entity.userid', (string) $owner);
+        }
 
         // TAG FILTER
         if (!empty(((array) $this->App->Request->query->get('tags'))[0])) {

f4c51271459864a272c54597632f8c029bf9fb34
Nicolas CARPi <nico-git@deltablot.email>
2021-07-24 16:56:56 +0200

    fix cypress and try and add it to circleci

---
+++ tests/cypress/integration/admin_items_types_spec.js
@@ -0,0 +1,17 @@
+describe('Items Types', () => {
+  beforeEach(() => {
+    cy.login()
+  })
+
+  it('Create and edit an item type', () => {
+    const newname = 'New test item type'
+    cy.visit('/admin.php?tab=5')
+    cy.get('#itemsTypesName').type(newname)
+    cy.get('#itemsTypesCreate').click()
+    cy.get('input[id^=itemsTypesName_]').should('have.value', newname).then(($el) => {
+      // get the id of the newly created element so we can destroy it
+      const newid = $el.get(0).id.split('_').pop()
+      cy.get('button[data-id="' + newid + '"]').contains('Delete').click()
+    })
+  });
+});

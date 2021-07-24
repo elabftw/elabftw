f390db27803d69d03795c090355550cc0502df2c
Nicolas CARPi <nico-git@deltablot.email>
2021-07-25 00:36:14 +0200

    fix and upgrade cypress

--- tests/cypress/integration/admin_items_types_spec.js
+++ tests/cypress/integration/admin_items_types_spec.js
@@ -8,7 +8,7 @@ describe('Items Types', () => {
     cy.visit('/admin.php?tab=5')
     cy.get('#itemsTypesName').type(newname)
     cy.get('#itemsTypesCreate').click()
-    cy.get('input[id^=itemsTypesName_]').should('have.value', newname).then(($el) => {
+    cy.get('input[id^=itemsTypesName_]').last().should('have.value', newname).then(($el) => {
       // get the id of the newly created element so we can destroy it
       const newid = $el.get(0).id.split('_').pop()
       cy.get('button[data-id="' + newid + '"]').contains('Delete').click()

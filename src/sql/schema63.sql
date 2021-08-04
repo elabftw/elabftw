71598274d559c7768808c7748700fe48a817c8d2
Nicolas CARPi <nico-git@deltablot.email>
2021-08-05 00:15:23 +0200

    use int(10) in schema too

--- src/sql/schema63.sql
+++ src/sql/schema63.sql
@@ -1,6 +1,6 @@
 -- Schema 63
 START TRANSACTION;
-    CREATE TABLE `authfail` ( `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, `users_id` INT UNSIGNED NOT NULL , `attempt_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, `device_token` VARCHAR(255) NULL DEFAULT NULL, PRIMARY KEY (`id`),
+    CREATE TABLE `authfail` ( `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, `users_id` INT(10) UNSIGNED NOT NULL , `attempt_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, `device_token` VARCHAR(255) NULL DEFAULT NULL, PRIMARY KEY (`id`),
     KEY `fk_authfail_userid` (`users_id`),
     CONSTRAINT `fk_authfail_userid` FOREIGN KEY (`users_id`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE);
     ALTER TABLE `users` ADD `allow_untrusted` tinyint(1) UNSIGNED NOT NULL DEFAULT '1';

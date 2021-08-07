961f65358ed163449f194a6a241c5c2211c0dd5f
Nicolas CARPi <nico-git@deltablot.email>
2021-08-07 12:13:56 +0200

    remove the FK on authfail

--- src/sql/schema63.sql
+++ src/sql/schema63.sql
@@ -1,8 +1,7 @@
 -- Schema 63
 START TRANSACTION;
-    CREATE TABLE `authfail` ( `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, `users_id` INT(10) UNSIGNED NOT NULL , `attempt_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, `device_token` VARCHAR(255) NULL DEFAULT NULL, PRIMARY KEY (`id`),
-    KEY `fk_authfail_userid` (`users_id`),
-    CONSTRAINT `fk_authfail_userid` FOREIGN KEY (`users_id`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE);
+    -- No FK on users_id because it might be 0 if we cannot get a userid for the auth try (user doesn't exist)
+    CREATE TABLE `authfail` ( `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, `users_id` INT(10) UNSIGNED NOT NULL , `attempt_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, `device_token` VARCHAR(255) NULL DEFAULT NULL, PRIMARY KEY (`id`));
     ALTER TABLE `users` ADD `allow_untrusted` tinyint(1) UNSIGNED NOT NULL DEFAULT '1';
     ALTER TABLE `users` ADD `auth_lock_time` DATETIME NULL DEFAULT NULL;
     CREATE TABLE `lockout_devices` ( `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, `locked_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, `device_token` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));

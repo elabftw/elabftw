bdcd9c99b043365a8beb13c5cdb37e39154b92a1
Marcel <65481677+MarcelBolten@users.noreply.github.com>
2021-08-26 22:49:10 -0700

    return email attribute or empty string but not NULL (#2892)

--- src/sql/schema64.sql
+++ src/sql/schema64.sql
@@ -1,7 +1,7 @@
 -- Schema 64
 START TRANSACTION;
     ALTER TABLE `idps` ADD `email_attr` VARCHAR(255) NOT NULL, ADD `team_attr` VARCHAR(255) NULL DEFAULT NULL, ADD `fname_attr` VARCHAR(255) NULL DEFAULT NULL, ADD `lname_attr` VARCHAR(255) NULL DEFAULT NULL;
-    UPDATE `idps` SET idps.email_attr = (SELECT config.conf_value FROM config WHERE config.conf_name = 'saml_email');
+    UPDATE `idps` SET idps.email_attr = COALESCE((SELECT config.conf_value FROM config WHERE config.conf_name = 'saml_email'), '');
     UPDATE `idps` SET idps.team_attr = (SELECT config.conf_value FROM config WHERE config.conf_name = 'saml_team');
     UPDATE `idps` SET idps.fname_attr = (SELECT config.conf_value FROM config WHERE config.conf_name = 'saml_firstname');
     UPDATE `idps` SET idps.lname_attr = (SELECT config.conf_value FROM config WHERE config.conf_name = 'saml_lastname');

19d2995dba5c1b582225d77649c2f170f350851c
Nicolas CARPi <nico-git@deltablot.email>
2021-08-28 22:04:52 +0200

    simplify code in show.ts, use getEntity

--- src/ts/show.ts
+++ src/ts/show.ts
@@ -7,7 +7,7 @@
  */
 declare let key: any;
 declare let MathJax: any;
-import { getCheckedBoxes, insertParamAndReload, notif, reloadTagsAndLocks } from './misc';
+import { getCheckedBoxes, insertParamAndReload, notif, reloadTagsAndLocks, getEntity } from './misc';
 import { EntityType } from './interfaces';
 import 'bootstrap/js/src/modal.js';
 import i18next from 'i18next';
@@ -24,12 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
     return;
   }
 
-  let entityType = EntityType.Experiment;
-  if (about.type === 'items') {
-    entityType = EntityType.Item;
-  }
+  const entity = getEntity();
+  // PAGINATION
+  const offset = parseInt(about.offset);
+  const limit = parseInt(about.limit);
 
-  const EntityC = new EntityClass(entityType);
+  const EntityC = new EntityClass(entity.type);
 
   // CREATE EXPERIMENT or DATABASE item with shortcut
   key(document.getElementById('shortcuts').dataset.create, function() {
@@ -315,21 +315,13 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Add click listener and do action based on which element is clicked
-  document.querySelector('.real-container').addEventListener('click', (event) => {
+  document.querySelector('.real-container').addEventListener('click', event => {
     const el = (event.target as HTMLElement);
-    // PAGINATION
     // previous page
     if (el.matches('[data-action="previous-page"]')) {
-      const info = (document.querySelector('#info') as HTMLElement);
-      const offset = parseInt(info.dataset.offset);
-      const limit = parseInt(info.dataset.limit);
       insertParamAndReload('offset', offset - limit);
-
     // next page
     } else if (el.matches('[data-action="next-page"]')) {
-      const info = (document.querySelector('#info') as HTMLElement);
-      const offset = parseInt(info.dataset.offset);
-      const limit = parseInt(info.dataset.limit);
       insertParamAndReload('offset', offset + limit);
     // END PAGINATION
     }

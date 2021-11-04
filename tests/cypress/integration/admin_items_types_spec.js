a698bac8ba16ef145790c771948aee194a6662c1
Nicolas CARPi <nico-git@deltablot.email>
2021-11-04 13:54:59 +0100

    fix items types destroy and fix cypress tests

--- tests/cypress/integration/admin_items_types_spec.js
+++ tests/cypress/integration/admin_items_types_spec.js
@@ -3,15 +3,16 @@ describe('Items Types', () => {
     cy.login()
   })
 
-  it('Create and edit an item type', () => {
+  it('Create and delete an item type', () => {
     const newname = 'New test item type'
     cy.visit('/admin.php?tab=5')
-    cy.get('#itemsTypesName').type(newname)
-    cy.get('#itemsTypesCreate').click()
-    cy.get('input[id^=itemsTypesName_]').last().should('have.value', newname).then(($el) => {
-      // get the id of the newly created element so we can destroy it
-      const newid = $el.get(0).id.split('_').pop()
-      cy.get('button[data-id="' + newid + '"]').contains('Delete').click()
+    cy.window().then(win => {
+      cy.stub(win, 'prompt').returns(newname)
+      cy.get('[data-action="itemstypes-create"]').click()
     })
+    cy.url().should('contains', 'templateid')
+    cy.get('#itemsTypesName').should('have.value', newname)
+    cy.get('[data-action="itemstypes-destroy"]').click()
+    cy.url().should('contains', 'tab=5')
   });
 });

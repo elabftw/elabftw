d9701708c9d2941bb4414d83b8587c4a3c4d2be2
Nicolas CARPi <nico-git@deltablot.email>
2021-11-15 04:59:26 +0100

    make sure stamppass is not broken for update
    this might happen with some password managers (not sure, might also be a
    browser bug), gives error that data is too big (right truncated) and
    makes the update fail.

--- src/sql/schema67.sql
+++ src/sql/schema67.sql
@@ -10,6 +10,7 @@ START TRANSACTION;
     ALTER TABLE `teams` ADD `ts_override` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0';
     ALTER TABLE `teams` ADD `ts_authority` VARCHAR(255) NOT NULL DEFAULT 'dfn';
     ALTER TABLE `teams` CHANGE `stamplogin` `ts_login` VARCHAR(255) NULL DEFAULT NULL;
+    UPDATE `teams` SET `stamppass` = NULL;
     ALTER TABLE `teams` CHANGE `stamppass` `ts_password` VARCHAR(255) NULL DEFAULT NULL;
     ALTER TABLE `teams` CHANGE `stampprovider` `ts_url` VARCHAR(255) NULL DEFAULT NULL;
     ALTER TABLE `teams` CHANGE `stampcert` `ts_cert` VARCHAR(255) NULL DEFAULT NULL;

f010a13af2d09af91a288303dd1f0097c845227f
Nicolas CARPi <3043706+NicolasCARPi@users.noreply.github.com>
2022-01-25 13:40:57 +0100

    Changes to the advanced query code (#3216)
    * WIP merge extended search and advanced search
    
    * continuation of the simplification/rework of advanced serch
    
    * rename extended to extendedArea for the id of the textarea
    * change placeholder and default text of extended search -> provide
      defaults and example at the same time
    * extendedFeedback div doesn't need to be inside an if, nor the
      aria-describedby (consequence of previous)
    * remove the "Yourself" value for owner filter now that it's default
    * move the visibility select on the same line as status and rating
    * get rid of the title and body inputs
    
    * rename the author filter and fix ts bug
    
    * fix issue with tags (and array params)
    
    query->get() should be query->all() for tags or any other array value
    
    * make date helper simpler to work with and continue to remove duplicated
    
    code
    
    * add a clear action for filters and remove more dead code
    
    * add the dateOperator and make a better regexp
    
    * remove debug stuff, fix placeholder, and add lock and timestamp helpers
    
    * rework the toggle-next logic/markup
    
    * make data-action toggle-next elements visibly clickable (cursor:pointer) in css
    * use chevron-circle to denote a togglable-next element
    * use normal chevron for the go back to links
    * remove the custom localStorage function and use localStorage.clear() instead
    * clear up some code from todolist.ts now that the behavior is common
    
    * hide favtags opener from search page
    
    * add box to advanced search togglable things
    
    * fix favtags opener showing up on search page if search is done

--- src/ts/search.ts
+++ src/ts/search.ts
@@ -16,52 +16,67 @@ document.addEventListener('DOMContentLoaded', () => {
     window.location.hash = '#anchor';
   }
 
-  function toogleSearchMode(): void {
-    const searchMode = localStorage.getItem('isExtendedSearchMode');
-    if (!searchMode || searchMode === '0') {
-      localStorage.setItem('isExtendedSearchMode', '1');
-    }
-    if (searchMode === '1') {
-      localStorage.setItem('isExtendedSearchMode', '0');
-    }
-    // Clear search input to avoid interference between modes
-    window.location.href = 'search.php';
-  }
-
-  // Add click listener and do action based on which element is clicked
-  document.getElementById('toggleSearchMode').addEventListener('click', event => {
-    const el = (event.target as HTMLElement);
-    // Toggle search mode
-    if (el.matches('[data-action="toggle-search-mode"]')) {
-      toogleSearchMode();
-      // Block button during transition
-      el.style.pointerEvents = 'none';
-    }
-  });
+  const extendedArea = (document.getElementById('extendedArea') as HTMLTextAreaElement);
 
   // Submit form with ctrl+enter from within textarea
-  document.getElementById('extended').addEventListener('keydown', event => {
+  extendedArea.addEventListener('keydown', event => {
     if ((event.keyCode == 10 || event.keyCode == 13) && (event.ctrlKey || event.metaKey)) {
       (document.getElementById('searchButton') as HTMLButtonElement).click();
     }
   });
 
-  // Unblock button after transition. Couldn't make it work without jQuery
-  $('.collapseExtendedSearch').on('hidden.bs.collapse', () => {
-    document.getElementById('toggleSearchMode').style.pointerEvents = 'auto';
-  });
-
-  if (localStorage.getItem('isExtendedSearchMode') === '1') {
-    $('.collapseExtendedSearch').collapse('toggle');
-    document.getElementById('toggleSearchMode').innerHTML = 'Switch to Default Search';
-
-    // Owner has to be set to team in extended search
-    (document.getElementById('searchonly') as HTMLSelectElement).value = '0';
-
-    // Only keep Experiments and Database entries in 'searchin' select
-    const searchin = document.getElementById('searchin') as HTMLSelectElement;
-    const keep = Array.from(searchin.children).slice(0,3);
-    searchin.replaceChildren(...keep);
-    searchin.selectedIndex = 0;
+  // a filter helper can be a select or an input (for date), so we need a function to get its value
+  function getFilterValueFromElement(element: HTMLElement): string {
+    if (element instanceof HTMLSelectElement) {
+      if (element.options[element.selectedIndex].dataset.action === 'clear') {
+        return '';
+      }
+      return `${element.options[element.selectedIndex].innerText}`;
+    }
+    if (element instanceof HTMLInputElement) {
+      // a cleared date input will be empty
+      if (element.value === '') {
+        return '';
+      }
+      // for the date, get the operator
+      let operator = '';
+      if (element.dataset.filter === 'date') {
+        const operatorSelect = document.getElementById('dateOperator') as HTMLSelectElement;
+        operator = operatorSelect.options[operatorSelect.selectedIndex].value;
+      }
+      return operator + element.value;
+    }
+    return '😶';
   }
+
+  // add a change event listener to all elements that helps constructing the query string
+  document.querySelectorAll('.filterHelper').forEach(el => {
+    el.addEventListener('change', event => {
+      const elem = event.currentTarget as HTMLElement;
+      const curVal = extendedArea.value;
+
+      // look if the filter key already exists in the extendedArea
+      // paste the regex on regex101.com to understand it, note that here \ need to be escaped
+      const regex = new RegExp(elem.dataset.filter + ':(\\w+|\\d|"[\\w\\s+]+"|([=><!,]?=?)?(\\d{4}[\\-\\.\\/,]\\d{2}[\\-\\.\\/,]\\d{2}))');
+      const found = curVal.match(regex);
+      // don't add quotes unless we need them (space exists)
+      let quotes = '';
+      const filterValue = getFilterValueFromElement(elem);
+      if (filterValue.includes(' ')) {
+        quotes = '"';
+      }
+      // default value is clearing everything
+      let filter = '';
+      // but if we have a correct value, we add the filter
+      if (filterValue !== '') {
+        filter = `${elem.dataset.filter}:${quotes}${filterValue}${quotes}`;
+      }
+
+      if (found) {
+        extendedArea.value = curVal.replace(regex, filter);
+      } else {
+        extendedArea.value = extendedArea.value + ' ' + filter;
+      }
+    });
+  });
 });
--- web/search.php
+++ web/search.php
@@ -10,7 +10,6 @@ declare(strict_types=1);
 
 namespace Elabftw\Elabftw;
 
-use function count;
 use Elabftw\Controllers\SearchController;
 use Elabftw\Exceptions\ImproperActionException;
 use Elabftw\Models\Experiments;
@@ -23,7 +22,6 @@ use Elabftw\Services\AdvancedSearchQuery;
 use Elabftw\Services\AdvancedSearchQuery\Visitors\VisitorParameters;
 use Elabftw\Services\Check;
 use Elabftw\Services\Filter;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * The search page
@@ -58,84 +56,21 @@ if ($Request->query->get('type') === 'experiments') {
     $Entity = $Database;
 }
 
-// line below is for testing
-$whereClauseDevLog = '';
+// EXTENDED SEARCH
+// default input for extendedArea
+$extended = 'author:"' . $Entity->Users->userData['fullname'] . '" ';
+$extendedError = '';
 
-// With variable variables
-// $extended = $extendedError = $title = $titleError = $body = $bodyError = '';
-// $extendedSearches = array(
-    // array('extended', false),
-    // array('title', true),
-    // array('body', true),
-// );
-// foreach ($extendedSearches as $extendedSearche) {
-    // ${$extendedSearche[0]} = '';
-    // ${$extendedSearche[0] . 'Error'} = '';
-    // if ($Request->query->has($extendedSearche[0]) && !empty($Request->query->get($extendedSearche[0]))) {
-        // ${$extendedSearche[0]} = trim($Request->query->get($extendedSearche[0]));
-        // $column = $extendedSearche[1] ? $extendedSearche[0] : '';
-        // $advancedQuery = new AdvancedSearchQuery(${$extendedSearche[0]}, new VisitorParameters($Entity->type, $visibilityArr, $column));
-        // $whereClause = $advancedQuery->getWhereClause();
-        // line below is for testing
-        // $whereClauseDevLog .= print_r($whereClause, true);
-        // if ($whereClause) {
-            // $Entity->addToExtendedFilter($whereClause['where'], $whereClause['bindValues']);
-        // }
+if ($Request->query->has('extended') && !empty($Request->query->get('extended'))) {
+    $extended = trim((string) $Request->query->get('extended'));
 
-        // ${$extendedSearche[0] . 'Error'} = $advancedQuery->getException() ?: '';
-    // }
-// }
-
-// With function
-function prepareExtendedSearch(
-    Request $Request,
-    Experiments | Items $Entity,
-    string $type,
-    array $visibilityArr,
-    string $column = ''
-): array {
-    if ($Request->query->has($type) && !empty($Request->query->get($type))) {
-        $userInput = trim((string) $Request->query->get($type));
-
-        $advancedQuery = new AdvancedSearchQuery($userInput, new VisitorParameters($Entity->type, $visibilityArr, $column));
-        $whereClause = $advancedQuery->getWhereClause();
-        if ($whereClause) {
-            $Entity->addToExtendedFilter($whereClause['where'], $whereClause['bindValues']);
-        }
-
-        $searchFeedback = $advancedQuery->getException();
+    $advancedQuery = new AdvancedSearchQuery($extended, new VisitorParameters($Entity->type, $visibilityArr));
+    $whereClause = $advancedQuery->getWhereClause();
+    if ($whereClause) {
+        $Entity->addToExtendedFilter($whereClause['where'], $whereClause['bindValues']);
     }
-    return array(
-        $userInput ?? '',
-        $searchFeedback ?? '',
-        // line below is for testing
-        print_r($whereClause ?? '', true),
-    );
-}
 
-[$extended, $extendedError, $tmp] = prepareExtendedSearch($Request, $Entity, 'extended', $visibilityArr);
-$whereClauseDevLog .= $tmp;
-[$title, $titleError, $tmp] = prepareExtendedSearch($Request, $Entity, 'title', $visibilityArr, 'title');
-$whereClauseDevLog .= $tmp;
-[$body, $bodyError, $tmp] = prepareExtendedSearch($Request, $Entity, 'body', $visibilityArr, 'body');
-$whereClauseDevLog .= $tmp;
-
-// VISIBILITY
-$vis = '';
-if ($Request->query->has('vis') && !empty($Request->query->get('vis'))) {
-    $vis = Check::visibility($Request->query->get('vis'));
-}
-
-// FROM
-$from = '';
-if ($Request->query->has('from') && !empty($Request->query->get('from'))) {
-    $from = Filter::kdate($Request->query->get('from'));
-}
-
-// TO
-$to = '';
-if ($Request->query->has('to') && !empty($Request->query->get('to'))) {
-    $to = Filter::kdate($Request->query->get('to'));
+    $extendedError = $advancedQuery->getException();
 }
 
 // RENDER THE FIRST PART OF THE PAGE (search form)
@@ -143,21 +78,15 @@ $renderArr = array(
     'Request' => $Request,
     'Experiments' => $Experiments,
     'Database' => $Database,
-    'body' => $body,
-    'bodyError' => $bodyError,
     'categoryArr' => $categoryArr,
     'itemsTypesArr' => $itemsTypesArr,
     'tagsArr' => $tagsArr,
     'teamGroupsArr' => $teamGroupsArr,
-    'title' => $title,
-    'titleError' => $titleError,
     'statusArr' => $statusArr,
     'usersArr' => $usersArr,
     'visibilityArr' => $visibilityArr,
     'extended' => $extended,
     'extendedError' => $extendedError,
-    // line below is for testing
-    'whereClause' => $whereClauseDevLog,
 );
 echo $App->render('search.html', $renderArr);
 
@@ -165,84 +94,21 @@ echo $App->render('search.html', $renderArr);
  * Here the search begins
  * If there is a search, there will be get parameters, so this is our main switch
  */
-if ($Request->query->count() > 0 && $bodyError === '' && $titleError === '' && $extendedError === '') {
-
-    // STATUS
-    $status = '';
-    if (Check::id((int) $Request->query->get('status')) !== false) {
-        $status = $Request->query->get('status');
-    }
-
-    // RATING
-    $rating = null;
-    $allowedRatings = array('null', '1', '2', '3', '4', '5');
-    if (in_array($Request->query->get('rating'), $allowedRatings, true)) {
-        $rating = $Request->query->get('rating');
-    }
-
+if ($Request->query->count() > 0 && $extendedError === '') {
     // PREPARE SQL query
-
     /////////////////////////////////////////////////////////////////
     if ($Request->query->has('type')) {
-        // Tag search
-        if (!empty($Request->query->get('tags'))) {
-            // get all the ids with that tag
-            $ids = $Entity->Tags->getIdFromTags($Request->query->get('tags'), (int) $App->Users->userData['team']);
-            if (count($ids) > 0) {
-                $Entity->idFilter = Tools::getIdFilterSql($ids);
-            }
-        }
-
-        // Visibility search
-        if (!empty($vis)) {
-            $Entity->addFilter('entity.canread', $vis);
-        }
-
-        // Date search
-        if (!empty($from) && !empty($to)) {
-            $Entity->dateFilter = " AND entity.date BETWEEN '$from' AND '$to'";
-        } elseif (!empty($from) && empty($to)) {
-            $Entity->dateFilter = " AND entity.date BETWEEN '$from' AND '99991212'";
-        } elseif (empty($from) && !empty($to)) {
-            $Entity->dateFilter = " AND entity.date BETWEEN '00000101' AND '$to'";
-        }
-
-        // Rating search
-        if (!empty($rating)) {
-            // rating is whitelisted here
-            $Entity->addFilter('entity.rating', $rating);
-        }
-
         // Metadata search
         if ($Request->query->get('metakey')) {
             $Entity->addMetadataFilter($Request->query->get('metakey'), $Request->query->get('metavalue'));
         }
 
-        if ($Request->query->get('type') === 'experiments') {
-
-            // USERID FILTER
-            if ($Request->query->has('owner')) {
-                $owner = $App->Users->userData['userid'];
-                if (Check::id((int) $Request->query->get('owner')) !== false) {
-                    $owner = $Request->query->get('owner');
-                }
-                // all the team is 0 as userid
-                if ($Request->query->get('owner') !== '0') {
-                    $Entity->addFilter('entity.userid', $owner);
-                }
-            }
-
-            // Status search
-            if (!empty($status)) {
-                $Entity->addFilter('entity.category', $status);
-            }
-        } else {
+        if ($Request->query->get('type') !== 'experiments') {
             // FILTER ON DATABASE ITEMS TYPES
             if (Check::id((int) $Request->query->get('type')) !== false) {
                 $Entity->addFilter('categoryt.id', $Request->query->get('type'));
             }
         }
-
 
         try {
             $Controller = new SearchController($App, $Entity);

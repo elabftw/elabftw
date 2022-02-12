33b903121852095c98cdcc5235d5cac49f55e9f2
Nicolas CARPi <nico-git@deltablot.email>
2022-02-12 15:29:14 +0100

    add qr pdf test

--- tests/unit/services/MakeQrPdfTest.php
+++ tests/unit/services/MakeQrPdfTest.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 /**
  * @author Nicolas CARPi <nico-git@deltablot.email>
- * @copyright 2012 Nicolas CARPi
+ * @copyright 2022 Nicolas CARPi
  * @see https://www.elabftw.net Official website
  * @license AGPL-3.0
  * @package elabftw
@@ -12,16 +12,16 @@ namespace Elabftw\Services;
 use Elabftw\Models\Experiments;
 use Elabftw\Models\Users;
 
-class MakePdfTest extends \PHPUnit\Framework\TestCase
+class MakeQrPdfTest extends \PHPUnit\Framework\TestCase
 {
-    private MakePdf $MakePdf;
+    private MakeQrPdf $MakePdf;
 
     protected function setUp(): void
     {
         $Entity = new Experiments(new Users(1, 1), 1);
         $Entity->canOrExplode('read');
         $MpdfProvider = new MpdfProvider('Toto');
-        $this->MakePdf = new MakePdf($MpdfProvider, $Entity);
+        $this->MakePdf = new MakeQrPdf($MpdfProvider, $Entity, array('1', '2', '3'));
     }
 
     public function testGetFileContent(): void
@@ -29,8 +29,8 @@ class MakePdfTest extends \PHPUnit\Framework\TestCase
         $this->assertIsString($this->MakePdf->getFileContent());
     }
 
-    public function testGetContentType(): void
+    public function testGetFileName(): void
     {
-        $this->assertEquals('application/pdf', $this->MakePdf->getContentType());
+        $this->assertEquals('qr-codes.elabftw.pdf', $this->MakePdf->getFileName());
     }
 }

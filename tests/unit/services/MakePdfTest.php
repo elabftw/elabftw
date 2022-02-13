4f329065843601803878f81428402f0d2e5113b0
Marcel Bolten <github@marcelbolten.de>
2022-02-12 21:43:16 -0800

    add comment

--- tests/unit/services/MakePdfTest.php
+++ tests/unit/services/MakePdfTest.php
@@ -20,6 +20,7 @@ class MakePdfTest extends \PHPUnit\Framework\TestCase
     {
         $Entity = new Experiments(new Users(1, 1), 1);
         $Entity->canOrExplode('read');
+        // add invalid tex macro to body to cover notification being created upon failing mathjax
         $Entity->entityData['body'] .= '\n<p>$ \someInvalidTexMacro $</p>';
         $MpdfProvider = new MpdfProvider('Toto');
         $this->MakePdf = new MakePdf($MpdfProvider, $Entity);

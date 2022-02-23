6f2faafa6d312116ea312990f73d1b17be4b6484
Marcel Bolten <github@marcelbolten.de>
2022-02-22 23:38:58 -0800

    update tests

--- tests/unit/services/MakeMultiPdfTest.php
+++ tests/unit/services/MakeMultiPdfTest.php
@@ -9,7 +9,9 @@
 
 namespace Elabftw\Services;
 
+use Elabftw\Elabftw\CreateUpload;
 use Elabftw\Models\Experiments;
+use Elabftw\Models\Uploads;
 use Elabftw\Models\Users;
 
 class MakeMultiPdfTest extends \PHPUnit\Framework\TestCase
@@ -19,9 +21,14 @@ class MakeMultiPdfTest extends \PHPUnit\Framework\TestCase
     protected function setUp(): void
     {
         $idArr = array('1', '2', '3');
-        $Entity = new Experiments(new Users(1, 1));
+        $Entity = new Experiments(new Users(1, 1), 1);
         // test >Append attached PDFs<
         $Entity->Users->userData['append_pdfs'] = true;
+        $Uploads = new Uploads($Entity);
+        // add a pdf
+        $Uploads->create($this->createUpload('digicert.pdf'));
+        // add a pdf with password -> cannot be appended
+        $Uploads->create($this->createUpload('with_password_123456.pdf'));
         $MpdfProvider = new MpdfProvider('Toto');
         $this->MakePdf = new MakeMultiPdf($MpdfProvider, $Entity, $idArr);
     }
@@ -34,5 +41,23 @@ class MakeMultiPdfTest extends \PHPUnit\Framework\TestCase
     public function testGetFileName(): void
     {
         $this->assertEquals('multientries.elabftw.pdf', $this->MakePdf->getFileName());
+    }
+
+    public function createUpload(string $fileName): CreateUpload
+    {
+        $params = $this->createMock(CreateUpload::class);
+        // this would be the real name of the file uploaded by user
+        $params->method('getFilename')->willReturn($fileName);
+        // and this corresponds to the temporary file created after upload
+        $tmpFilePath = '/tmp/phpELABFTW';
+        $params->method('getFilePath')->willReturn($tmpFilePath);
+        $fs = (new StorageFactory(StorageFactory::MEMORY))->getStorage()->getFs();
+        // write our temporary file as if it was uploaded by a user
+        $fs->createDirectory('tmp');
+        $fixturesFs = (new StorageFactory(StorageFactory::FIXTURES))->getStorage()->getFs();
+        $fs->write(basename($tmpFilePath), $fixturesFs->read($fileName));
+        // we use the same fs for source and storage because it's all in memory anyway
+        $params->method('getSourceFs')->willReturn($fs);
+        return $params;
     }
 }
--- tests/unit/services/MakePdfTest.php
+++ tests/unit/services/MakePdfTest.php
@@ -9,7 +9,9 @@
 
 namespace Elabftw\Services;
 
+use Elabftw\Elabftw\CreateUpload;
 use Elabftw\Models\Experiments;
+use Elabftw\Models\Uploads;
 use Elabftw\Models\Users;
 
 class MakePdfTest extends \PHPUnit\Framework\TestCase
@@ -24,6 +26,11 @@ class MakePdfTest extends \PHPUnit\Framework\TestCase
         $Entity->entityData['body'] .= '\n<p>$ \someInvalidTexMacro $</p>';
         // test >Append attached PDFs<
         $Entity->Users->userData['append_pdfs'] = true;
+        $Uploads = new Uploads($Entity);
+        // add a pdf
+        $Uploads->create($this->createUpload('digicert.pdf'));
+        // add a pdf with password -> cannot be appended
+        $Uploads->create($this->createUpload('with_password_123456.pdf'));
         $MpdfProvider = new MpdfProvider('Toto');
         $this->MakePdf = new MakePdf($MpdfProvider, $Entity);
     }
@@ -36,5 +43,23 @@ class MakePdfTest extends \PHPUnit\Framework\TestCase
     public function testGetContentType(): void
     {
         $this->assertEquals('application/pdf', $this->MakePdf->getContentType());
+    }
+
+    public function createUpload(string $fileName): CreateUpload
+    {
+        $params = $this->createMock(CreateUpload::class);
+        // this would be the real name of the file uploaded by user
+        $params->method('getFilename')->willReturn($fileName);
+        // and this corresponds to the temporary file created after upload
+        $tmpFilePath = '/tmp/phpELABFTW';
+        $params->method('getFilePath')->willReturn($tmpFilePath);
+        $fs = (new StorageFactory(StorageFactory::MEMORY))->getStorage()->getFs();
+        // write our temporary file as if it was uploaded by a user
+        $fs->createDirectory('tmp');
+        $fixturesFs = (new StorageFactory(StorageFactory::FIXTURES))->getStorage()->getFs();
+        $fs->write(basename($tmpFilePath), $fixturesFs->read($fileName));
+        // we use the same fs for source and storage because it's all in memory anyway
+        $params->method('getSourceFs')->willReturn($fs);
+        return $params;
     }
 }

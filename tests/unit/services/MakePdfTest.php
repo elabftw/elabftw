361da04a15540b1671533d8cd4f8744050e8b7f4
Marcel Bolten <github@marcelbolten.de>
2022-02-23 00:21:34 -0800

    no need for helper method

--- tests/unit/services/MakePdfTest.php
+++ tests/unit/services/MakePdfTest.php
@@ -26,9 +26,9 @@ class MakePdfTest extends \PHPUnit\Framework\TestCase
         // test >Append attached PDFs<
         $Entity->Users->userData['append_pdfs'] = true;
         // add a pdf
-        $Entity->Uploads->create($this->createUpload('digicert.pdf'));
+        $Entity->Uploads->create(new createUpload('digicert.pdf', dirname(__DIR__, 2) . '/_data/digicert.pdf'));
         // add a pdf with password -> cannot be appended
-        $Entity->Uploads->create($this->createUpload('with_password_123456.pdf'));
+        $Entity->Uploads->create(new createUpload('with_password_123456.pdf', dirname(__DIR__, 2) . '/_data/with_password_123456.pdf'));
         $MpdfProvider = new MpdfProvider('Toto');
         $this->MakePdf = new MakePdf($MpdfProvider, $Entity);
     }
@@ -41,23 +41,5 @@ class MakePdfTest extends \PHPUnit\Framework\TestCase
     public function testGetContentType(): void
     {
         $this->assertEquals('application/pdf', $this->MakePdf->getContentType());
-    }
-
-    public function createUpload(string $fileName): CreateUpload
-    {
-        $params = $this->createMock(CreateUpload::class);
-        // this would be the real name of the file uploaded by user
-        $params->method('getFilename')->willReturn($fileName);
-        // and this corresponds to the temporary file created after upload
-        $tmpFilePath = '/tmp/phpELABFTW';
-        $params->method('getFilePath')->willReturn($tmpFilePath);
-        $fs = (new StorageFactory(StorageFactory::MEMORY))->getStorage()->getFs();
-        // write our temporary file as if it was uploaded by a user
-        $fs->createDirectory('tmp');
-        $fixturesFs = (new StorageFactory(StorageFactory::FIXTURES))->getStorage()->getFs();
-        $fs->write(basename($tmpFilePath), $fixturesFs->read($fileName));
-        // we use the same fs for source and storage because it's all in memory anyway
-        $params->method('getSourceFs')->willReturn($fs);
-        return $params;
     }
 }

42017f184b4b7b0c1ad37ab1e23673ace3fc085c
Nicolas CARPi <nico-git@deltablot.email>
2022-03-25 16:48:01 +0100

    fix update process

--- src/sql/schema57.sql
+++ src/sql/schema57.sql
@@ -1,5 +1,8 @@
 -- Schema 57
 START TRANSACTION;
+    ALTER TABLE `users2teams` ADD COLUMN `id` int(10) UNSIGNED NOT NULL;
+    ALTER TABLE `users2teams` ADD PRIMARY KEY (`id`);
+    ALTER TABLE `users2teams` MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
     ALTER TABLE `users` ADD `display_mode` VARCHAR(2) NOT NULL DEFAULT 'it';
     ALTER TABLE `users` CHANGE `password` `password` VARCHAR(255) NULL DEFAULT NULL;
@@ -56,7 +59,6 @@ START TRANSACTION;
     ALTER TABLE `experiments_templates_revisions`
         ADD CONSTRAINT `fk_experiments_templates_revisions_experiments_templates_id` FOREIGN KEY (`item_id`) REFERENCES `experiments_templates`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
         ADD CONSTRAINT `fk_experiments_templates_revisions_users_userid` FOREIGN KEY (`userid`) REFERENCES `users`(`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
-    ALTER TABLE `users2teams` ADD `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
     ALTER TABLE `users2team_groups` ADD `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
     ALTER TABLE `tags2entity` ADD `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
 
--- src/sql/schema66.sql
+++ src/sql/schema66.sql
@@ -1,10 +1,8 @@
 -- Schema 66
 START TRANSACTION;
-    CREATE TABLE `favtags2users` IF NOT EXISTS (
-      `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    CREATE TABLE IF NOT EXISTS `favtags2users` (
       `users_id` int UNSIGNED NOT NULL,
-      `tags_id` int UNSIGNED NOT NULL,
-      PRIMARY KEY (`id`)
+      `tags_id` int UNSIGNED NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci;
     ALTER TABLE `favtags2users` ADD CONSTRAINT `fk_favtags2users_tags_id` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
     ALTER TABLE `favtags2users` ADD CONSTRAINT `fk_favtags2users_users_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;

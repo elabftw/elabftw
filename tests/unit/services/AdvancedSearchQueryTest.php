a0559b6bf6f3274bb48c3660c3b1c718093fe0fc
Marcel Bolten <github@marcelbolten.de>
2022-04-19 19:45:55 -0700

    add test

--- tests/unit/services/AdvancedSearchQueryTest.php
+++ tests/unit/services/AdvancedSearchQueryTest.php
@@ -54,6 +54,7 @@ class AdvancedSearchQueryTest extends \PHPUnit\Framework\TestCase
         $query .= ' timestamped: timestamped:true title:"very cool experiment" visibility:me';
         $query .= ' date:>2020.06,21 date:2020/06-21..20201231';
         $query .= ' group:"Group Name"';
+        $query .= ' attachment:"hello world"';
 
         $advancedSearchQuery = new AdvancedSearchQuery($query, new VisitorParameters(
             'experiments',

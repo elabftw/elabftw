b684efd74fcc3cbba0b947b0c97480a1a6e7c1c5
Nicolas CARPi <nico-git@deltablot.email>
2022-06-06 23:05:50 +0200

    forgot a file

---
+++ src/traits/CategoryTrait.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2022 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+namespace Elabftw\Traits;
+
+use Elabftw\Elabftw\Db;
+use Elabftw\Models\ItemsTypes;
+use PDO;
+
+/**
+ * For Status and ItemsTypes
+ */
+trait CategoryTrait
+{
+    protected Db $Db;
+
+    public function countEntities(): int
+    {
+        $sql = 'SELECT COUNT(id) FROM ' . $this->countableTable . ' WHERE category = :category';
+        $req = $this->Db->prepare($sql);
+        $req->bindParam(':category', $this->id, PDO::PARAM_INT);
+        $this->Db->execute($req);
+
+        return (int) $req->fetchColumn();
+    }
+}

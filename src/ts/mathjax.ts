120b9c7e95f99f237f539ee64c4c384716ad793a
Nicolas CARPi <nico-git@deltablot.email>
2022-06-11 01:18:14 +0200

    upgrade mathjax and the rest

--- src/ts/mathjax.ts
+++ src/ts/mathjax.ts
@@ -61,7 +61,7 @@ insert(
       ready: (): void => {
         MathJax.startup.defaultReady();
       },
-      pageReady(): void {
+      pageReady(): Promise<void> {
         const options = MathJax.startup.document.options;
         const BaseMathItem = options.MathItem;
         options.MathItem = class FixedMathItem extends BaseMathItem {

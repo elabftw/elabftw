dad67c15535817b7fc44fb8401d81a506cbfff9e
Nicolas CARPi <nico-git@deltablot.email>
2022-06-29 13:21:03 +0200

    allow style and class attributes on div and h{1,6}
    fix #3667

--- src/services/Filter.php
+++ src/services/Filter.php
@@ -134,7 +134,7 @@ class Filter
         // create base config for html5
         $config = HTMLPurifier_HTML5Config::createDefault();
         // allow only certain elements
-        $config->set('HTML.Allowed', 'div[class],br,p[class|style],sub,img[src|class|style|width|height],sup,strong,b,em,u,a[href],s,span[style],ul,li,ol,dl,dt,dd,blockquote,h1,h2,h3,h4,h5,h6,hr,table[style],tr[style],td[style|colspan|rowspan],th[style|colspan|rowspan],code,video,audio,pre[class],details,summary,figure,figcaption');
+        $config->set('HTML.Allowed', 'div[class|style],br,p[class|style],sub,img[src|class|style|width|height],sup,strong,b,em,u,a[href],s,span[style],ul,li,ol,dl,dt,dd,blockquote,h1[class|style],h2[class|style],h3[class|style],h4[class|style],h5[class|style],h6[class|style],hr,table[style],tr[style],td[style|colspan|rowspan],th[style|colspan|rowspan],code,video,audio,pre[class],details,summary,figure,figcaption');
         $config->set('HTML.TargetBlank', true);
         // configure the cache for htmlpurifier
         $tmpDir = FsTools::getCacheFolder('purifier');

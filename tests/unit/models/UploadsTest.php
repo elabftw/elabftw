2d7006a84bfd2b4f94ce9d9cc3c4e0a9b3655f43
Marcel Bolten <github@marcelbolten.de>
2022-07-11 00:23:05 -0700

    more to fix

--- tests/unit/models/UploadsTest.php
+++ tests/unit/models/UploadsTest.php
@@ -95,8 +95,11 @@ class UploadsTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($upArrBefore['comment'], $upArrNew['comment']);
 
         $Uploads->setId($id);
-        $upArrAfter = $Uploads->read(new ContentParams());
+        $upArrAfter = $Uploads->readAll();
+        $upArrAfter = array_filter($upArrAfter, function ($u) use ($id) {
+            return ((int) $u['id']) === $id;
+        });
 
-        $this->assertEquals($upArrAfter['state'], $Uploads::STATE_ARCHIVED);
+        $this->assertEquals((int) $upArrAfter['state'], $Uploads::STATE_ARCHIVED);
     }
 }

a4fb4c6ed011cf3a553bd99b1ea847e2b49a2c87
Nicolas CARPi <nico-git@deltablot.email>
2022-10-02 12:18:22 +0200

    don't assume code is without flaws :p

--- src/services/advancedSearchQuery/visitors/QueryBuilderVisitor.php
+++ src/services/advancedSearchQuery/visitors/QueryBuilderVisitor.php
@@ -101,7 +101,7 @@ class QueryBuilderVisitor implements Visitor
         // Tag and Metadata not implemented!
 
         // Call class methods dynamically to avoid many if statements.
-        // This works here flawless because the parser and the Fields enum define the list of fields.
+        // This works because the parser and the Fields enum define the list of fields.
         $method = 'visitField' . ucfirst($field->getFieldType()->value);
         return $this->$method($field->getValue(), $field->getAffix(), $parameters);
     }

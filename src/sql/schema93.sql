7af1c4ceb5020bdeb9908bfa2b83d198d32d9052
Nicolas CARPi <nico-git@deltablot.email>
2022-10-12 10:28:04 +0200

    set the experiments content_type to 2 for users with the use_markdown setting ON

--- src/sql/schema93.sql
+++ src/sql/schema93.sql
@@ -1,6 +1,8 @@
 -- Schema 93
 -- add content_type column to entities so we know what is inside (html or md)
 ALTER TABLE `experiments` ADD `content_type` TINYINT(1) NOT NULL DEFAULT 1;
+-- set the content_type to 2 for users with the use_markdown setting ON
+UPDATE `experiments` INNER JOIN `users` ON (experiments.userid = users.userid) SET experiments.content_type = 2 WHERE users.use_markdown = 1;
 ALTER TABLE `experiments_revisions` ADD `content_type` TINYINT(1) NOT NULL DEFAULT 1;
 ALTER TABLE `experiments_templates` ADD `content_type` TINYINT(1) NOT NULL DEFAULT 1;
 ALTER TABLE `experiments_templates_revisions` ADD `content_type` TINYINT(1) NOT NULL DEFAULT 1;

d45a9129f38715b45ed859a425b55a58bd728947
Nicolas CARPi <nico-git@deltablot.email>
2022-10-26 00:44:38 +0200

    fix ts error on ucp related to new template editor switch capability

--- src/ts/Editor.class.ts
+++ src/ts/Editor.class.ts
@@ -83,5 +83,8 @@ export class MdEditor extends Editor implements EditorInterface {
 }
 
 export function getEditor(): EditorInterface {
-  return document.getElementById('iHazEditor').dataset.editor === 'md' ? new MdEditor() : new TinyEditor();
+  if (document.getElementById('iHazEditor')) {
+    return document.getElementById('iHazEditor').dataset.editor === 'md' ? new MdEditor() : new TinyEditor();
+  }
+  return new MdEditor();
 }

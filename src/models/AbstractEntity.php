985462a4f93e34dd7d32917cff5a82bce71e562e
Nicolas CARPi <nico-git@deltablot.email>
2022-11-02 01:02:29 +0100

    move extended query code to a private function

--- src/models/AbstractEntity.php
+++ src/models/AbstractEntity.php
@@ -240,17 +240,8 @@ abstract class AbstractEntity implements RestInterface
 
         // extended search
         if ($displayParams->searchType === 'extended') {
-            $advancedQuery = new AdvancedSearchQuery($displayParams->extendedQuery, new VisitorParameters($this->type, $this->TeamGroups->getVisibilityList(), $this->TeamGroups->readGroupsWithUsersFromUser()));
-            $whereClause = $advancedQuery->getWhereClause();
-            if ($whereClause) {
-                $this->addToExtendedFilter($whereClause['where'], $whereClause['bindValues']);
-            }
-            $searchError = $advancedQuery->getException();
-            if (!empty($searchError)) {
-                throw new ImproperActionException('Error with extended search: ' . $searchError);
-            }
+            $this->processExtendedQuery($displayParams->extendedQuery);
         }
-
 
         // metadata filter (this will just be empty if we're not doing anything metadata related)
         $sql .= implode(' ', $this->metadataFilter);
@@ -817,6 +808,19 @@ abstract class AbstractEntity implements RestInterface
     {
         foreach ($this->extendedValues as $bindValue) {
             $req->bindValue($bindValue['param'], $bindValue['value'], $bindValue['type']);
+        }
+    }
+
+    private function processExtendedQuery(string $extendedQuery): void
+    {
+        $advancedQuery = new AdvancedSearchQuery($extendedQuery, new VisitorParameters($this->type, $this->TeamGroups->getVisibilityList(), $this->TeamGroups->readGroupsWithUsersFromUser()));
+        $whereClause = $advancedQuery->getWhereClause();
+        if ($whereClause) {
+            $this->addToExtendedFilter($whereClause['where'], $whereClause['bindValues']);
+        }
+        $searchError = $advancedQuery->getException();
+        if (!empty($searchError)) {
+            throw new ImproperActionException('Error with extended search: ' . $searchError);
         }
     }
 }

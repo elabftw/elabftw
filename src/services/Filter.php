469ad01c84bcd044bb45a3ceae601a9ed7fcbccc
Nicolas CARPi <nico-git@deltablot.email>
2022-12-09 01:41:56 +0100

    add blank_on_duplicate attribute to extra_fields in metadata
    fix #3919

--- src/services/Filter.php
+++ src/services/Filter.php
@@ -136,6 +136,20 @@ class Filter
         return str_replace('?', '_', mb_convert_encoding(self::forFilesystem($input), 'ASCII', 'UTF-8'));
     }
 
+    public static function blankExtraFieldsValueOnDuplicate(?string $metadata): ?string
+    {
+        if ($metadata === null) {
+            return null;
+        }
+        $decoded = json_decode($metadata, true, 512, JSON_THROW_ON_ERROR);
+        foreach ($decoded['extra_fields'] as &$field) {
+            if ($field['blank_value_on_duplicate'] === true) {
+                $field['value'] = '';
+            }
+        }
+        return json_encode($decoded, JSON_THROW_ON_ERROR);
+    }
+
     /**
      * Sanitize body with a list of allowed html tags.
      *

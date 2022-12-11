3d0b8b578256b6b5ac72dd9c059d205351e35606
Nicolas CARPi <nico-git@deltablot.email>
2022-12-11 14:32:28 +0100

    extract getReadSqlBeforeWhere to a class

--- src/models/AbstractEntity.php
+++ src/models/AbstractEntity.php
@@ -15,6 +15,7 @@ use Elabftw\Elabftw\ContentParams;
 use Elabftw\Elabftw\Db;
 use Elabftw\Elabftw\DisplayParams;
 use Elabftw\Elabftw\EntityParams;
+use Elabftw\Elabftw\EntitySqlBuilder;
 use Elabftw\Elabftw\Permissions;
 use Elabftw\Elabftw\Tools;
 use Elabftw\Enums\Action;
@@ -89,13 +90,13 @@ abstract class AbstractEntity implements RestInterface
 
     public bool $isReadOnly = false;
 
+    // inserted in sql
+    public array $extendedValues = array();
+
     protected TeamGroups $TeamGroups;
 
     // inserted in sql
     private string $extendedFilter = '';
-
-    // inserted in sql
-    private array $extendedValues = array();
 
     /**
      * Constructor
@@ -217,7 +218,8 @@ abstract class AbstractEntity implements RestInterface
             $this->processExtendedQuery(trim($displayParams->query . ' ' . $displayParams->extendedQuery));
         }
 
-        $sql = $this->getReadSqlBeforeWhere($extended, $extended, $displayParams->hasMetadataSearch);
+        $EntitySqlBuilder = new EntitySqlBuilder($this);
+        $sql = $EntitySqlBuilder->getReadSqlBeforeWhere($extended, $extended, $displayParams->hasMetadataSearch);
         $teamgroupsOfUser = array_column($this->TeamGroups->readGroupsFromUser(), 'id');
 
         // first where is the state
@@ -526,7 +528,8 @@ abstract class AbstractEntity implements RestInterface
         if ($this->id === null) {
             throw new IllegalActionException('No id was set!');
         }
-        $sql = $this->getReadSqlBeforeWhere(true, true, true);
+        $EntitySqlBuilder = new EntitySqlBuilder($this);
+        $sql = $EntitySqlBuilder->getReadSqlBeforeWhere(true, true, true);
 
         $sql .= sprintf(' WHERE entity.id = %d', $this->id);
 
@@ -640,143 +643,6 @@ abstract class AbstractEntity implements RestInterface
                 throw new ImproperActionException(_('Write permissions enforced by admin. Aborting change.'));
             }
         }
-    }
-
-    /**
-     * Get the SQL string for read before the WHERE
-     *
-     * @param bool $getTags do we get the tags too?
-     * @param bool $fullSelect select all the columns of entity
-     * @phan-suppress PhanPluginPrintfVariableFormatString
-     */
-    private function getReadSqlBeforeWhere(bool $getTags = true, bool $fullSelect = false, bool $includeMetadata = false): string
-    {
-        if ($fullSelect) {
-            // get all the columns of entity table, we add a literal string for the page that can be used by the mention tinymce plugin code
-            $select = 'SELECT DISTINCT entity.*,
-                GROUP_CONCAT(DISTINCT (team_events.experiment IS NOT NULL OR team_events.item_link IS NOT NULL)) AS is_bound,
-                GROUP_CONCAT(DISTINCT team_events.item) AS events_item_id,
-                GROUP_CONCAT(DISTINCT team_events.id) AS events_id,
-                "' . $this->page .'" AS page,
-                "' . $this->type.'" AS type,';
-        } else {
-            // only get the columns interesting for show mode
-            $select = 'SELECT DISTINCT entity.id,
-                entity.title,
-                entity.date,
-                entity.category,
-                entity.rating,
-                entity.userid,
-                entity.locked,
-                entity.canread,
-                entity.canwrite,
-                entity.modified_at,';
-            // don't include the metadata column unless we really need it
-            // see https://stackoverflow.com/questions/29575835/error-1038-out-of-sort-memory-consider-increasing-sort-buffer-size
-            if ($includeMetadata) {
-                $select .= 'entity.metadata,';
-            }
-            // only include columns (created_at, locked_at, timestamped_at,) if actually searching for it
-            if (!empty(array_column($this->extendedValues, 'additional_columns'))) {
-                $select .= implode(', ', array_unique(array_column($this->extendedValues, 'additional_columns'))) . ',';
-            }
-        }
-        $select .= "uploads.up_item_id, uploads.has_attachment,
-            SUBSTRING_INDEX(GROUP_CONCAT(stepst.next_step ORDER BY steps_ordering, steps_id SEPARATOR '|'), '|', 1) AS next_step,
-            categoryt.id AS category_id,
-            categoryt.title AS category,
-            categoryt.color,
-            users.firstname, users.lastname, users.orcid,
-            CONCAT(users.firstname, ' ', users.lastname) AS fullname,
-            commentst.recent_comment,
-            (commentst.recent_comment IS NOT NULL) AS has_comment";
-
-        $tagsSelect = '';
-        $tagsJoin = '';
-        if ($getTags) {
-            $tagsSelect = ", GROUP_CONCAT(DISTINCT tags.tag ORDER BY tags.id SEPARATOR '|') as tags, GROUP_CONCAT(DISTINCT tags.id) as tags_id";
-            $tagsJoin = 'LEFT JOIN tags2entity ON (entity.id = tags2entity.item_id AND tags2entity.item_type = \'%1$s\') LEFT JOIN tags ON (tags2entity.tag_id = tags.id)';
-        }
-
-        // only include columns if actually searching for comments/filenames
-        $searchAttachments = '';
-        if (!empty(array_column($this->extendedValues, 'searchAttachments'))) {
-            $searchAttachments = ',
-                GROUP_CONCAT(uploads.comment) AS comments,
-                GROUP_CONCAT(uploads.real_name) AS real_names';
-        }
-
-        $uploadsJoin = 'LEFT JOIN (
-            SELECT uploads.item_id AS up_item_id,
-                (uploads.item_id IS NOT NULL) AS has_attachment,
-                uploads.type' . $searchAttachments . '
-            FROM uploads
-            GROUP BY uploads.item_id, uploads.type)
-            AS uploads
-            ON (uploads.up_item_id = entity.id AND uploads.type = \'%1$s\')';
-
-        $usersJoin = 'LEFT JOIN users ON (entity.userid = users.userid)';
-        $teamJoin = sprintf(
-            'LEFT JOIN users2teams ON (users2teams.users_id = users.userid AND users2teams.teams_id = %s)',
-            $this->Users->userData['team']
-        );
-
-        $categoryTable = $this->type === 'experiments' ? 'status' : 'items_types';
-        $categoryJoin = 'LEFT JOIN ' . $categoryTable . ' AS categoryt ON (categoryt.id = entity.category)';
-
-        $commentsJoin = 'LEFT JOIN (
-            SELECT MAX(
-                %1$s_comments.created_at) AS recent_comment,
-                %1$s_comments.item_id
-                FROM %1$s_comments GROUP BY %1$s_comments.item_id
-            ) AS commentst
-            ON (commentst.item_id = entity.id)';
-        $stepsJoin = 'LEFT JOIN (
-            SELECT %1$s_steps.item_id AS steps_item_id,
-            %1$s_steps.body AS next_step,
-            %1$s_steps.ordering AS steps_ordering,
-            %1$s_steps.id AS steps_id,
-            %1$s_steps.finished AS finished
-            FROM %1$s_steps)
-            AS stepst ON (
-            entity.id = steps_item_id
-            AND stepst.finished = 0)';
-        $linksJoin = 'LEFT JOIN %1$s_links AS linkst ON (linkst.item_id = entity.id)';
-
-
-        $from = 'FROM %1$s AS entity';
-
-        if ($this instanceof Experiments) {
-            $select .= ', entity.timestamped';
-            $eventsColumn = 'experiment';
-        } elseif ($this instanceof Items) {
-            $select .= ', categoryt.bookable';
-            $eventsColumn = 'item_link';
-        } else {
-            throw new IllegalActionException('Nope.');
-        }
-        $eventsJoin = '';
-        if ($fullSelect) {
-            $eventsJoin = 'LEFT JOIN team_events ON (team_events.' . $eventsColumn . ' = entity.id)';
-        }
-
-        $sqlArr = array(
-            $select,
-            $tagsSelect,
-            $from,
-            $categoryJoin,
-            $commentsJoin,
-            $tagsJoin,
-            $eventsJoin,
-            $stepsJoin,
-            $linksJoin,
-            $usersJoin,
-            $teamJoin,
-            $uploadsJoin,
-        );
-
-        // replace all %1$s by 'experiments' or 'items'
-        return sprintf(implode(' ', $sqlArr), $this->type);
     }
 
     private function bindExtendedValues(PDOStatement $req): void

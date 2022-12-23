f78507ad1cf1cde8c4028edaa8372b8f6cb92435
Nicolas CARPi <nico-git@deltablot.email>
2022-12-23 03:16:44 +0100

    fix the unfinished steps

--- src/classes/Permissions.php
+++ src/classes/Permissions.php
@@ -194,7 +194,7 @@ class Permissions
 
         // it's not our entity, our last chance is to be admin in the same team as owner
         // also make sure that it's not in "useronly" mode
-        if ($this->Users->userData['is_admin'] && $this->canwrite['useronly'] === false) {
+        if ($this->Users->userData['is_admin'] && $this->canwrite['base'] !== BasePermissions::UserOnly->value) {
             // if it's an item (has team attribute), we need to be logged in in same team
             if (isset($this->item['team'])) {
                 if ($this->item['team'] === $this->Users->userData['team']) {

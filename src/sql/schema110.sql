e59c512b7fb67adb651261db3aa21d7efb8de858
Marcel Bolten <github@marcelbolten.de>
2023-01-07 22:40:30 -0800

    avoid problem during tags2entity update - clean up potential orphan tag ids - remove potential duplicate entries

--- src/sql/schema110.sql
+++ src/sql/schema110.sql
@@ -1,7 +1,23 @@
 -- schema 110
-ALTER TABLE `tags2entity` DROP `id`;
-ALTER TABLE `tags2entity`
-  ADD PRIMARY KEY (`item_id`, `tag_id`, `item_type`);
-ALTER TABLE `tags2entity`
-  ADD CONSTRAINT `fk_tags2entity_tags_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- tags2entity remove row if tag does not exists
+DELETE `tags2entity`
+  FROM `tags2entity`
+  LEFT JOIN tags
+    ON (`tags2entity`.`tag_id` = `tags`.`id`)
+  WHERE `tags`.`id` IS NULL;
+-- tags2entity remove duplicates, triplicates, ...
+CREATE TABLE `tmp` LIKE `tags2entity`;
+-- tmp change PRIMARY KEY
+ALTER TABLE `tmp` DROP `id`;
+ALTER TABLE `tmp` ADD PRIMARY KEY (`item_id`, `tag_id`, `item_type`);
+-- tags2entity add constraints
+ALTER TABLE `tmp`
+  ADD CONSTRAINT `fk_tags2entity_tags_id`
+    FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+-- Copy data to tmp, ignore duplicates
+INSERT IGNORE INTO `tmp` SELECT `item_id`, `tag_id`, `item_type` FROM `tags2entity`;
+-- Drop original and rename tmp
+DROP TABLE `tags2entity`;
+RENAME TABLE `tmp` TO `tags2entity`;
 UPDATE config SET conf_value = 110 WHERE conf_name = 'schema';

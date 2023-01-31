a5d1fd81ccd21748e9250fe49b1144a5aa37c55e
Nicolas CARPi <nico-git@deltablot.email>
2023-02-01 00:01:58 +0100

    improve and fix the test

--- tests/unit/classes/TwigFiltersTest.php
+++ tests/unit/classes/TwigFiltersTest.php
@@ -19,8 +19,47 @@ class TwigFiltersTest extends \PHPUnit\Framework\TestCase
 
     public function testFormatMetadata(): void
     {
-        $metadataJson = '{"extra_fields":{"foo":{"type":"text","value":"bar","description":"buzz"}}}';
-        $expected = '<h4>foo</h4><h5>buzz</h5><p>bar</p>';
+        $metadataJson = '{
+          "extra_fields": {
+            "url default": {
+              "type": "url",
+              "value": "https://example.com",
+              "position": 6
+            },
+            "url current tab": {
+              "type": "url",
+              "value": "https://example.com",
+              "open_in_current_tab": true,
+              "position": 5
+            },
+            "last one": {
+              "type": "text",
+              "value": "last content",
+              "position": 42,
+              "description": "last position"
+            },
+            "first one": {
+              "type": "text",
+              "value": "first",
+              "position": 1
+            },
+            "second one": {
+              "type": "text",
+              "value": "second",
+              "position": 2
+            },
+            "unchecked checkbox": {
+              "type": "checkbox",
+              "value": "",
+              "position": 4
+            },
+            "checked checkbox": {
+              "type": "checkbox",
+              "value": "on"
+            }
+          }
+        }';
+        $expected = '<h4 class="m-0">first one</h4><p>first</p><h4 class="m-0">second one</h4><p>second</p><h4 class="m-0">unchecked checkbox</h4><p><input class="d-block" disabled type="checkbox" ></p><h4 class="m-0">url current tab</h4><p><a href="https://example.com" >https://example.com</a></p><h4 class="m-0">url default</h4><p><a href="https://example.com" target="_blank" rel="noopener">https://example.com</a></p><h4 class="m-0">last one</h4><h5>last position</h5><p>last content</p><h4 class="m-0">checked checkbox</h4><p><input class="d-block" disabled type="checkbox" checked></p>';
         $this->assertEquals($expected, TwigFilters::formatMetadata($metadataJson));
     }
 }

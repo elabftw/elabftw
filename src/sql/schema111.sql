d3095cc627d435a51b6fd56ce74e8e301974da06
Nicolas CARPi <3043706+NicolasCARPi@users.noreply.github.com>
2023-02-03 15:32:58 +0100

    add team level announcement. fix #4065 (#4115)

--- src/sql/schema111.sql
+++ src/sql/schema111.sql
@@ -1,3 +1,4 @@
 -- schema 111
 INSERT INTO config (conf_name, conf_value) VALUES ('smtp_verify_cert', '1');
+ALTER TABLE `teams` ADD `announcement` VARCHAR(255) NULL DEFAULT NULL;
 UPDATE config SET conf_value = 111 WHERE conf_name = 'schema';

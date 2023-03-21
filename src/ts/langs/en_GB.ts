c1691fdd45a7fe8ca7d9aa9b5470c304dc31460e
Nicolas CARPi <nico-git@deltablot.email>
2023-03-21 03:42:36 +0100

    rework the json editor

--- src/ts/langs/en_GB.ts
+++ src/ts/langs/en_GB.ts
@@ -5,7 +5,7 @@ const t = {
     "cancel": "Cancel",
     "comment-add": "Add a comment",
     "dropzone-upload-area": "Drop files here to upload",
-    "editing-metadata": "Editing metadata",
+    "editing-metadata": "You are currently editing the metadata attached to this entry.",
     "entity-default-title": "Untitled",
     "entity-delete-warning": "The selected item(s) and all associated data will be permanently deleted. This cannot be undone. Are you sure?",
     "extra-fields": "Extra fields",

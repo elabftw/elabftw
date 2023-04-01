b1d5cda15a4d3abaff9ca5d25a335ce2e303d593
Nicolas CARPi <nico-git@deltablot.email>
2023-04-01 22:42:27 +0200

    finish the idp api test

--- tests/apiv2/IdpCest.php
+++ tests/apiv2/IdpCest.php
@@ -24,4 +24,49 @@ class IdpCest
         $I->seeResponseCodeIs(HttpCode::OK); // 200
         $I->seeResponseIsJson();
     }
+
+    public function getIdp(ApiTester $I)
+    {
+        $I->wantTo('Get an idp');
+        $I->sendGET('/idps/1');
+        $I->seeResponseCodeIs(HttpCode::OK); // 200
+        $I->seeResponseIsJson();
+    }
+
+    public function createIdp(ApiTester $I)
+    {
+        $I->wantTo('Create an idp');
+        $I->sendPOST('/idps', array(
+            'name' => 'apitest',
+            'entityid' => 'https://idp.example.com',
+            'sso_url' => 'https://idp.example.com/saml/sso',
+            'sso_binding' => 'u',
+            'slo_url' => 'https://idp.example.com/saml/slo',
+            'slo_binding' => 'u',
+            'x509' => 'a',
+            'x509_new' => 'a',
+            'email_attr' => 'mail',
+            'team_attr' => '',
+            'fname_attr' => 'givenname',
+            'lname_attr' => 'sn',
+        ));
+        $I->seeResponseCodeIs(HttpCode::CREATED);
+    }
+
+    public function updateIdp(ApiTester $I)
+    {
+        $I->wantTo('Update an idp');
+        $I->sendPATCH('/idps/2', array(
+            'name' => 'apitest updated',
+        ));
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+    }
+
+    public function deletedIdp(ApiTester $I)
+    {
+        $I->wantTo('Delete an idp');
+        $I->sendDELETE('/idps/2');
+        $I->seeResponseCodeIs(HttpCode::NO_CONTENT);
+    }
 }

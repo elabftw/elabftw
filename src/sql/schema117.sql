1c311647a167a5c8a386f89b749792809b81a11c
Nicolas CARPi <nico-git@deltablot.email>
2023-04-09 21:30:04 +0200

    add default value to usergroup_old

--- src/sql/schema117.sql
+++ src/sql/schema117.sql
@@ -6,4 +6,6 @@ UPDATE `users2teams` JOIN `users` ON (users.userid = users2teams.users_id) SET u
 ALTER TABLE users RENAME COLUMN usergroup TO usergroup_old;
 ALTER TABLE `users` ADD `is_sysadmin` TINYINT UNSIGNED NOT NULL DEFAULT 0;
 UPDATE `users` SET is_sysadmin = 1 WHERE usergroup_old = 1;
+-- add a default value for that column
+ALTER TABLE `users` CHANGE `usergroup_old` `usergroup_old` TINYINT UNSIGNED NOT NULL DEFAULT '4';
 UPDATE config SET conf_value = 117 WHERE conf_name = 'schema';

24848e778e1bd54c9c57e4b9914f4404bf84f575
Nicolas CARPi <nico-git@deltablot.email>
2023-04-25 00:12:13 +0200

    fix schema120-down.sql
    avoid error "BLOB/TEXT column 'userid' used in key specification without a key length"
    by dropping the keys before changing the column type

--- src/sql/schema120-down.sql
+++ src/sql/schema120-down.sql
@@ -1,8 +1,8 @@
 -- revert schema 120
--- change column type int to text
-ALTER TABLE `uploads` MODIFY `userid` text NOT NULL;
 -- remove keys/constraints
 ALTER TABLE `uploads`
   DROP KEY `idx_uploads_item_id_type`,
   DROP KEY `fk_uploads_users_userid`;
+-- change column type int to text
+ALTER TABLE `uploads` MODIFY `userid` text NOT NULL;
 UPDATE config SET conf_value = 119 WHERE conf_name = 'schema';

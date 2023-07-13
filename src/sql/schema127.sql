6ec2ed4177cf8cf91c3187262bef88ea7f15d1a7
Nicolas CARPi <nico-git@deltablot.email>
2023-07-14 00:17:46 +0200

    make old bookable items bookable again

--- src/sql/schema127.sql
+++ src/sql/schema127.sql
@@ -7,6 +7,7 @@ ALTER TABLE `items` ADD `book_max_slots` INT UNSIGNED NOT NULL DEFAULT 0;
 ALTER TABLE `items` ADD `book_can_overlap` TINYINT UNSIGNED NOT NULL DEFAULT 1;
 ALTER TABLE `items` ADD `book_is_cancellable` TINYINT UNSIGNED NOT NULL DEFAULT 1;
 ALTER TABLE `items` ADD `book_cancel_minutes` INT UNSIGNED NOT NULL DEFAULT 0;
+UPDATE items JOIN items_types ON items.category = items_types.id SET items.is_bookable = items_types.bookable WHERE items_types.bookable = 1;
 ALTER TABLE `items_types` CHANGE `bookable` `bookable_old` TINYINT UNSIGNED DEFAULT 0;
 
 UPDATE config SET conf_value = 127 WHERE conf_name = 'schema';

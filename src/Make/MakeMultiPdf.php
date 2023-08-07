50948aedf8399e8dd81c8b0c1a557a75bfd6ed5b
Nicolas CARPi <nico-git@deltablot.email>
2023-08-07 04:48:02 +0200

    improve make multipdf code by using a child class

---
+++ src/Make/MakeMultiPdf.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2023 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+namespace Elabftw\Make;
+
+use DateTimeImmutable;
+use Elabftw\Interfaces\MpdfProviderInterface;
+use Elabftw\Models\AbstractEntity;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Create a pdf from several Entities
+ */
+class MakeMultiPdf extends MakePdf
+{
+    public function __construct(protected LoggerInterface $log, MpdfProviderInterface $mpdfProvider, AbstractEntity $entity, protected array $entityIdArr = array())
+    {
+        parent::__construct($log, $mpdfProvider, $entity, $this->entityIdArr);
+    }
+
+    public function getFileName(): string
+    {
+        return sprintf('%s-elabftw-export.pdf', (new DateTimeImmutable())->format('Y-m-d'));
+    }
+
+    protected function getTitle(): string
+    {
+        return _('Multientry eLabFTW PDF');
+    }
+
+    // we don't add keywords to multi pdf
+    protected function getKeywords(): string
+    {
+        return '';
+    }
+}

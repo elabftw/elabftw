a98c5d7ee4bf0e417becb1bd4d460d11d483944a
Marcel Bolten <github@marcelbolten.de>
2023-08-19 08:17:56 -0700

    try to make test less flaky

--- tests/cypress/integration/admin_items_types.cy.js
+++ tests/cypress/integration/admin_items_types.cy.js
@@ -1,30 +1,35 @@
 describe('Items Types', () => {
   beforeEach(() => {
-    cy.login()
-    cy.enableCodeCoverage(Cypress.currentTest.titlePath[0])
-  })
-  const newname = 'New test item type'
+    cy.login();
+    cy.enableCodeCoverage(Cypress.currentTest.titlePath[0]);
+  });
+  const newname = 'New test item type';
 
-  it('Create an item type', () => {
-    cy.visit('/admin.php?tab=5')
+  it('Create and delete an item type', () => {
+    cy.visit('/admin.php?tab=5');
     cy.intercept('POST', '/api/v2/items_types', req => {
       req.on('before:response', res => {
-          expect(res.statusCode).to.equal(201)
-          expect(res.headers.location).to.include('templateid=10')
-      })
-    })
+          expect(res.statusCode).to.equal(201);
+          expect(res.headers.location).to.include('templateid=10');
+      });
+    }).as('create');
     cy.window().then(win => {
-      cy.stub(win, 'prompt').returns(newname)
-      cy.get('[data-action="itemstypes-create"]').click()
-    })
-    cy.visit('/admin.php?tab=5&templateid=10')
-    cy.get('#itemsTypesName').should('have.value', newname)
-  });
+      cy.stub(win, 'prompt').returns(newname);
+      cy.get('[data-action="itemstypes-create"]').click();
+      cy.wait('@create').then(() => {
+        cy.get('#itemsTypesName').should('have.value', newname);
+      });
+    });
 
-  it('Delete an item type', () => {
-    cy.visit('/admin.php?tab=5&templateid=10')
-    cy.get('[data-action="itemstypes-destroy"]').click()
-    cy.url().should('contains', 'tab=5')
-    cy.get('[data-table="items_types"]').get('#itemstypes_10').should('not.exist');
+    // Delete
+    cy.window().then(win => {
+      cy.intercept('DELETE', '/api/v2/items_types/*').as('delete');
+      cy.stub(win, 'confirm').returns(true);
+      cy.get('[data-action="itemstypes-destroy"]').click();
+      cy.wait('@delete').then(() => {
+        cy.location('search').should('contain', 'tab=5').should('not.contain', 'templateid=10');
+        cy.get('[data-table="items_types"]').get('#itemstypes_10').should('not.exist');
+      });
+    });
   });
 });

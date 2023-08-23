38b944853c3d0fd625d8e3cee41b7b009fd759c6
Marcel Bolten <github@marcelbolten.de>
2023-08-23 13:43:51 -0700

    more cypress tests on search page

--- tests/cypress/integration/search.cy.js
+++ tests/cypress/integration/search.cy.js
@@ -1,11 +1,22 @@
-describe('Search', () => {
+describe('Search page', () => {
   beforeEach(() => {
     cy.login();
     cy.enableCodeCoverage(Cypress.currentTest.titlePath[0]);
   });
 
-  it('Show search page', () => {
+  it('shows important elements', () => {
     cy.visit('/search.php');
     cy.get('h1#pageTitle').should('have.text', 'Advanced search');
+    cy.get('table#itemList').should('not.exist');
+  });
+
+  it('lists results', () => {
+    cy.visit('/search.php?type=experiments&q=author%3A%25&');
+    cy.get('h2').should('have.text', 'Results');
+    cy.get('table#itemList').should('be.visible');
+
+    cy.visit('/search.php?type=1&q=author%3A%25&');
+    cy.get('h2').should('have.text', 'Results');
+    cy.get('table#itemList').should('be.visible');
   });
 });

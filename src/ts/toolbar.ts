692317b51ff957f51b81a4ac78fa85e4c9c4ce7a
Marcel Bolten <github@marcelbolten.de>
2023-09-07 00:00:29 -0700

    fix html violations

--- src/ts/toolbar.ts
+++ src/ts/toolbar.ts
@@ -70,8 +70,13 @@ document.addEventListener('DOMContentLoaded', () => {
         if (['/team.php', '/ucp.php'].includes(window.location.pathname) && !urlParams.has('templateid')) {
           window.location.replace('?tab=3');
         } else {
-          document.getElementById('toggle-pin-icon').classList.toggle('color-weak');
-          ['bgnd-gray', 'hl-hover-gray'].forEach(cl => document.getElementById('toggle-pin-icon-div').classList.toggle(cl));
+          const tplTable = document.getElementById('tplTable');
+          tplTable.querySelectorAll('[data-toggle-pin-icon]').forEach(el => {
+            el.classList.toggle('color-weak');
+          });
+          tplTable.querySelectorAll('[data-action="toggle-pin"]').forEach(el => {
+            ['bgnd-gray', 'hl-hover-gray'].forEach(cl => el.classList.toggle(cl));
+          });
         }
       });
 

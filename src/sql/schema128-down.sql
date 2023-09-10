9de2eeb8852ddee7995fea79deb9ccf51a0f29ae
Nicolas CARPi <nico-git@deltablot.email>
2023-09-10 18:16:36 +0200

    fix schema down

--- src/sql/schema128-down.sql
+++ src/sql/schema128-down.sql
@@ -4,6 +4,7 @@ RENAME TABLE `experiments_status` TO `status`;
 DROP TABLE IF EXISTS `items_status`;
 ALTER TABLE `items` DROP COLUMN `status`;
 ALTER TABLE `experiments` DROP COLUMN `category`;
-ALTER TABLE `experiments` CHANGE `status` `category` INT UNSIGNED NOT NULL;
+-- recreate it as nullable to avoid fk issues
+ALTER TABLE `experiments` CHANGE `status` `category` INT UNSIGNED NULL DEFAULT NULL;
 DROP TABLE IF EXISTS `experiments_categories`;
 UPDATE config SET conf_value = 127 WHERE conf_name = 'schema';

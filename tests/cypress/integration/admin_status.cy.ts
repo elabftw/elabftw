5cb15c804d59e90e23c0213c058f2062f3e49ee3
Nicolas CARPi <nico-git@deltablot.email>
2023-09-20 21:47:53 +0200

    update cypress

--- tests/cypress/integration/admin_status.cy.ts
+++ tests/cypress/integration/admin_status.cy.ts
@@ -8,7 +8,8 @@ describe('Status in admin panel', () => {
     const newname = 'Cypress :: New status name';
     cy.visit('/admin.php?tab=5');
     cy.get('[data-target="createexperiments_statusModal"]').click();
-    cy.get('#createexperiments_statusName').type(newname);
+    // the wait is necessary or it doesn't have the time to type all
+    cy.get('#createexperiments_statusName').wait(500).type(newname);
     cy.get('[data-action="create-statuslike"]').filter(':visible').click();
     cy.get('#overlay').should('be.visible').should('contain', 'Saved');
     cy.get('[data-action="destroy-status"][data-target="experiments_status"]').last().click();

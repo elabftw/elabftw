246d5c8e78e920df1d2c04e1eb761f0c45f317b1
Nicolas CARPi <nico-git@deltablot.email>
2023-09-20 22:41:06 +0200

    make experiments status cypress test work even if status out of order

--- tests/cypress/integration/admin_status.cy.ts
+++ tests/cypress/integration/admin_status.cy.ts
@@ -8,11 +8,22 @@ describe('Status in admin panel', () => {
     const newname = 'Cypress :: New status name';
     cy.visit('/admin.php?tab=5');
     cy.get('[data-target="createexperiments_statusModal"]').click();
+    cy.get('#createexperiments_statusModal').should('be.visible');
     // the wait is necessary or it doesn't have the time to type all
     cy.get('#createexperiments_statusName').wait(500).type(newname);
     cy.get('[data-action="create-statuslike"]').filter(':visible').click();
-    cy.get('#overlay').should('be.visible').should('contain', 'Saved');
-    cy.get('[data-action="destroy-status"][data-target="experiments_status"]').last().click();
-    cy.get('#overlay').should('be.visible').should('contain', 'Saved');
+    // need to wait for it to appear
+    cy.get('#overlay').should('be.visible').should('contain', 'Saved').wait(400);
+    cy.get('[data-action="destroy-status"][data-target="experiments_status"]').then(allBtn => {
+      const elementWithHighestDataId = Array.from(allBtn).reduce((prev, current) => {
+        const prevDataId = parseInt(prev.dataset.id);
+        const currentDataId = parseInt(current.dataset.id);
+        return prevDataId > currentDataId ? prev : current;
+      });
+      const id = elementWithHighestDataId.dataset.id;
+      cy.get(`[data-action="destroy-status"][data-target="experiments_status"][data-id="${id}"]`).click();
+      // this doesn't work so we get the element instead
+      //cy.wrap(elementWithHighestDataId).click();
+    });
   });
 });

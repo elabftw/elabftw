15faa780e9f27841b62a102481af65cc545e5edb
Nicolas CARPi <nico-git@deltablot.email>
2023-09-23 02:46:32 +0200

    fix cypress tests

--- tests/cypress/integration/admin_status.cy.ts
+++ tests/cypress/integration/admin_status.cy.ts
@@ -14,14 +14,14 @@ describe('Status in admin panel', () => {
     cy.get('[data-action="create-statuslike"]').filter(':visible').click();
     // need to wait for it to appear
     cy.get('#overlay').should('be.visible').should('contain', 'Saved').wait(400);
-    cy.get('[data-action="destroy-status"][data-target="experiments_status"]').then(allBtn => {
+    cy.get('[data-action="destroy-catstat"][data-target="experiments_status"]').then(allBtn => {
       const elementWithHighestDataId = Array.from(allBtn).reduce((prev, current) => {
         const prevDataId = parseInt(prev.dataset.id);
         const currentDataId = parseInt(current.dataset.id);
         return prevDataId > currentDataId ? prev : current;
       });
       const id = elementWithHighestDataId.dataset.id;
-      cy.get(`[data-action="destroy-status"][data-target="experiments_status"][data-id="${id}"]`).click();
+      cy.get(`[data-action="destroy-catstat"][data-target="experiments_status"][data-id="${id}"]`).click();
       // this doesn't work so we get the element instead
       //cy.wrap(elementWithHighestDataId).click();
     });

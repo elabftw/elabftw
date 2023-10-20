a4e0c9a68a31dd3d8cc2e01a5be4c006561f8b9e
Marcel Bolten <github@marcelbolten.de>
2023-10-20 00:18:23 -0700

    add more team.php tests

--- tests/cypress/integration/team.cy.ts
+++ tests/cypress/integration/team.cy.ts
@@ -11,6 +11,11 @@ describe('Team', () => {
     cy.get('h1#pageTitle').should('have.text', 'Team');
     cy.get('[data-tabtarget="1"]').should('have.class', 'selected');
 
+    // Scheduler with selected item
+    cy.visit('/team.php?tab=1&item=1');
+    cy.get('[data-tabtarget="1"]').should('have.class', 'selected');
+    cy.get('[data-action="remove-param-reload"][data-target="item"]').should('exist');
+
     // Members
     cy.visit('/team.php?tab=2');
     cy.get('[data-tabtarget="2"]').should('have.class', 'selected');
@@ -20,7 +25,12 @@ describe('Team', () => {
     cy.visit('/team.php?tab=3');
     cy.get('[data-tabtarget="3"]').should('have.class', 'selected');
     cy.get('h3.section-title').should('contain.text', 'Experiments Templates');
+    cy.get('#tplTable').should('exist');
 
+    // View individual template
+    cy.visit('/team.php?tab=3&mode=view&templateid=1');
+    cy.get('#templatesDiv').should('exist');
+    
     // Send email
     cy.visit('/team.php?tab=4');
     cy.get('[data-tabtarget="4"]').should('have.class', 'selected');

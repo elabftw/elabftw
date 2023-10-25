dc33d19a575b8e0a5df0fdd2539ac38428b93fd3
Marcel Bolten <github@marcelbolten.de>
2023-10-25 08:05:15 -0700

    config cypress to use html-validate

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'cypress';
+import htmlvalidate from 'cypress-html-validate/plugin';
+import { Severity } from 'html-validate';
 
 export default defineConfig({
   fixturesFolder: 'tests/cypress/fixtures',
@@ -8,6 +10,28 @@ export default defineConfig({
   viewportHeight: 900,
   e2e: {
     setupNodeEvents(on, config) {
+      htmlvalidate.install(
+        on,
+        {
+          extends: [
+            'html-validate:document',
+            'html-validate:standard',
+            //'html-validate:a11y',
+          ],
+          rules: {
+            'heading-level': Severity.DISABLED, // should be WARN but does not work, TODO: fix violations
+            'require-sri': [Severity.ERROR, {
+              target: 'crossorigin',
+              exclude: ['https://elabtmp'], // this is treated as crossorigin so we exclude it
+            }],
+          },
+        },
+        {
+          exclude: [
+            '#sketcher_search_dialog', // chemdoodle 2d-sketcher
+          ],
+        },
+      );
       return require('./tests/cypress/plugins/index.ts')(on, config);
     },
     baseUrl: 'https://elab.local:3148',
@@ -18,4 +42,5 @@ export default defineConfig({
   defaultCommandTimeout: 8000,
   requestTimeout: 10000,
   responseTimeout: 130000,
+  taskTimeout: 300000,
 });

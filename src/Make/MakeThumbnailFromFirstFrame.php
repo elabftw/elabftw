af1957735c5e0f4903227bf2dc036e2114edb004
Nicolas CARPi <3043706+NicolasCARPi@users.noreply.github.com>
2023-11-01 22:12:54 +0100

    improve thumbnail generation (#4711)
    * allow using only the first frame of various filetypes (pdf, gif, ...)
    
    
    ---------
    
    Co-authored-by: Marcel <65481677+MarcelBolten@users.noreply.github.com>

---
+++ src/Make/MakeThumbnailFromFirstFrame.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2023 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+namespace Elabftw\Make;
+
+use League\Flysystem\Filesystem;
+
+/**
+ * Create a thumbnail from an image format with potentially multiple frames/pages.
+ * e.g. PDF, eps, tiff, gif, heic, postscript
+ */
+final class MakeThumbnailFromFirstFrame extends MakeThumbnail
+{
+    public function __construct(string $mime, string $filePath, string $longName, Filesystem $storageFs)
+    {
+        parent::__construct($mime, $filePath, $longName, $storageFs);
+        // Overwrite filePath: use [0] at the end of the file path to load only the first page/frame of the file into imagick
+        $this->filePath = $filePath . '[0]';
+    }
+}
---
+++ src/factories/MakeThumbnailFactory.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2023 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+namespace Elabftw\Factories;
+
+use Elabftw\Interfaces\MakeThumbnailInterface;
+use Elabftw\Make\MakeNullThumbnail;
+use Elabftw\Make\MakeThumbnail;
+use Elabftw\Make\MakeThumbnailFromFirstFrame;
+use League\Flysystem\Filesystem;
+
+/**
+ * Get a thumbnail maker depending on the mime type
+ */
+class MakeThumbnailFactory
+{
+    /**
+     * Do some sane white-listing. In theory, gmagick handles almost all image formats,
+     * but the processing of rarely used formats may be less tested/stable or may have security issues
+     * when adding new mime types take care of ambiguities:
+     * e.g. image/eps may be a valid application/postscript; image/bmp may also be image/x-bmp or
+     * image/x-ms-bmp
+     */
+    public static function getMaker(string $mime, string $filePath, string $longName, Filesystem $storageFs): MakeThumbnailInterface
+    {
+        return match ($mime) {
+            'application/pdf',
+            'application/postscript',
+            'image/gif',
+            'image/heic',
+            'image/tiff',
+            'image/x-eps' => new MakeThumbnailFromFirstFrame($mime, $filePath, $longName, $storageFs),
+            'image/png',
+            'image/jpeg',
+            'image/svg+xml' => new MakeThumbnail($mime, $filePath, $longName, $storageFs),
+            default => new MakeNullThumbnail($mime, $filePath, $longName, $storageFs),
+        };
+    }
+}

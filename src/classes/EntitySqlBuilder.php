914da1ff154af63cd27b8ce464446a3468e8cb8a
Nicolas CARPi <nico-git@deltablot.email>
2023-11-18 02:51:41 +0100

    fix team events appearing twice

--- src/classes/EntitySqlBuilder.php
+++ src/classes/EntitySqlBuilder.php
@@ -37,7 +37,7 @@ class EntitySqlBuilder
         if ($fullSelect) {
             // get all the columns of entity table, we add a literal string for the page that can be used by the mention tinymce plugin code
             $select = sprintf("SELECT DISTINCT entity.*,
-                GROUP_CONCAT(team_events.start ORDER BY team_events.start SEPARATOR '|') AS events_start,
+                GROUP_CONCAT(DISTINCT team_events.start ORDER BY team_events.start SEPARATOR '|') AS events_start,
                 '%s' AS page,
                 '%s' AS type,", $this->entity->page, $this->entity->type);
         } else {

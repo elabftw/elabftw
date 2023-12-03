71b29ce8d618b30b1e9220a135cecc2398cb8225
Nicolas CARPi <nico-git@deltablot.email>
2023-12-03 23:52:14 +0100

    add uploads:check command to add hash and or filesize to all files
    because old files (filesize missing) or big files (hash missing) can
    exist

---
+++ tests/unit/services/UploadsCheckerTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2023 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+namespace Elabftw\Services;
+
+class UploadsCheckerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testFix(): void
+    {
+        $UploadsChecker = new UploadsChecker();
+        $this->assertEquals(0, $UploadsChecker->fixNullFilesize());
+        $this->assertEquals(0, $UploadsChecker->fixNullHash());
+    }
+}

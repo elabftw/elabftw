03e561e552a21eac16aa536bd68671306ff6e203
Marcel Bolten <github@marcelbolten.de>
2023-12-05 23:03:02 -0800

    wait for pages to load aka loading-spinner to disappear

--- tests/cypress/integration/admin.cy.ts
+++ tests/cypress/integration/admin.cy.ts
@@ -7,11 +7,13 @@ describe('admin page', () => {
   it('has valid html', () => {
     cy.visit('/admin.php?');
     cy.get('h1#pageTitle').should('have.text', 'Admin panel');
+    cy.get('#loading-spinner').should('not.exist');
     cy.htmlvalidate();
 
     for (let i = 1; i <= 7; i++) {
       cy.visit(`/admin.php?tab=${i}`);
       cy.get(`[data-tabtarget="${i}"]`).should('have.class', 'selected');
+      cy.get('#loading-spinner').should('not.exist');
       cy.get(`div[data-tabcontent="${i}"]`).htmlvalidate();
     }
 
--- tests/cypress/integration/sysconfig.cy.ts
+++ tests/cypress/integration/sysconfig.cy.ts
@@ -7,11 +7,13 @@ describe('Sysconfig', () => {
   it('Show sysconfig page', () => {
     cy.visit('/sysconfig.php');
     cy.get('h1#pageTitle').should('have.text', 'eLabFTW Configuration');
+    cy.get('#loading-spinner').should('not.exist');
     cy.htmlvalidate();
 
     for (let i = 1; i <= 13; i++) {
       cy.visit(`/sysconfig.php?tab=${i}`);
       cy.get(`[data-tabtarget="${i}"]`).should('have.class', 'selected');
+      cy.get('#loading-spinner').should('not.exist');
       cy.get(`div[data-tabcontent="${i}"]`).htmlvalidate();
     }
 

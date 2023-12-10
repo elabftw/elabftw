3783f83149eeac4f0dc72e8bbb8bbafb4fc0afe1
Nicolas CARPi <nico-git@deltablot.email>
2023-12-10 23:34:30 +0100

    fix 136 revert

--- src/sql/schema136-down.sql
+++ src/sql/schema136-down.sql
@@ -1,3 +1,11 @@
 -- revert schema 136
 DROP TABLE IF EXISTS audit_logs;
+-- old structure from schema 134
+CREATE TABLE `audit_logs` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `body` TEXT NOT NULL,
+    `category` INT UNSIGNED NOT NULL,
+    `userid` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`));
 UPDATE config SET conf_value = 135 WHERE conf_name = 'schema';

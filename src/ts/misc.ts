543e23800c6860ab280c1abb6c18dd339e65fe72
Nicolas CARPi <nico-git@deltablot.email>
2024-01-18 19:20:24 +0100

    try and fix issue with multiple data-trigger listeners on the page
    now only re-attach to children of the reloaded element in
    reloadElement()

--- src/ts/misc.ts
+++ src/ts/misc.ts
@@ -93,8 +93,14 @@ function triggerHandler(event: Event, el: HTMLInputElement): void {
   });
 }
 
-export function listenTrigger(): void {
-  document.querySelectorAll('[data-trigger]').forEach((el: HTMLInputElement) => {
+export function listenTrigger(elementId: string = ''): void {
+  let elems: NodeList;
+  if (elementId) {
+    elems = document.getElementById(elementId).querySelectorAll('[data-trigger]');
+  } else {
+    elems = document.querySelectorAll('[data-trigger]');
+  }
+  elems.forEach((el: HTMLInputElement) => {
     // remove event first to avoid stacking them
     el.removeEventListener(el.dataset.trigger, event => { triggerHandler(event, el); });
     el.addEventListener(el.dataset.trigger, event => { triggerHandler(event, el); });
@@ -286,18 +292,6 @@ export async function reloadEntitiesShow(tag = ''): Promise<void | Response> {
   listenTrigger();
 }
 
-export async function reloadElements(elementIds: string[]): Promise<void> {
-  const html = await fetchCurrentPage();
-  elementIds.forEach(id => {
-    if (!document.getElementById(id)) {
-      console.error(`Could not find element with id ${id} to reload!`);
-      return;
-    }
-    document.getElementById(id).innerHTML = html.getElementById(id).innerHTML;
-    listenTrigger();
-  });
-}
-
 export async function reloadElement(elementId: string): Promise<void> {
   if (!document.getElementById(elementId)) {
     console.error(`Could not find element with id ${elementId} to reload!`);
@@ -305,7 +299,11 @@ export async function reloadElement(elementId: string): Promise<void> {
   }
   const html = await fetchCurrentPage();
   document.getElementById(elementId).innerHTML = html.getElementById(elementId).innerHTML;
-  listenTrigger();
+  listenTrigger(elementId);
+}
+
+export async function reloadElements(elementIds: string[]): Promise<void> {
+  elementIds.forEach(id => reloadElement(id));
 }
 
 /**

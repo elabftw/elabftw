33b61f447d64d0584ea57e7c61942c290a015a18
Nicolas CARPi <nico-git@deltablot.email>
2024-01-19 13:26:59 +0100

    add sysconfig setting to prevent admins from archiving users
    fix #4866

--- src/sql/schema140-down.sql
+++ src/sql/schema140-down.sql
@@ -1,5 +1,6 @@
 -- revert schema 140
 DELETE FROM config WHERE conf_name = 'emit_audit_logs';
 DELETE FROM config WHERE conf_name = 'chat_url';
+DELETE FROM config WHERE conf_name = 'admins_archive_users';
 ALTER TABLE `users` DROP COLUMN `last_seen_version`;
 UPDATE config SET conf_value = 139 WHERE conf_name = 'schema';

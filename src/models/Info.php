8dc9ad25f235d7e47d80197d0a85446450f58bd6
Nicolas CARPi <nico-git@deltablot.email>
2024-02-26 18:09:56 +0100

    feat: add ts_limit and entities_timestamped_count_last_30_days
    to api/v2/info

--- src/models/Info.php
+++ src/models/Info.php
@@ -50,6 +50,7 @@ class Info implements RestInterface
         $base = array(
             'elabftw_version' => App::INSTALLED_VERSION,
             'ts_balance' => (int) $Config->configArr['ts_balance'],
+            'ts_limit' => (int) $Config->configArr['ts_balance'],
         );
         return array_merge($base, $this->getAllStats());
     }
@@ -75,7 +76,8 @@ class Info implements RestInterface
         (SELECT COUNT(items.id) FROM items WHERE items.state = :state) AS items_count,
         (SELECT COUNT(teams.id) FROM teams) AS teams_count,
         (SELECT COUNT(experiments.id) FROM experiments WHERE experiments.state = :state) AS experiments_count,
-        (SELECT COUNT(experiments.id) FROM experiments WHERE experiments.state = :state AND experiments.timestamped = 1) AS experiments_timestamped_count';
+        (SELECT COUNT(experiments.id) FROM experiments WHERE experiments.state = :state AND experiments.timestamped = 1) AS experiments_timestamped_count,
+        (SELECT COUNT(id) FROM uploads WHERE comment LIKE "Timestamp archive%" = 1 AND created_at > (NOW() - INTERVAL 1 MONTH)) AS entities_timestamped_count_last_30_days';
         $req = $this->Db->prepare($sql);
         $req->bindValue(':state', State::Normal->value, PDO::PARAM_INT);
         $this->Db->execute($req);

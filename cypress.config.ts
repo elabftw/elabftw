619cb2c8fdc8f4d479c21a54d9cb793d31311f26
Nicolas CARPi <3043706+NicolasCARPi@users.noreply.github.com>
2024-03-12 16:20:30 +0100

    Fix checkbox linting error with cypress (#4981)
    While not a valid attribute for checkbox, the `autocomplete=off` attribute is necessary in firefox, or the browser will "remember" the state instead of displaying the correct html defined state. So it's best to keep it, as other browsers will simply ignore it.
    
    For reference: https://github.com/whatwg/html/issues/1894
    https://bugzilla.mozilla.org/show_bug.cgi?id=1752290

--- cypress.config.ts
+++ cypress.config.ts
@@ -22,6 +22,8 @@ export default defineConfig({
           ],
           rules: {
             'heading-level': Severity.DISABLED, // should be WARN but does not work, TODO: fix violations
+            // we keep autocomplete on checkboxes because it's important in firefox
+            'valid-autocomplete': Severity.DISABLED,
             'require-sri': [Severity.ERROR, {
               target: 'crossorigin',
               exclude: ['https://elabtmp'], // this is treated as crossorigin so we exclude it

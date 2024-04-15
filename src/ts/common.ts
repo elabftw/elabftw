d342dd3635e25995cc37fc27ba3392ac5f5652b9
Nicolas CARPi <nico-git@deltablot.email>
2024-04-15 03:53:47 +0200

    fix clash with pre-existing data-filter elements on search page

--- src/ts/common.ts
+++ src/ts/common.ts
@@ -203,15 +203,15 @@ document.addEventListener('DOMContentLoaded', () => {
    * Add listeners for filter bar on top of a table
    * The "filter" data attribute value is the id of the tbody element with rows to filter
    */
-  document.querySelectorAll('input[data-filter]').forEach((input: HTMLInputElement) => {
-    const target = document.getElementById(input.dataset.filter);
+  document.querySelectorAll('input[data-filter-target]').forEach((input: HTMLInputElement) => {
+    const target = document.getElementById(input.dataset.filterTarget);
     let targetType = 'tr';
     if (input.dataset.targetType === 'li') {
       targetType = 'li';
     }
     // FIRST LISTENER is to filter the rows
     input.addEventListener('keyup', () => {
-      target.querySelectorAll(`#${input.dataset.filter} ${targetType}`).forEach((row: HTMLTableRowElement|HTMLUListElement) => {
+      target.querySelectorAll(`#${input.dataset.filterTarget} ${targetType}`).forEach((row: HTMLTableRowElement|HTMLUListElement) => {
         // show or hide the row if it matches the query
         if (row.innerText.toLowerCase().includes(input.value)) {
           row.removeAttribute('hidden');
@@ -224,7 +224,7 @@ document.addEventListener('DOMContentLoaded', () => {
     input.nextElementSibling.addEventListener('click', () => {
       input.value = '';
       input.focus();
-      target.querySelectorAll(`#${input.dataset.filter} ${targetType}`).forEach((row: HTMLTableRowElement) => {
+      target.querySelectorAll(`#${input.dataset.filterTarget} ${targetType}`).forEach((row: HTMLTableRowElement) => {
         row.removeAttribute('hidden');
       });
     });

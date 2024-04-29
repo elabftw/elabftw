a438fb535aafa16bc35e780ce208a96c1c85d86b
Nicolas CARPi <nico-git@deltablot.email>
2024-04-29 20:30:07 +0200

    use a mutation observer to adjust the count for links/steps/uploads
    this saves us from having to do two requests to reload the link list +
    the title, and is a much better approach imho

--- src/ts/steps-links.ts
+++ src/ts/steps-links.ts
@@ -14,7 +14,6 @@ import { relativeMoment, makeSortableGreatAgain, reloadElement, reloadElements, 
 import { Action, Target } from './interfaces';
 import { Api } from './Apiv2.class';
 
-
 document.addEventListener('DOMContentLoaded', () => {
   if (!document.getElementById('info')) {
     return;
@@ -53,11 +52,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // DESTROY LINK
     } else if (el.matches('[data-action="destroy-link"]')) {
       if (confirm(i18next.t('link-delete-warning'))) {
-        ApiC.delete(`${entity.type}/${entity.id}/${el.dataset.endpoint}/${el.dataset.target}`).then(() => reloadElements(['linksDiv', 'linksExpDiv', 'linksExpDivTitle', 'linksDivTitle']));
+        ApiC.delete(`${entity.type}/${entity.id}/${el.dataset.endpoint}/${el.dataset.target}`)
+          .then(() => el.parentElement.parentElement.remove());
       }
     } else if (el.matches('[data-action="destroy-related-link"]')) {
       if (confirm(i18next.t('link-delete-warning'))) {
-        ApiC.delete(`${el.dataset.endpoint.split('_')[0]}/${el.dataset.target}/${entity.type}_links/${entity.id}`).then(() => reloadElements(['relatedItemsDiv', 'relatedExperimentsDiv']));
+        ApiC.delete(`${el.dataset.endpoint.split('_')[0]}/${el.dataset.target}/${entity.type}_links/${entity.id}`)
+          .then(() => el.parentElement.parentElement.remove());
       }
     }
   });
@@ -76,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const content = input.value;
     if (content.length > 0) {
       StepC.create(content).then(() => {
-        reloadElements(['stepsDiv', 'stepsDivTitle']).then(() => {
+        reloadElement('stepsDiv').then(() => {
           // clear input field
           input.value = '';
           input.focus();
@@ -145,7 +146,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (confirm(i18next.t('step-delete-warning'))) {
       const stepId = e.currentTarget.dataset.stepid;
       StepC.destroy(stepId).then(() => {
-        reloadElements(['stepsDiv', 'stepsDivTitle']).then(() => {
+        reloadElement('stepsDiv').then(() => {
           // keep to do list in sync
           $('#todo_step_' + stepId).parent().hide();
         });

a1087a7e5a51c34c9fd6879dcd0cf5e5e0da6f51
Marcel Bolten <github@marcelbolten.de>
2024-05-06 21:37:13 -0700

    add user constraints

---
+++ src/sql/schema150-down.sql
@@ -0,0 +1,8 @@
+-- revert schema 150
+ALTER TABLE `experiments_request_actions`
+  DROP FOREIGN KEY `fk_experiments_request_actions_requester_users_userid`,
+  DROP FOREIGN KEY `fk_experiments_request_actions_target_users_userid`;
+ALTER TABLE `items_request_actions`
+  DROP FOREIGN KEY `fk_items_request_actions_requester_users_userid`,
+  DROP FOREIGN KEY `fk_items_request_actions_target_users_userid`;
+UPDATE config SET conf_value = 149 WHERE conf_name = 'schema';

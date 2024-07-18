1e9e6b6497e2f34af6b5d7b52e1be454ef6a9944
anargam <anargam@protonmail.com>
2024-07-18 18:58:43 +0200

    Remove copy-paste error from schema update 157 (only relevant in schema 156).

--- src/sql/schema157.sql
+++ src/sql/schema157.sql
@@ -1,4 +1,3 @@
 -- schema 157
-UPDATE `users` SET `created_at` = FROM_UNIXTIME(`register_date`);
 UPDATE config SET conf_value = 0 WHERE conf_name = 'trust_imported_archives';
 UPDATE config SET conf_value = 157 WHERE conf_name = 'schema';

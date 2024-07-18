3ee3bf44bc8be248ae7dca55a5e9382fb82a3a34
anargam <anargam@protonmail.com>
2024-07-18 21:33:14 +0200

    Insert import settings into config table, don't update them.

--- src/sql/schema157.sql
+++ src/sql/schema157.sql
@@ -1,3 +1,3 @@
 -- schema 157
-UPDATE config SET conf_value = 0 WHERE conf_name = 'trust_imported_archives';
+INSERT INTO config (conf_name, conf_value) values ('trust_imported_archives', '0');
 UPDATE config SET conf_value = 157 WHERE conf_name = 'schema';

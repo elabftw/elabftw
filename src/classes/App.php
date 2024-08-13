bf763094f72db60ac775d398fd42f04e7b174ff6
Nicolas CARPi <nico-git@deltablot.email>
2024-08-13 18:15:18 +0200

    beta2

--- src/classes/App.php
+++ src/classes/App.php
@@ -46,7 +46,7 @@ class App
 {
     use TwigTrait;
 
-    public const string INSTALLED_VERSION = '5.1.0-beta';
+    public const string INSTALLED_VERSION = '5.1.0-beta2';
 
     // this version format is used to compare with last_seen_version of users
     // major is untouched, and minor and patch are padded with one 0 each

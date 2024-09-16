1d0ce0db7a4ac150a2be291925699b5ce46c7e70
Nicolas CARPi <nico-git@deltablot.email>
2024-09-16 20:33:34 +0200

    bug/minor: sign: allow signature with only read access

--- src/classes/App.php
+++ src/classes/App.php
@@ -47,7 +47,7 @@ class App
 {
     use TwigTrait;
 
-    public const string INSTALLED_VERSION = '5.1.5';
+    public const string INSTALLED_VERSION = '5.1.6';
 
     // this version format is used to compare with last_seen_version of users
     // major is untouched, and minor and patch are padded with one 0 each

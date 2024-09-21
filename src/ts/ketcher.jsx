d53fb56d14167b144083ce15166cbd13d9580732
Nicolas CARPi <nico-git@deltablot.email>
2024-09-22 00:03:06 +0200

    works with standalone

--- src/ts/ketcher.jsx
+++ src/ts/ketcher.jsx
@@ -1,8 +1,12 @@
-import { StandaloneStructServiceProvider } from 'ketcher-standalone';
+//import { StandaloneStructServiceProvider } from 'ketcher-standalone';
 import { Editor } from 'ketcher-react'; // Ensure you have this installed
 import "ketcher-react/dist/index.css";
+import { RemoteStructServiceProvider } from 'ketcher-core';
+const structServiceProvider = new RemoteStructServiceProvider(
+  'https://elab.local:3148/indigo/v2',
+);
 
-const structServiceProvider = new StandaloneStructServiceProvider();
+//const structServiceProvider = new StandaloneStructServiceProvider();
 
 const KetcherEditor = () => {
   return (

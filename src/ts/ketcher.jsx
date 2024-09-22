d845229b3b6945918d10a48b24a707727bb8856d
Nicolas CARPi <nico-git@deltablot.email>
2024-09-22 13:24:38 +0200

    use relative urls

--- src/ts/ketcher.jsx
+++ src/ts/ketcher.jsx
@@ -3,7 +3,7 @@ import { Editor } from 'ketcher-react'; // Ensure you have this installed
 import "ketcher-react/dist/index.css";
 import { RemoteStructServiceProvider } from 'ketcher-core';
 const structServiceProvider = new RemoteStructServiceProvider(
-  'https://elab.local:3148/indigo/v2',
+  '/indigo/v2',
 );
 
 //const structServiceProvider = new StandaloneStructServiceProvider();
@@ -12,7 +12,7 @@ const KetcherEditor = () => {
   return (
   <div className="ketcher-editor-container">
     <Editor
-      staticResourcesUrl={JSON.stringify('https://elab.local:3148')}
+      staticResourcesUrl={JSON.stringify('/')}
       structServiceProvider={structServiceProvider}
     />
   </div>

f46273d48234fa274026370cdfdba07b785ca245
Marcel Bolten <github@marcelbolten.de>
2024-09-27 13:18:45 -0700

    don't change old schemata

--- src/sql/schema166-down.sql
+++ src/sql/schema166-down.sql
@@ -1,3 +1,6 @@
 -- revert schema 166
--- nothing to do here; changes are done in schema151-down.sql as the backend changes happened between 151 and 152
+-- nothing to do here but could be:
+-- UPDATE `notifications`
+--   SET `body` = JSON_SET(`body`, '$.entity_page', REPLACE(`body`->>'$.entity_page', '.php', ''))
+--   WHERE `body`->>'$.entity_page' LIKE '%.php';
 UPDATE config SET conf_value = 165 WHERE conf_name = 'schema';

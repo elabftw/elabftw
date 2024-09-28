ab75dd7c001ec39c97a85e9ac774bd71e43decd5
Nicolas CARPi <nico-git@deltablot.email>
2024-09-28 14:54:08 +0200

    bug/typo: teams.ts: fix typo in comment

--- src/ts/team.ts
+++ src/ts/team.ts
@@ -151,7 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
       };
       ApiC.post(`events/${itemid}`, postParams).then(()=> {
         // note: here the event is shown empty (without title), until the user clicks somewhere. Still better than a full page reload.
-        // FIXME: this should be adressed eventually
+        // FIXME: this should be addressed eventually
         calendar.refetchEvents();
       }).catch(() => {
         calendar.unselect();

3d9857cb9798c939181c2c607cbdf2433b16a5f6
Nicolas CARPi <nico-git@deltablot.email>
2024-10-14 12:38:56 +0200

    added syc

---
+++ src/controllers/SycController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2024 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+declare(strict_types=1);
+
+namespace Elabftw\Controllers;
+
+use Elabftw\Elabftw\App;
+use Elabftw\Interfaces\ControllerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class SycController implements ControllerInterface
+{
+    public function __construct(protected App $app) {}
+
+    public function getResponse(): Response
+    {
+        $template = 'syc.html';
+
+        $Response = new Response();
+        $Response->prepare($this->app->Request);
+        $Response->setContent($this->app->render($template, array()));
+
+        return $Response;
+    }
+}

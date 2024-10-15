b8981485fbf2df84525992dc7ea5b881d7b9efc4
MoustaphaCamara <cmr.mous@gmail.com>
2024-10-15 19:07:05 +0200

    bug/minor: templates: always pin on create or duplicate, additional tests

--- tests/unit/models/PinsTest.php
+++ tests/unit/models/PinsTest.php
@@ -19,12 +19,14 @@ class PinsTest extends \PHPUnit\Framework\TestCase
 
     private Templates $Templates;
 
+    private Users $Users;
+
     protected function setUp(): void
     {
-        $Users = new Users(1, 1);
-        $this->Experiments = new Experiments($Users, 1);
-        $this->Items = new Items($Users, 1);
-        $this->Templates = new Templates($Users, 1);
+        $this->Users = new Users(1, 1);
+        $this->Experiments = new Experiments($this->Users, 1);
+        $this->Items = new Items($this->Users, 1);
+        $this->Templates = new Templates($this->Users, 1);
     }
 
     public function testTogglePin(): void
@@ -48,5 +50,52 @@ class PinsTest extends \PHPUnit\Framework\TestCase
         $this->Templates->Pins->togglePin();
         $this->assertTrue($this->Templates->Pins->isPinned());
         $this->assertTrue(count($this->Templates->Pins->readAll()) > 0);
+    }
+
+    /**
+     * ensure that duplicated entry is not pinned
+     * @return void
+     */
+    public function testDuplicateIsNotPinned(): void
+    {
+        // duplicate existing experiment
+        $source = $this->Experiments;
+        $duplicate = $source->duplicate();
+        $freshSource = new Experiments($this->Users, $duplicate);
+        $this->assertNotEquals($source->entityData['id'], $freshSource->entityData['id']);
+
+        // ensure the duplicate experiment is not pinned
+        $this->assertCount(0, $freshSource->Pins->readAll());
+        $this->assertCount(0, $freshSource->Pins->readAllSimple());
+
+        // duplicate existing item
+        $sourceItem = $this->Items;
+        $duplicateItem = $sourceItem->duplicate();
+        $freshItem = new Items($this->Users, $duplicateItem);
+        $this->assertNotEquals($sourceItem->entityData['id'], $freshItem->entityData['id']);
+
+        // ensure the duplicate item is not pinned
+        $this->assertCount(0, $freshItem->Pins->readAll());
+        $this->assertCount(0, $freshItem->Pins->readAllSimple());
+    }
+
+    /**
+     * ensure that duplicating a pinned/non-pinned template results in a new pinned template in both cases
+     * @return void
+     */
+    public function testTemplateIsAlwaysPinnedWhenCreated(): void
+    {
+        // create a new template and ensure it is pinned
+        $source = new Templates($this->Users, 1);
+        $this->assertTrue($source->Pins->isPinned());
+        $this->assertTrue(count($source->Pins->readAll()) > 0);
+
+        // duplicate the template
+        $duplicate = $source->duplicate();
+        $fresh = new Templates($this->Users, $duplicate);
+        $this->assertNotEquals($source->entityData['id'], $fresh->entityData['id']);
+
+        // ensure the duplicate is pinned on creation
+        $this->assertTrue($fresh->Pins->isPinned());
     }
 }

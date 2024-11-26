9104f2a18c12f3b22738b798b6b44b1216e48e24
Mouss <104678778+MoustaphaCamara@users.noreply.github.com>
2024-11-26 10:18:30 +0100

    dev: fix discrepancies in user-team-apikeys combinations (#5366)
    Make sure user/team association corresponds to apikey association too.
    
    ---------
    
    Co-authored-by: Nicolas CARPi <nico-git@deltablot.email>

--- src/classes/Update.php
+++ src/classes/Update.php
@@ -33,7 +33,7 @@ use function sprintf;
 class Update
 {
     /** @var int REQUIRED_SCHEMA the current version of the database structure */
-    public const int REQUIRED_SCHEMA = 167;
+    public const int REQUIRED_SCHEMA = 168;
 
     private Db $Db;
 

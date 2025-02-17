c75111c6d25d4261ac20286c99688b6c9eb2616b
Nicolas CARPi <nico-git@deltablot.email>
2025-02-17 16:56:02 +0100

    ci: cypres: fix unauth handler test

--- tests/cypress/integration/unauthRequestHandler.cy.ts
+++ tests/cypress/integration/unauthRequestHandler.cy.ts
@@ -7,7 +7,7 @@ describe('UnauthRequestHandler', () => {
     cy.request('/app/controllers/UnauthRequestHandler.php').then(resp => {
       expect(resp.status).to.eq(200);
       expect(resp.headers['content-type']).to.eq('application/json');
-      expect(resp.body).to.have.keys('privacy', 'tos', 'a11y', 'legal');
+      expect(resp.body).to.have.keys('privacy', 'privacy_name', 'tos', 'tos_name', 'a11y', 'a11y_name', 'legal', 'legal_name');
     });
   });
 });

4a2ef2d25959a71ac9877d53258dd93b6d640915
MoustaphaCamara <cmr.mous@gmail.com>
2025-05-27 15:17:09 +0200

    ci: fix cypress timeout issue

--- tests/cypress/integration/admin_users.cy.ts
+++ tests/cypress/integration/admin_users.cy.ts
@@ -17,12 +17,16 @@ describe('Users tab in Admin page', () => {
   });
 
   it('creates user', () => {
+    cy.intercept('POST', '/api/v2/users').as('apiPOST');
+
     // Team & Permission group are already filled on this form, by default
     cy.get('input[name=firstname]').type('theNewToto');
     cy.get('input[name=lastname]').type('notSysAdmin');
     cy.get('input[name=email]').type('totonew@yopmail.com');
+
     // create the user
     cy.get('#initialCreateUserBtn').should('exist').click();
+    cy.wait('@apiPOST');
     cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
   });
 
@@ -30,6 +34,8 @@ describe('Users tab in Admin page', () => {
     // if there's a second user, target the dropdown and delete target
     cy.get('button[aria-label="More options"]').then(($buttons) => {
       if ($buttons.length > 1) {
+        cy.intercept('DELETE', '/api/v2/users/**').as('apiDELETE');
+
         // target the 2nd dropdown to delete target user and not the admin
         cy.wrap($buttons.eq(1)).click();
 
@@ -41,6 +47,7 @@ describe('Users tab in Admin page', () => {
 
         // click the 'Delete user' from dropdown
         cy.get('[data-action="destroy-user"]:visible').click();
+        cy.wait('@apiDELETE');
         cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
       } else {
         cy.log('Second dropdown not found, skipping deletion');

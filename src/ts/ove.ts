cb624021e6b322041faf008975ce8e366aa12332
Mouss <104678778+MoustaphaCamara@users.noreply.github.com>
2025-05-28 16:19:43 +0200

    optim/minor: i18n: replace current sentences with i18n translatable (#5675)
    * optim/minor: i18n: replace current sentences with i18n translatable
    
    * accidental ctrl+x -,-
    
    * new translations: copied, file-too-large, replaced errorDivs with notifications and removed unused check-max-size
    
    check-max-size action was not used anywhere, and the errorDiv behaviour was only proper to that place and 'file too large' on the profile/import page. It was used only there and the behaviour should be close to elsewhere on the application
    
    * correct handling for import errors
    
    * import: do not proceed if file is not usable
    
    ---------
    
    Co-authored-by: Nicolas CARPi <nico-git@deltablot.email>

--- src/ts/ove.ts
+++ src/ts/ove.ts
@@ -68,7 +68,7 @@ export function displayPlasmidViewer(about: DOMStringMap): void {
       }
 
       if (parsedData[0].success === false) {
-        (new Notification()).error(`Invalid DNA data in file: ${realName}`);
+        new Notification().error('invalid-info');
       }
 
       const parsedSequence = parsedData[0].parsedSequence;

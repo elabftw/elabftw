c8f4cea0e64fd9607a05e0fff8cbc3ab906ed296
MoustaphaCamara <cmr.mous@gmail.com>
2025-06-03 08:18:25 +0200

    fix structure: alter table only after creation of referenced tables

--- src/sql/structure.sql
+++ src/sql/structure.sql
@@ -392,11 +392,7 @@ CREATE TABLE `experiments_templates_comments` (
   `comment` text NOT NULL,
   `userid` int(10) UNSIGNED NOT NULL,
   `immutable` TINYINT UNSIGNED NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `fk_experiments_templates_comments_experiments_templates_id` (`item_id`),
-  KEY `fk_experiments_templates_comments_users_userid` (`userid`),
-  CONSTRAINT `fk_experiments_templates_comments_experiments_templates_id` FOREIGN KEY (`item_id`) REFERENCES `experiments_templates` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_experiments_templates_comments_users_userid` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --

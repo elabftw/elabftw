7c55d5d75a550a931ee22ceeda1becaef9b0edfd
Mouss <104678778+MoustaphaCamara@users.noreply.github.com>
2025-06-05 15:42:10 +0200

    feat: metadata: convert text input to textarea (default extra field) & fix number default unit (#5683)
    fix #5680
    fix #5676
    
    ---------
    
    Co-authored-by: Nicolas CARPi <nico-git@deltablot.email>

--- src/classes/TwigFilters.php
+++ src/classes/TwigFilters.php
@@ -24,6 +24,7 @@ use Elabftw\Models\Users;
 
 use function is_array;
 use function sprintf;
+use function nl2br;
 
 /**
  * Twig filters
@@ -103,6 +104,10 @@ final class TwigFilters
                 if ($metadataType === 'checkbox') {
                     $checked = $field[MetadataEnum::Value->value] === 'on' ? ' checked="checked"' : '';
                     $value = '<input class="d-block" disabled type="checkbox"' . $checked . '>';
+                }
+                // type:text is another special case that becomes textarea, handles multiple lines
+                elseif ($metadataType === 'text') {
+                    $value = nl2br(Tools::eLabHtmlspecialchars($value));
                 }
                 // type:url is another special case
                 elseif ($metadataType === 'url') {

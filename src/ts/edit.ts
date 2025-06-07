d4613db9ab43c1ad7b151ca18500302cb9a72b4b
Nicolas CARPi <nico-git@deltablot.email>
2025-06-07 13:58:18 +0200

    spacing

--- src/ts/edit.ts
+++ src/ts/edit.ts
@@ -20,7 +20,7 @@ import $ from 'jquery';
 import { Api } from './Apiv2.class';
 import { Notification } from './Notifications.class';
 import { Uploader } from './uploader';
-import {clearLocalStorage} from './localStorage';
+import { clearLocalStorage } from './localStorage';
 
 document.addEventListener('DOMContentLoaded', async () => {
   // only run in edit mode

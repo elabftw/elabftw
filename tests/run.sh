d9ec17551d439d3609495da73c9f83a2a6f264ba
Nicolas CARPi <nico-git@deltablot.email>
2025-06-14 02:04:20 +0200

    remove all scrutinizer stuff from test script

--- tests/run.sh
+++ tests/run.sh
@@ -11,14 +11,6 @@
 # stop on failure
 set -eu
 
-# the behavior between local dev and ci pipeline is slightly different
-# use env vars set in ci to guess where we run
-# https://scrutinizer-ci.com/g/elabftw/elabftw
-# https://scrutinizer-ci.com/docs/build/environment-variables
-# https://circleci.com/docs/variables
-# the both define a CI bool, so let's use that
-ci=${CI:-false}
-
 # when the script stops (or is stopped), say goodbye
 cleanup() {
     echo "Buh bye!"
@@ -28,63 +20,32 @@ trap cleanup EXIT
 # if there are no custom env_file, touch one, as this will trigger an error
 if [ ! -f tests/elabftw-user.env ]; then
     touch tests/elabftw-user.env
-    if (${SCRUTINIZER:-false}); then
-        printf "ELABFTW_USER=scrutinizer\nELABFTW_GROUP=scrutinizer\nELABFTW_USERID=1001\nELABFTW_GROUPID=1001\n" > tests/elabftw-user.env
-    fi
 fi
 
 # launch a fresh environment if needed
 if [ ! "$(docker ps -q -f name=mysqltmp)" ] && [ ! "$(docker container ls -q -f name=elab-cypress)" ]; then
-    if ($ci); then
-        # Use the freshly built elabtmp image
-        # use DOCKER_BUILDKIT env instead of calling "docker buildx" or it fails in scrutinizer
-        export DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain COMPOSE_DOCKER_CLI_BUILD=1
-        docker build -q -t elabtmp -f tests/elabtmp.Dockerfile .
-    fi
     docker compose -f tests/docker-compose.yml up -d --quiet-pull
     # give some time for containers to start
     echo -n "Waiting for containers to start..."
     while [ "$(docker inspect -f {{.State.Health.Status}} elabtmp)" != "healthy" ]; do echo -n .; sleep 2; done; echo
 fi
-if ($ci); then
-    # install and initial tests
-    docker exec -it elabtmp yarn install --silent --immutable
-    docker exec -it elabtmp yarn csslint
-    docker exec -it elabtmp yarn jslint
-    docker exec -it elabtmp yarn buildall:dev
-    docker exec -it elabtmp composer install --no-progress -q
-    docker exec -it elabtmp yarn phpcs-dry
-else
-    # we need to add the parser because it's in cache/ and it's tmpfs mounted now
-    docker exec -it elabtmp yarn buildparser
-    if [ "${SKIP_TWIGCS:-0}" -ne 1 ]; then
-        echo "Running twigcs. Use SKIP_TWIGCS=1 to disable."
-        docker exec -it elabtmp yarn twigcs
-    fi
+# we need to add the parser because it's in cache/ and it's tmpfs mounted now
+docker exec -it elabtmp yarn buildparser
+if [ "${SKIP_TWIGCS:-0}" -ne 1 ]; then
+    echo "Running twigcs. Use SKIP_TWIGCS=1 to disable."
+    docker exec -it elabtmp yarn twigcs
 fi
 # fix permissions on cache folders
 docker exec -it elabtmp mkdir -p cache/purifier/{HTML,CSS,URI} cache/{elab,mpdf,twig}
 worker_user=$(docker exec -it elabtmp tail -n1 /etc/shadow |awk -F ":" '{print $1}')
 docker exec -it elabtmp chown -R "$worker_user":"$worker_user" cache
 
-if ($ci); then
-    docker exec -it elabtmp yarn static
-fi
 # populate the database
 if [ "${SKIP_POPULATE:-0}" -ne 1 ]; then
     echo "Running populate script. Use SKIP_POPULATE=1 to disable."
     docker exec -it elabtmp bin/init db:populate src/tools/populate-config.yml.dist -y
 fi
 # RUN TESTS
-if ($ci); then
-    # fix permissions on test output and uploads
-    sudo chmod -R 777 tests/_output
-    sudo chmod -R 777 uploads
-    if (${SCRUTINIZER:-false}); then
-        sudo chown -R scrutinizer:scrutinizer cache
-        sudo chown -R scrutinizer:scrutinizer uploads
-    fi
-fi
 # when trying to use a bash variable to hold the skip api options, I ran into issues that this option doesn't exist, so the command is entirely duplicated instead
 if [ "${1:-}" = "unit" ]; then
     # Remove "unit" from the list of arguments, we are going to allow multiple arguments (test files) so this one should be excluded from the list.
@@ -128,19 +89,9 @@ elif [ "${1:-}" = "cy" ]; then
         && tar -xf ./tests/_output/cypress-coverage.tar -C ./tests/_output/cypress-coverage-html
     docker cp elabtmp:/elabftw/tests/_output/c3tmp/codecoverage.clover.xml ./tests/_output/cypress-coverage.clover.xml
 else
-    docker exec -it elabtmp php vendor/bin/codecept run --skip cypress --coverage --coverage-html --coverage-xml
+    docker exec -it elabtmp php vendor/bin/codecept run --skip cypress --coverage --coverage-html
 fi
 
-# in ci we copy the coverage output file in current directory
-if ($ci); then
-    docker cp elabtmp:/elabftw/tests/_output/coverage.xml .
-fi
-
-if [ "${1:-}" != "cy" ]; then
-    # make a copy with adjusted path for local sonar scanner
-    ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && pwd )
-    sed -e "s:/elabftw/:$ROOT_DIR/:g" tests/_output/coverage.xml > tests/_output/coverage-sonar.xml
-fi
 # all tests succeeded, display a koala
 cat << WALAEND
 

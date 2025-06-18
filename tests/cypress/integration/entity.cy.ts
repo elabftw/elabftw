5e6fce3b9cc1f01f97e09e9e8fdfc920c019f8bc
Nicolas CARPi <nico-git@deltablot.email>
2025-06-18 17:30:49 +0200

    let's try this version

--- tests/cypress/integration/entity.cy.ts
+++ tests/cypress/integration/entity.cy.ts
@@ -3,41 +3,45 @@ describe('Experiments', () => {
     cy.login();
   });
 
-  const entityEdit = () => {
+  const entityEdit = (endpoint: string) => {
     cy.url().should('include', 'mode=edit');
 
-    // update date only on Experiment & Resources pages
-    cy.location('pathname').then((path) => {
-      if (!path.includes('/templates.php')) {
-        cy.get('#date_input').type('2021-05-01').blur();
-        cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
-      }
-    });
+    // update date
+    cy.intercept('PATCH', `/api/v2/${endpoint}/**`).as('apiPATCH');
+    cy.get('#date_input').type('2021-05-01').blur();
+    cy.wait('@apiPATCH');
+    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
 
     // create Tag
+    cy.intercept('POST', `/api/v2/${endpoint}/**`).as('apiPOST');
     cy.get('#createTagInput').type('some tag').blur();
+    cy.wait('@apiPOST');
     cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
     cy.get('div.tags').contains('some tag').should('exist');
 
     // delete tag
     cy.on('window:confirm', () => { return true; });
     cy.contains('some tag').click();
+    cy.wait('@apiPATCH');
     cy.get('div.tags').contains('some tag').should('not.exist');
 
     // create step
     cy.get('.stepinput').type('some step');
     cy.get('[data-action="create-step"').click();
+    cy.wait('@apiPOST');
     cy.get('.step-static').should('contain', 'some step');
 
     // complete step
     cy.get('.stepbox').click();
+    cy.wait('@apiPATCH');
     cy.get('.text-muted').should('contain', 'completed');
 
-    // TODO-cypress: (wip) fix is coming on next PR as there's many anchors with no label, and other invalid html
     //cy.htmlvalidate();
 
     // delete step
+    cy.intercept('DELETE', `/api/v2/${endpoint}/**`).as('apiDELETE');
     cy.get('[data-action="destroy-step"]').click();
+    cy.wait('@apiDELETE');
     cy.contains('some step').should('not.exist');
   };
 
@@ -48,10 +52,18 @@ describe('Experiments', () => {
 
     cy.get('#commentsCreateArea').type('This is a very nice experiment');
     cy.get('[data-action="create-comment"]').click();
+    cy.wait('@apiPOST');
     cy.get('#commentsDiv').contains('Toto Le sysadmin commented').should('be.visible');
     cy.get('[data-action="destroy-comment"]').click();
+    cy.wait('@apiDELETE');
     cy.get('#commentsDiv').contains('Toto Le sysadmin commented').should('not.exist');
-    // cy.htmlvalidate();
+    cy.htmlvalidate(
+      // {
+      //   rules: {
+      //     'prefer-native-element': 'off',
+      //   },
+      // },
+    );
   };
 
   const entityDuplicate = () => {
@@ -60,13 +72,10 @@ describe('Experiments', () => {
       cy.log(url);
       cy.get('[data-target="duplicateModal"]').click()
         .get('[data-action="duplicate-entity"]').click();
-      cy.location('pathname').then((path) => {
-        if (!path.includes('/templates.php')) {
-          cy.get('#documentTitle').should('be.visible').should('contain', 'Untitled I');
-        } else {
-          cy.get('#documentTitle').should('be.visible').should('contain', 'Template I');
-        }
-      });
+      cy.wait('@apiGET');
+      cy.wait('@apiGET');
+      cy.wait('@apiPOST');
+      cy.get('#documentTitle').should('be.visible').should('contain', 'Untitled I');
       // destroy the duplicated entity now
       entityDestroy();
       // go back to the original entity
@@ -77,53 +86,46 @@ describe('Experiments', () => {
   const entityDestroy = () => {
     cy.get('button[title="More options"]').click()
       .get('button[data-action="destroy"]').click();
+    cy.wait('@apiDELETE');
   };
 
-  it('Create, edit, duplicate and delete an experiment', () => {
+  const entityCatStat = (category: string, categoryTarget: string, statusTarget: string) => {
+    cy.wait('@apiGET');
+    // change category
+    cy.get('.malleableCategory').click();
+    cy.get(`select:has(option:selected:contains("${category}"))`).select(`${categoryTarget}`);
+    cy.get('.form-inline > .btn-primary').click();
+    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
+    // change status
+    cy.get('.malleableStatus').click();
+    cy.get('select:has(option:selected:contains("Not set"))').select(`${statusTarget}`);
+    cy.get('.form-inline > .btn-primary').click();
+    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
+  };
+
+  it('Create and edit an experiment', () => {
+    const endpoint = 'experiments';
     cy.visit('/experiments.php');
     cy.htmlvalidate();
     cy.contains('Create').click();
+    cy.intercept('GET', `/api/v2/${endpoint}/**`).as('apiGET');
     cy.get('#createModal_experiments').should('be.visible').should('contain', 'Default template').contains('Default template').click();
-    entityEdit();
-    // change status
-    cy.get('#status_select').select('Success').blur();
-    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
+    entityCatStat('Not set', 'Cell biology', 'Success');
+    entityEdit(endpoint);
     entityComment();
     entityDuplicate();
     entityDestroy();
   });
 
-  it('Create, edit, duplicate and delete an item', () => {
+  it('Create and edit an item', () => {
+    const endpoint = 'items';
     cy.visit('/database.php');
     cy.htmlvalidate();
     cy.contains('Create').click();
+    cy.intercept('GET', `/api/v2/${endpoint}/**`).as('apiGET');
     cy.get('#createModal_database').should('be.visible').should('contain', 'Microscope').contains('Microscope').click();
-    entityEdit();
-    cy.get('#category_select').select('Plasmid').blur();
-    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
-    entityComment();
-    entityDuplicate();
-    entityDestroy();
-  });
-
-  it('Create, edit, duplicate and delete an experiment template', () => {
-    cy.visit('/templates.php');
-    cy.htmlvalidate();
-    cy.contains('Create').click();
-    cy.get('#createModal_templates').should('be.visible').should('contain', 'Create a new template');
-    cy.get('input[name=title]').type('Template');
-    cy.get('button[data-action="create-entity"][data-type="experiments_templates"]').click();
-    // change category and status
-    cy.get('#category_select').select('Cell biology').blur();
-    cy.get('#status_select').select('Success').blur();
-    // actions specific to template: lock permissions for derived entities
-    cy.get('#canread_is_immutable').check({ force: true });
-    cy.get('#canwrite_is_immutable').check({ force: true });
-    cy.get('#canread_is_immutable').should('be.checked');
-    cy.get('#canwrite_is_immutable').should('be.checked');
-    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
-    // actions for all entities
-    entityEdit();
+    entityCatStat('Microscope', 'Plasmid', 'In stock');
+    entityEdit(endpoint);
     entityComment();
     entityDuplicate();
     entityDestroy();

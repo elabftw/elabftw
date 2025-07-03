696c0be26cc3aa7614a2be9350250eeb90107333
MoustaphaCamara <cmr.mous@gmail.com>
2025-07-03 10:39:50 +0200

    links tests ','

--- tests/cypress/integration/links.cy.ts
+++ tests/cypress/integration/links.cy.ts
@@ -10,7 +10,7 @@ describe('Test links', () => {
     // link to an experiment
     cy.get('#addLinkExpInput').type('Synthesis');
     cy.get('.ui-menu-item-wrapper').first().click();
-    cy.get('#addLinkExpInput').type('{enter}');
+    cy.get('button[aria-label="Add experiment link"]').click();
     cy.get('#experimentsLinksDiv').should('contain.text', 'Synthesis');
     cy.intercept('DELETE', '/api/v2/**').as('delete');
     cy.get('[data-action="destroy-link"]').first().click();
@@ -18,14 +18,12 @@ describe('Test links', () => {
     //cy.get('#experimentsLinksDiv').should('not.contain.text', 'Synthesis and Characterization');
 
     // link to a resource
-    /*
     cy.get('#addLinkItemsInput').type('Ammonia');
-    cy.get('.ui-menu-item-wrapper').click();
-    cy.get('#addLinkExpInput').type('{enter}');
+    cy.get('.ui-menu-item-wrapper').contains('Ammonia').click();
+    cy.get('button[aria-label="Add item link"]').click();
     cy.get('#itemsLinksDiv').should('contain.text', 'Ammonia - NH3');
     cy.intercept('DELETE', '/api/v2/**').as('delete');
     cy.get('[data-action="destroy-link"]').first().click();
     cy.wait('@delete').its('response.statusCode').should('eq', 204);
-   */
   });
 });

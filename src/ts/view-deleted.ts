d0fbb55e304d2a37448943fee011cca3d0643585
MoustaphaCamara <cmr.mous@gmail.com>
2025-07-17 15:07:39 +0200

    handle restore client-side view-deleted page

--- src/ts/view-deleted.ts
+++ src/ts/view-deleted.ts
@@ -7,6 +7,10 @@
  * @package elabftw
  */
 
+import { Api } from './Apiv2.class';
+import { Action } from './interfaces';
+import { getEntity } from './misc';
+
 document.addEventListener('DOMContentLoaded', () => {
   if (!document.getElementById('info')) {
     return;
@@ -16,4 +20,13 @@ document.addEventListener('DOMContentLoaded', () => {
   if (about.page !== 'deleted') {
     return;
   }
+  const ApiC = new Api();
+  const entity = getEntity();
+
+  document.querySelector('.real-container').addEventListener('click', (event) => {
+    if ((event.target as HTMLElement).matches('[data-action="restore-entity"]')) {
+      ApiC.patch(`${entity.type}/${entity.id}`, { action: Action.Restore })
+        .then(() => window.location.href = `?mode=view&id=${entity.id}`);
+    }
+  });
 });

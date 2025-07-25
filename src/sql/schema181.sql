7970113527a12607aa2126771d2042c4b8839579
Patrick Kibies <patrick.kibies@tu-dortmund.de>
2025-07-25 21:45:37 +0200

    Did some minor edits to address https://github.com/elabftw/elabftw/pull/5823#discussion_r2231764205, https://github.com/elabftw/elabftw/pull/5823#discussion_r2231764526, https://github.com/elabftw/elabftw/pull/5823#discussion_r2231765046, and https://github.com/elabftw/elabftw/pull/5823#discussion_r2231764913

--- src/sql/schema181.sql
+++ src/sql/schema181.sql
@@ -4,4 +4,3 @@ INSERT INTO config (conf_name, conf_value) VALUES ('allow_permission_user', '1')
 INSERT INTO config (conf_name, conf_value) VALUES ('allow_permission_full', '1');
 INSERT INTO config (conf_name, conf_value) VALUES ('allow_permission_organization', '1');
 UPDATE config SET conf_name = 'allow_permission_useronly' WHERE conf_name = 'allow_useronly';
-
--- src/sql/schema182.sql
+++ src/sql/schema182.sql
@@ -1,3 +1,2 @@
--- schema 181
+-- schema 182
 INSERT INTO config (conf_name, conf_value) VALUES ('s3_use_path_style_endpoint', '0');
-

df2c7f284da8138a4f58ce102fd31644ef78b28a
Nicolas CARPi <nico-git@deltablot.email>
2025-08-03 21:06:09 +0200

    add 183 down

--- src/sql/schema183-down.sql
+++ src/sql/schema183-down.sql
@@ -1,3 +1,23 @@
 -- revert schema 183
+CALL drop_fk_if_exists('items_types', 'category');
+CALL drop_fk_if_exists('items', 'category');
+DROP TABLE IF EXISTS `items_categories`;
+CALL DropFK('experiments_categories', 'fk_experiments_categories_teams_id');
+CALL DropIdx('experiments_categories', 'fk_experiments_categories_teams_team_id');
+ALTER TABLE `items_types` DROP COLUMN `category`;
+CALL DropFK('items', 'fk_items_items_types_id');
+CALL DropIdx('items', 'fk_items_items_types_id');
+
+-- add missing created_at and modified_at on experiments_categories
+ALTER TABLE `experiments_categories`
+  DROP COLUMN `created_at`,
+  DROP COLUMN `modified_at`;
+
+-- give status to experiments templates too
+DROP TABLE IF EXISTS `experiments_templates_status`;
+
+ALTER TABLE teams
+  ADD COLUMN `common_template` TEXT,
+  ADD COLUMN `common_template_md` TEXT;
 
 UPDATE config SET conf_value = 182 WHERE conf_name = 'schema';
--- src/sql/schema183.sql
+++ src/sql/schema183.sql
@@ -15,9 +15,9 @@ CREATE TABLE `items_categories` (
 -- add missing fk on experiments_categories
 CALL DropFK('experiments_categories', 'fk_experiments_categories_teams_id');
 CALL DropIdx('experiments_categories', 'fk_experiments_categories_teams_team_id');
+CALL DropFK('experiments_categories', 'fk_experiments_categories_teams_team_id');
 ALTER TABLE `experiments_categories`
-  ADD KEY `fk_experiments_categories_teams_team_id` (`team`),
-  ADD CONSTRAINT `fk_experiments_categories_teams_id` FOREIGN KEY (`team`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT FOREIGN KEY (`team`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 -- now insert items_types into items_categories
 INSERT INTO `items_categories` (
   `team`,
@@ -34,15 +34,13 @@ FROM
   `items_types`;
 ALTER TABLE `items_types`
   ADD COLUMN `category` INT UNSIGNED NULL DEFAULT NULL,
-  ADD KEY `fk_items_types_category_items_categories_id` (`category`),
-  ADD KEY `fk_items_types_category_items_categories_id` FOREIGN KEY (`category`) REFERENCES `items_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT FOREIGN KEY (`category`) REFERENCES `items_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- on table items, set a correct fk/index cascade because they can exist without a category
 CALL DropFK('items', 'fk_items_items_types_id');
 CALL DropIdx('items', 'fk_items_items_types_id');
 ALTER TABLE `items`
-  ADD KEY `fk_items_category_items_categories_id` (`category`),
-  ADD KEY `fk_items_category_items_categories_id` FOREIGN KEY (`category`) REFERENCES `items_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT FOREIGN KEY (`category`) REFERENCES `items_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- add missing created_at and modified_at on experiments_categories
 ALTER TABLE `experiments_categories`

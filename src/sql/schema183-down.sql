9de91a4aa65ce322a6fb41f27236c03e46d4d27a
Nicolas CARPi <nico-git@deltablot.email>
2025-08-06 19:28:44 +0200

    fix down

--- src/sql/schema183-down.sql
+++ src/sql/schema183-down.sql
@@ -4,16 +4,17 @@ CALL drop_fk_if_exists('items', 'category');
 DROP TABLE IF EXISTS `items_categories`;
 CALL DropFK('experiments_categories', 'fk_experiments_categories_teams_id');
 CALL DropIdx('experiments_categories', 'fk_experiments_categories_teams_team_id');
-ALTER TABLE `items_types` DROP COLUMN `category`;
+CALL DropColumn('items_types', 'category');
 CALL DropFK('items', 'fk_items_items_types_id');
 CALL DropIdx('items', 'fk_items_items_types_id');
 
--- add missing created_at and modified_at on experiments_categories
-ALTER TABLE `experiments_categories`
-  DROP COLUMN `created_at`,
-  DROP COLUMN `modified_at`;
+CALL DropColumn('experiments_categories', 'created_at');
+CALL DropColumn('experiments_categories', 'modified_at');
+CALL DropColumn('experiments_status', 'created_at');
+CALL DropColumn('experiments_status', 'modified_at');
+CALL DropColumn('items_status', 'created_at');
+CALL DropColumn('items_status', 'modified_at');
 
--- give status to experiments templates too
 DROP TABLE IF EXISTS `experiments_templates_status`;
 
 ALTER TABLE teams
@@ -22,6 +23,15 @@ ALTER TABLE teams
 
 DROP TABLE IF EXISTS `items_types_comments`;
 DROP TABLE IF EXISTS `pin_items_types2users`;
-ALTER TABLE `users` DROP COLUMN `scope_items_types`;
+DROP TABLE IF EXISTS `items_types_status`;
+DROP TABLE IF EXISTS `items_types_revisions`;
+
+CALL DropColumn('users', 'scope_items_types');
+CALL DropColumn('experiments_changelog', 'modified_at');
+CALL DropColumn('experiments_revisions', 'modified_at');
+CALL DropColumn('experiments_templates_changelog', 'modified_at');
+CALL DropColumn('experiments_templates_revisions', 'modified_at');
+CALL DropColumn('items_changelog', 'modified_at');
+CALL DropColumn('items_revisions', 'modified_at');
 
 UPDATE config SET conf_value = 182 WHERE conf_name = 'schema';

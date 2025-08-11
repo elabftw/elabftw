28ad94a167c330101d18508f6cf74b29dc4f0b79
Nicolas CARPi <nico-git@deltablot.email>
2025-08-11 16:34:54 +0200

    cy: remove the wait

--- tests/cypress/integration/links.cy.ts
+++ tests/cypress/integration/links.cy.ts
@@ -1,3 +1,4 @@
+// WARNING flaky test
 describe('Test links', () => {
   beforeEach(() => {
     cy.login();
@@ -13,7 +14,7 @@ describe('Test links', () => {
     cy.get('#addLinkExpInput').type('Testing');
     cy.get('.ui-menu-item-wrapper').first().click();
     cy.get('button[aria-label="Add link to an experiment"]').click();
-    cy.get('#experimentsLinksDiv').wait(500).should('contain.text', 'Testing');
+    cy.get('#experimentsLinksDiv').should('contain.text', 'Testing');
     cy.intercept('DELETE', '/api/v2/**').as('delete');
     cy.get('[data-action="destroy-link"]').first().click();
     cy.wait('@delete').its('response.statusCode').should('eq', 204);

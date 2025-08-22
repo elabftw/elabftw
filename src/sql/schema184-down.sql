4e54c55ec1318cca8c7e4a65bc3ba18614ec3744
MoustaphaCamara <cmr.mous@gmail.com>
2025-08-22 16:37:48 +0200

    fix migration

--- src/sql/schema184-down.sql
+++ src/sql/schema184-down.sql
@@ -2,4 +2,21 @@
 ALTER TABLE `team_events`
     MODIFY COLUMN `start` varchar(255) NOT NULL,
     MODIFY COLUMN `end` varchar(255) DEFAULT NULL;
+-- Add back temporary VARCHAR columns after `item`
+ALTER TABLE `team_events`
+    ADD COLUMN `start_rollback` VARCHAR(255) NULL AFTER `item`,
+    ADD COLUMN `end_rollback` VARCHAR(255) NULL AFTER `start_rollback`;
+
+-- Backfill from the current DATETIME columns
+UPDATE `team_events`
+    SET `start_rollback` = REPLACE(DATE_FORMAT(`start`, '%Y-%m-%d %H:%i:%s'), ' ', 'T'),
+        `end_rollback` = IFNULL(REPLACE(DATE_FORMAT(`end`, '%Y-%m-%d %H:%i:%s'), ' ', 'T'), '0000-00-00T00:00:00');
+
+-- Drop the new DATETIME columns and swap in the old VARCHAR ones
+ALTER TABLE `team_events`
+DROP COLUMN `start`,
+    DROP COLUMN `end`,
+    CHANGE COLUMN `start_rollback` `start` VARCHAR(255) NOT NULL,
+    CHANGE COLUMN `end_rollback`   `end`   VARCHAR(255) NULL;
+
 UPDATE config SET conf_value = 183 WHERE conf_name = 'schema';
--- src/sql/schema184.sql
+++ src/sql/schema184.sql
@@ -1,4 +1,35 @@
 -- schema 184 - convert Events start & end to datetime
+-- first migrate to a datetime column safely
 ALTER TABLE `team_events`
-    MODIFY COLUMN `start` DATETIME NOT NULL,
-    MODIFY COLUMN `end` DATETIME DEFAULT NULL;
+    ADD COLUMN `start_dt` DATETIME NULL AFTER `item`,
+    ADD COLUMN `end_dt` DATETIME NULL AFTER `start_dt`;
+
+-- migrate start column
+UPDATE `team_events`
+SET `start_dt` =
+    CASE
+        WHEN `start` IS NULL OR `start` = '' OR `start` = '0000-00-00 00:00:00'
+            THEN NULL
+        ELSE STR_TO_DATE(LEFT(REPLACE(`start`, 'T', ' '), 19), '%Y-%m-%d %H:%i:%s')
+        END;
+
+-- migrate end column
+UPDATE `team_events`
+SET `end_dt` =
+    CASE
+        WHEN `end` IS NULL OR `end` = '' OR `end` = '0000-00-00 00:00:00'
+            THEN NULL
+        ELSE STR_TO_DATE(LEFT(REPLACE(`end`, 'T', ' '), 19), '%Y-%m-%d %H:%i:%s')
+        END;
+
+-- lock nullability once backfill is ok
+ALTER TABLE `team_events`
+    MODIFY COLUMN `start_dt` DATETIME NOT NULL,
+    MODIFY COLUMN `end_dt` DATETIME NULL;
+
+-- finally, Swap columns
+ALTER TABLE `team_events`
+    DROP COLUMN `start`,
+    DROP COLUMN `end`,
+        CHANGE COLUMN `start_dt` `start` DATETIME NOT NULL,
+        CHANGE COLUMN `end_dt` `end` DATETIME NULL;

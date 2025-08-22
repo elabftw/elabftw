1f724c6cd923e6a0e9ebfbe124865d220c1a71df
Nicolas CARPi <nico-git@deltablot.email>
2025-08-22 17:29:09 +0200

    alpha4

--- src/Elabftw/App.php
+++ src/Elabftw/App.php
@@ -49,7 +49,7 @@ final class App
 {
     use TwigTrait;
 
-    public const string INSTALLED_VERSION = '5.3.0-alpha3';
+    public const string INSTALLED_VERSION = '5.3.0-alpha4';
 
     // this version format is used to compare with last_seen_version of users
     // major is untouched, and minor and patch are padded with one 0 each

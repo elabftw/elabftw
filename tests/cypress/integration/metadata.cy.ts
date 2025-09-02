dd1ce3af3093b9c24fd802da02cfecc349d9eb68
MoustaphaCamara <cmr.mous@gmail.com>
2025-09-02 17:47:42 +0200

    refactor test with create field method

--- tests/cypress/integration/metadata.cy.ts
+++ tests/cypress/integration/metadata.cy.ts
@@ -16,37 +16,33 @@ describe('Metadata Extra fields', () => {
   });
 
   const createTextExtraField = () => {
-    const fieldName = 'Some text';
-    cy.get('[data-target="fieldBuilderModal"][aria-label="Add field"]').first().click();
-    cy.get('#fieldBuilderModal').should('be.visible');
-    cy.get('#newFieldKeyInput').wait(500).type(fieldName);
-    cy.get('[data-action="save-new-field"]').click();
-    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
-    cy.get('#metadataDiv').should('be.visible').should('contain', fieldName).contains(fieldName);
-  }
+    createNewField('Text type field', 'text');
+  };
 
   const deleteExtrafield = () => {
     cy.get('#metadataDiv').should('be.visible');
     cy.on('window:confirm', () => { return true; });
     cy.get('[data-action="metadata-rm-field"]').click();
-  }
+  };
 
   const createUserExtraField = () => {
     const fieldName = 'User type field';
-    // use first() because two elements: the second one is for jsonEditor
-    cy.get('[data-target="fieldBuilderModal"][aria-label="Add field"]').first().click();
-    cy.get('#fieldBuilderModal').should('be.visible');
-    // select field type
-    cy.get('#newFieldTypeSelect').select('users');
-    cy.get('#newFieldKeyInput').wait(500).type(fieldName);
-    cy.get('[data-action="save-new-field"]').click();
-    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
-    cy.get('#metadataDiv').should('be.visible').should('contain', fieldName).contains(fieldName);
+    createNewField(fieldName, 'users');
     cy.get(`[data-field="${fieldName}"][data-target="users"]`).wait(500).type('Titi{enter}');
     cy.get('ul.ui-autocomplete')
       .should('be.visible')
       .contains('div.ui-menu-item-wrapper', 'Titi')
       .click();
     cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
-  }
+  };
+
+  const createNewField = (fieldName: string, type: string) => {
+    cy.get('[data-target="fieldBuilderModal"][aria-label="Add field"]').first().click();
+    cy.get('#fieldBuilderModal').should('be.visible');
+    cy.get('#newFieldTypeSelect').select(type);
+    cy.get('#newFieldKeyInput').wait(500).type(fieldName);
+    cy.get('[data-action="save-new-field"]').click();
+    cy.get('.overlay').first().should('be.visible').should('contain', 'Saved');
+    cy.get('#metadataDiv').should('be.visible').should('contain', fieldName).contains(fieldName);
+  };
 });

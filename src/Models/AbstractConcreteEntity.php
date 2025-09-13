d43166ec7b84b9870f6b3ec2bbe612a1f9682e99
Nicolas CARPi <nico-git@deltablot.email>
2025-09-13 22:09:13 +0200

    meh

--- src/Models/AbstractConcreteEntity.php
+++ src/Models/AbstractConcreteEntity.php
@@ -12,27 +12,11 @@ declare(strict_types=1);
 
 namespace Elabftw\Models;
 
-use Elabftw\Enums\Action;
-use Elabftw\Exceptions\ImproperActionException;
-use Elabftw\Params\EntityParams;
-use Override;
-use PDO;
-
 /**
  * An entity like Experiments or Items. Concrete as opposed to TemplateEntity for experiments templates or items types
  */
 abstract class AbstractConcreteEntity extends AbstractEntity
 {
-    #[Override]
-    public function patch(Action $action, array $params): array
-    {
-        match ($action) {
-            Action::SetNextCustomId => $this->update(new EntityParams('custom_id', $this->getNextIdempotentCustomId())),
-            default => parent::patch($action, $params),
-        };
-        return $this->readOne();
-    }
-
     /**
      * Count the number of timestamp archives created during past month (sliding window)
      * Here we merge bloxberg and trusted timestamp methods because there is no way currently to tell them apart
@@ -55,25 +39,5 @@ abstract class AbstractConcreteEntity extends AbstractEntity
             return null;
         }
         return $res + 1;
-    }
-
-    private function getCurrentHighestCustomId(int $category): int
-    {
-        $sql = 'SELECT custom_id FROM ' . $this->entityType->value . ' WHERE category = :category AND custom_id IS NOT NULL ORDER BY custom_id DESC LIMIT 1';
-        $req = $this->Db->prepare($sql);
-        $req->bindParam(':category', $category, PDO::PARAM_INT);
-        $this->Db->execute($req);
-        return (int) $req->fetchColumn();
-    }
-
-    // figure out the next custom id for our entity
-    private function getNextIdempotentCustomId(): int
-    {
-        if ($this->entityData['category'] === null) {
-            throw new ImproperActionException(_('A category is required to fetch the next Custom ID'));
-        }
-        // start by setting our current custom_id null to get idempotency
-        $this->update(new EntityParams('custom_id', null));
-        return $this->getCurrentHighestCustomId($this->entityData['category']) + 1;
     }
 }
--- src/Models/AbstractEntity.php
+++ src/Models/AbstractEntity.php
@@ -482,6 +482,7 @@ abstract class AbstractEntity extends AbstractRest
             Action::RemoveExclusiveEditMode => $this->ExclusiveEditMode->destroy(),
             Action::SetCanread => $this->update(new EntityParams('canread', $params['can'])),
             Action::SetCanwrite => $this->update(new EntityParams('canwrite', $params['can'])),
+            Action::SetNextCustomId => $this->update(new EntityParams('custom_id', $this->getNextIdempotentCustomId())),
             Action::Sign => $this->sign($params['passphrase'], Meaning::from((int) $params['meaning'])),
             Action::Timestamp => $this->timestamp(),
             Action::Unarchive => $this->handleArchivedState(from: State::Archived, to: State::Normal, toggleLock: fn() => $this->unlock()),
@@ -1015,6 +1016,26 @@ abstract class AbstractEntity extends AbstractRest
             return 'User not found!';
         }
         return $user->userData['fullname'];
+    }
+
+    protected function getCurrentHighestCustomId(int $category): int
+    {
+        $sql = 'SELECT custom_id FROM ' . $this->entityType->value . ' WHERE category = :category AND custom_id IS NOT NULL ORDER BY custom_id DESC LIMIT 1';
+        $req = $this->Db->prepare($sql);
+        $req->bindParam(':category', $category, PDO::PARAM_INT);
+        $this->Db->execute($req);
+        return (int) $req->fetchColumn();
+    }
+
+    // figure out the next custom id for our entity
+    protected function getNextIdempotentCustomId(): int
+    {
+        if ($this->entityData['category'] === null) {
+            throw new ImproperActionException(_('A category is required to fetch the next Custom ID'));
+        }
+        // start by setting our current custom_id null to get idempotency
+        $this->update(new EntityParams('custom_id', null));
+        return $this->getCurrentHighestCustomId($this->entityData['category']) + 1;
     }
 
     // Archive a normal entity, Unarchive an archived entity.

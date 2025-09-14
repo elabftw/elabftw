087797c459c741c479b98fd51dccf2533f428c79
Nicolas CARPi <nico-git@deltablot.email>
2025-09-14 21:14:34 +0200

    add schema down

--- src/sql/schema184-down.sql
+++ src/sql/schema184-down.sql
@@ -1,3 +1,5 @@
 -- revert schema 184
-
+ALTER TABLE `users`
+    DROP COLUMN `can_manage_compounds`,
+    DROP COLUMN `can_manage_inventory_locations`;
 UPDATE config SET conf_value = 183 WHERE conf_name = 'schema';

cbbbba2a302e53a9edd76c718cf71f7084452843
Nicolas CARPi <nico-git@deltablot.email>
2025-09-26 21:08:39 +0200

    yep

--- src/ts/keymaster.ts
+++ src/ts/keymaster.ts
@@ -156,11 +156,16 @@ function resetModifiers() {
 };
 
 function filter(event){
-  const el = event.target as HTMLElement;
-  const tagName = el.tagName;
+  const target = event.target as HTMLElement;
+  if (!(target instanceof HTMLElement)) {
+    return true;
+  }
+  const tagName = target.tagName;
   // ignore keypressed in any elements that support keyboard data input
-  const isTyping = tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA' || el.hasAttribute('contenteditable');
-  const inSpreadsheet = !!el.closest('#spreadsheetEditor');
+  const isTyping = tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA' || target.hasAttribute('contenteditable');
+  //const inSpreadsheet = !!target.closest('#spreadsheetEditor');
+  // FIXME: it appears a bit difficult to correctly detect we are using the spreadsheet editor, so just disable stuff if the element is present
+  const inSpreadsheet = !!document.getElementById('spreadsheetEditor');
   return !(isTyping || inSpreadsheet);
 }
 

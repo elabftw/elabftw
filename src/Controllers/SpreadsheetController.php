a619709a8d370386205a14fd35d5836d0804a376
MoustaphaCamara <cmr.mous@gmail.com>
2025-09-30 17:51:19 +0200

    csp fix test

---
+++ src/Controllers/SpreadsheetController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2025 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+declare(strict_types=1);
+
+namespace Elabftw\Controllers;
+
+use Override;
+
+final class SpreadsheetController extends AbstractHtmlController
+{
+    #[Override]
+    protected function getTemplate(): string
+    {
+        return 'spreadsheet-editor.html';
+
+    }
+
+    #[Override]
+    protected function getPageTitle(): string
+    {
+        return _('Spreadsheet Editor');
+    }
+}

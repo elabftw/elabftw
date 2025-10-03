496ca1a08af12efb24864d6dfbc02d766a4520cb
MoustaphaCamara <cmr.mous@gmail.com>
2025-10-03 17:38:57 +0200

    abstract html controller

---
+++ tests/unit/controllers/AbstractHtmlControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @copyright 2024 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+namespace Elabftw\Controllers;
+
+use Elabftw\Traits\ControllerUtilsTrait;
+use Elabftw\Traits\TestsUtilsTrait;
+use ReflectionClass;
+
+class AbstractHtmlControllerTest extends \PHPUnit\Framework\TestCase
+{
+    use ControllerUtilsTrait;
+    use TestsUtilsTrait;
+
+    public function testGetResponse(): void
+    {
+        $ref = new ReflectionClass(AbstractHtmlController::class);
+        $this->assertTrue($ref->getMethod('getPageTitle')->isAbstract());
+        $this->assertTrue($ref->getMethod('getTemplate')->isAbstract());
+        $this->assertTrue($ref->getMethod('getData')->hasReturnType());
+        $this->assertSame('array', $ref->getMethod('getData')->getReturnType()->getName());
+    }
+}

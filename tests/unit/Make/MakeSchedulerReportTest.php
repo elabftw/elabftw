1d7ddbd85fdb837fd8115b41b794d77b5c59fe55
MoustaphaCamara <cmr.mous@gmail.com>
2025-10-13 18:17:52 +0200

    unit tests for make report

--- tests/unit/Make/MakeSchedulerReportTest.php
+++ tests/unit/Make/MakeSchedulerReportTest.php
@@ -14,8 +14,10 @@ namespace Elabftw\Make;
 use DateInterval;
 use DateTimeImmutable;
 use Elabftw\Enums\Action;
+use Elabftw\Exceptions\ImproperActionException;
 use Elabftw\Models\Scheduler;
 use Elabftw\Traits\TestsUtilsTrait;
+use Symfony\Component\HttpFoundation\InputBag;
 
 class MakeSchedulerReportTest extends \PHPUnit\Framework\TestCase
 {
@@ -44,5 +46,50 @@ class MakeSchedulerReportTest extends \PHPUnit\Framework\TestCase
     {
         $this->assertIsString($this->Make->getFileContent());
         $this->assertMatchesRegularExpression('/\d{4}-\d{2}-\d{2}-report.elabftw.csv/', $this->Make->getFileName());
+    }
+
+    public function testMakeSchedulerReportOnEmpty(): void
+    {
+        $Items = $this->getFreshBookableItem(2);
+        $Scheduler = new Scheduler($Items);
+        $this->expectException(ImproperActionException::class);
+        new MakeSchedulerReport($Scheduler)->getResponse();
+    }
+
+    public function testMakeSchedulerReportWithParams(): void
+    {
+        $Items = $this->getFreshBookableItem(2);
+        $Scheduler = new Scheduler($Items);
+        $title = 'for report';
+        $start = new DateTimeImmutable('+3 hour')->format('c');
+        $end = new DateTimeImmutable('+4 hour')->format('c');
+        // Add our event to the current existing ones
+        $Scheduler->postAction(Action::Create, array('start' => $start, 'end' => $end, 'title' => $title));
+        $q = $Scheduler->getQueryParams(new InputBag(array('items' => array($Items->id))));
+        $Make = new MakeSchedulerReport($Scheduler, $q);
+        $response = $Make->getResponse();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('text/csv; charset=UTF-8', $response->headers->get('Content-Type'));
+        // csv body
+        $csv = $Make->getFileContent();
+        $this->assertNotEmpty($csv, 'CSV response should not be empty');
+        // normalize and split lines
+        $lines = array_filter(array_map('trim', explode('\n', $csv)));
+        $this->assertGreaterThan(1, count($lines), 'CSV should contain header + at least one data line');
+        // parse header + first data row
+        $header = str_getcsv($lines[0]);
+        $row = str_getcsv($lines[1]);
+        // simple sanity check: header contains known columns
+        $this->assertContains('title_only', $header);
+        $this->assertContains('items_id', $header);
+        // extract indexes to check row values
+        $idxTitle = array_search('title_only', $header, true);
+        $idxItem = array_search('items_id', $header, true);
+        $idxUser = array_search('fullname', $header, true);
+        // assertions on the row data
+        $this->assertSame($title, $row[$idxTitle], 'Event title should match');
+        $this->assertSame((string) $Items->id, $row[$idxItem], 'Item ID should match');
+        $this->assertNotEmpty($row[$idxUser], 'User full name should be present');
     }
 }

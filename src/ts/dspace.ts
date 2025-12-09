62b9cc9a4421885a17e0adbc64a88024479a40da
MoustaphaCamara <cmr.mous@gmail.com>
2025-12-09 12:26:57 +0100

    tomSelect & remember last selected

--- src/ts/dspace.ts
+++ src/ts/dspace.ts
@@ -13,8 +13,10 @@ import { DspaceCollection, DspaceVocabularyEntry, listCollections, listTypes, sa
 import { on } from './handlers';
 import i18next from './i18n';
 import { FileType, Method } from './interfaces';
+import { rememberLastSelected, selectLastSelected } from './localStorage';
 import { notify } from './notify';
 import { entity } from './getEntity';
+import { TomSelect } from './misc';
 
 if (document.getElementById('dspaceExportModal')) {
   on('export-to-dspace', async () => {
@@ -56,20 +58,23 @@ if (document.getElementById('dspaceExportModal')) {
 }
 
 $('#dspaceExportModal').on('shown.bs.modal', async () => {
-  const collectionSelect = document.getElementById('dspaceCollection') as HTMLSelectElement;
-  const typeSelect = document.getElementById('dspaceType') as HTMLSelectElement;
-  collectionSelect.innerHTML = '<option disabled selected>' + i18next.t('loading') + '...</option>';
-  typeSelect.innerHTML = '<option disabled selected>' + i18next.t('loading') + '...</option>';
+  const collectionSelect = document.getElementById('dspaceCollection') as HTMLSelectElement & { tomselect?: TomSelect };
+  const typeSelect = document.getElementById('dspaceType') as HTMLSelectElement & { tomselect?: TomSelect };
+  collectionSelect.innerHTML = `<option disabled selected>${i18next.t('loading')}...</option>`;
+  typeSelect.innerHTML = `<option disabled selected>${i18next.t('loading')}...</option>`;
 
   try {
     const [collectionsJson, typesJson] = await Promise.all([
       listCollections(),
       listTypes(),
     ]);
-    // populate collections and types select for modal
     const collections = collectionsJson as DspaceCollection[];
     const types = typesJson._embedded.entries;
 
+    // clear existing TomSelect if any
+    if (collectionSelect?.tomselect) collectionSelect.tomselect.destroy();
+    if (typeSelect?.tomselect) typeSelect.tomselect.destroy();
+    // build select options
     collectionSelect.innerHTML = '';
     collections.forEach((col: DspaceCollection) => {
       const opt = document.createElement('option');
@@ -83,6 +88,14 @@ $('#dspaceExportModal').on('shown.bs.modal', async () => {
       opt.value = type.value;
       opt.textContent = type.display;
       typeSelect.appendChild(opt);
+    });
+    // Initialize TomSelect
+    ['dspaceCollection','dspaceType'].forEach(id => {
+      new TomSelect(`#${id}`, {
+        plugins: ['dropdown_input', 'no_active_items'],
+        onChange: rememberLastSelected(id),
+        onInitialize: selectLastSelected(id),
+      });
     });
   } catch (e) {
     collectionSelect.innerHTML = `<option disabled selected>${i18next.t('error-fetch-request', { error: 'Collections' })}</option>`;

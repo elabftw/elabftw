f662761476fa38e7bbb013d202eac815277ae1ff
Nicolas CARPi <nico-git@deltablot.email>
2025-12-19 00:13:12 +0100

    cleanup: get rid of dspaceUtils

--- src/ts/dspace.ts
+++ src/ts/dspace.ts
@@ -1,6 +1,6 @@
 /**
- * @author Nicolas CARPi <nico-git@deltablot.email>
- * @author Mouss <Deltablot>
+ * @author Nicolas CARPi #Deltablot
+ * @author Mouss #Deltablot
  * @copyright 2025 Nicolas CARPi
  * @see https://www.elabftw.net Official website
  * @license AGPL-3.0
@@ -9,15 +9,57 @@
  * All logic related to DSpace export modal. Located in toolbar on view/edit pages
  */
 import { ApiC } from './api';
-import { DspaceCollection, DspaceVocabularyEntryList, DspaceVocabularyEntry, saveDspaceIdAsExtraField } from './dspaceUtils';
 import i18next from './i18n';
 import { Action, FileType, Method } from './interfaces';
 import { rememberLastSelected, selectLastSelected } from './localStorage';
 import { notify } from './notify';
 import { entity } from './getEntity';
-import { TomSelect, collectForm, mkSpin, mkSpinStop } from './misc';
+import { TomSelect, collectForm, mkSpin, mkSpinStop, reloadElements } from './misc';
 import { on } from './handlers';
 import $ from 'jquery';
+import JsonEditorHelper from './JsonEditorHelper.class';
+import { Metadata } from './Metadata.class';
+import { ExtraFieldInputType, ValidMetadata } from './metadataInterfaces';
+
+async function saveDspaceIdAsExtraField(itemUuid: string): Promise<void> {
+  const MetadataC = new Metadata(entity, new JsonEditorHelper(entity));
+  const raw = await MetadataC.read();
+  const metadata = (raw || {}) as ValidMetadata;
+  if (!metadata.extra_fields) {
+    metadata.extra_fields = {};
+  }
+
+  metadata.extra_fields['DSpace Item Public URL'] = {
+    type: ExtraFieldInputType.Url,
+    value: itemUuid,
+    description: 'Link to item in DSpace repository',
+    readonly: true,
+  };
+
+  const mode = new URLSearchParams(window.location.search).get('mode');
+  await MetadataC.save(metadata).then(() => mode === 'edit'
+    ? MetadataC.display('edit')
+    : reloadElements(['extraFieldsDiv']));
+}
+
+interface DspaceCollection {
+  uuid: string;
+  name: string;
+  [key: string]: unknown;
+}
+
+interface DspaceVocabularyEntry {
+  value: string;
+  display: string;
+  [key: string]: unknown;
+}
+
+interface DspaceVocabularyEntryList {
+  _embedded: {
+    entries: DspaceVocabularyEntry[];
+  };
+  [key: string]: unknown;
+}
 
 on('export-to-dspace', async (el, event: Event) => {
   const btn = el as HTMLButtonElement;

13a6497813f8362d7cbddada911d2f0e3f1566d0
MoustaphaCamara <cmr.mous@gmail.com>
2025-12-19 17:36:39 +0100

    fix conflict

--- src/sql/schema195-down.sql
+++ src/sql/schema195-down.sql
@@ -1,4 +1,4 @@
--- revert schema 194
+-- revert schema 195
 ALTER TABLE `experiments_templates` MODIFY `body` TEXT;
 ALTER TABLE `items_types` MODIFY `body` TEXT;
-UPDATE config SET conf_value = 193 WHERE conf_name = 'schema';
+UPDATE config SET conf_value = 194 WHERE conf_name = 'schema';

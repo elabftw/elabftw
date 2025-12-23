84b23501bee36987ea0ca9c850b4efb79807ee85
MoustaphaCamara <cmr.mous@gmail.com>
2025-12-23 12:11:32 +0100

    schema fix

--- src/sql/schema196-down.sql
+++ src/sql/schema196-down.sql
@@ -2,4 +2,4 @@ ALTER TABLE `experiments` DROP COLUMN `hide_main_text`;
 ALTER TABLE `items` DROP COLUMN `hide_main_text`;
 ALTER TABLE `experiments_templates` DROP COLUMN `hide_main_text`;
 ALTER TABLE `items_types` DROP COLUMN `hide_main_text`;
-UPDATE config SET conf_value = 196 WHERE conf_name = 'schema';
+UPDATE config SET conf_value = 195 WHERE conf_name = 'schema';

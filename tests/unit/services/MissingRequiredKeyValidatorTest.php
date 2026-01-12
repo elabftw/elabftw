053eb1f84f71a7d888c632a984c1a3f844bca0ec
MoustaphaCamara <cmr.mous@gmail.com>
2026-01-12 19:01:35 +0100

    unit test for RequiredMissingKeyValidator

---
+++ tests/unit/services/MissingRequiredKeyValidatorTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Nicolas CARPi <nico-git@deltablot.email>
+ * @author Moustapha <Deltablot>
+ * @copyright 2026 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+namespace Elabftw\Services;
+
+use Elabftw\Exceptions\MissingRequiredKeyException;
+
+class MissingRequiredKeyValidatorTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAllRequiredKeysPresent(): void
+    {
+        ApiParamsValidator::ensureRequiredKeysPresent(
+            array('target_owner', 'target_team'),
+            array('target_owner' => 3, 'target_team' => 1),
+        );
+        $this->assertTrue(true);
+    }
+
+    public function testMissingRequiredKeyThrowsException(): void
+    {
+        $this->expectException(MissingRequiredKeyException::class);
+        ApiParamsValidator::ensureRequiredKeysPresent(
+            array('target_owner', 'target_team'),
+            array('target_owner' => 3),
+        );
+    }
+
+    public function testNullValueIsConsideredMissing(): void
+    {
+        $this->expectException(MissingRequiredKeyException::class);
+
+        ApiParamsValidator::ensureRequiredKeysPresent(
+            array('target_owner', 'target_team'),
+            array(
+                'target_owner' => 3,
+                'target_team' => null,
+            ),
+        );
+    }
+
+    public function testMultipleMissingKeys(): void
+    {
+        try {
+            ApiParamsValidator::ensureRequiredKeysPresent(
+                array('target_owner', 'target_team'),
+                array(),
+            );
+            $this->fail('Expected MissingRequiredKeyException was not thrown');
+        } catch (MissingRequiredKeyException $e) {
+            $this->assertStringContainsString('target_owner', $e->getMessage());
+            $this->assertStringContainsString('target_team', $e->getMessage());
+        }
+    }
+}

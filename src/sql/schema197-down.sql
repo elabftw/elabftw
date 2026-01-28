9045e4992e4f82fbb3ffc12de71457acfed1a561
MoustaphaCamara <cmr.mous@gmail.com>
2026-01-28 09:50:06 +0100

    schema

--- src/sql/schema197-down.sql
+++ src/sql/schema197-down.sql
@@ -1,4 +1,4 @@
 -- revert schema 197
 ALTER TABLE `users` DROP COLUMN `dark_mode`;
 -- TODO put 197 when hypernext gets the 197 schema from #6386
-UPDATE config SET conf_value = 197 WHERE conf_name = 'schema';
+UPDATE config SET conf_value = 196 WHERE conf_name = 'schema';

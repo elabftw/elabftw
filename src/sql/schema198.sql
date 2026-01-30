5d570ac8c7f0dec77e2c652a7651692443dd6088
Szilárd Andai <17235528+sandaihu@users.noreply.github.com>
2026-01-30 10:19:11 +0100

    column name fix

--- src/sql/schema198.sql
+++ src/sql/schema198.sql
@@ -1,6 +1,6 @@
 -- schema 198
 ALTER TABLE `items` ADD `book_price_notax` DECIMAL(10, 2) UNSIGNED NOT NULL DEFAULT 0.00;
 ALTER TABLE `items` ADD `book_price_tax` DECIMAL(10,2) UNSIGNED NOT NULL DEFAULT 0.00;
-ALTER TABLE `items` ADD `book_currency` TINYINT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `items` ADD `book_price_currency` TINYINT UNSIGNED NOT NULL DEFAULT 0;
 
 UPDATE config SET conf_value = 198 WHERE conf_name = 'schema';

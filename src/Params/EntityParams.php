8544cbbf9f0f19e9c2df78b236beb33b58f4d2b6
Szilárd Andai <17235528+sandaihu@users.noreply.github.com>
2026-02-03 10:07:11 +0100

    params and sql fixes

--- src/Params/EntityParams.php
+++ src/Params/EntityParams.php
@@ -27,18 +27,16 @@ final class EntityParams extends ContentParams implements ContentParamsInterface
         return match ($this->target) {
             'title' => Filter::title($this->asString()),
             // MySQL with throw an error if this param is incorrect
-            'date', 'metadata',
-            'is_procurable', 'proc_price_notax', 'proc_price_tax' => $this->getUnfilteredContent(), 'proc_currency' => Currency::from($this->asInt())->value,
+            'date', 'metadata', 'proc_price_notax', 'proc_price_tax', 'book_price_notax', 'book_price_tax' => $this->getUnfilteredContent(),
+            'proc_currency', 'book_price_currency' => Currency::from($this->asInt())->value,
             'body', 'bodyappend' => $this->getBody(),
             'canread', 'canwrite', 'canbook', 'canread_target', 'canwrite_target' => $this->getCanJson(),
             'canread_base', 'canwrite_base', 'canbook_base', 'canread_target_base', 'canwrite_target_base', 'canbook_target_base' => $this->getCanBase(),
             'color' => Check::color($this->asString()),
-            'is_bookable', 'book_max_minutes', 'book_max_slots', 'book_is_cancellable', 'book_cancel_minutes', 'book_can_overlap', 'book_users_can_in_past',
-            'book_price_notax', 'book_price_tax' => $this->getUnfilteredContent(), 'book_price_currency' => Currency::from($this->asInt())->value,
-            'content_type', 'proc_pack_qty', 'rating', 'userid', 'team' => $this->asInt(),
+            'book_max_minutes', 'book_max_slots', 'book_is_cancellable', 'book_cancel_minutes', 'content_type', 'proc_pack_qty', 'rating', 'userid', 'team' => $this->asInt(),
             'state' => $this->getState(),
             'custom_id', 'status', 'category', 'storage', 'qty_stored' => $this->getPositiveIntOrNull(),
-            'canread_is_immutable', 'canwrite_is_immutable', 'hide_main_text' => $this->getBinary(),
+            'is_procurable', 'book_can_overlap', 'book_users_can_in_past', 'is_bookable', 'canread_is_immutable', 'canwrite_is_immutable', 'hide_main_text' => $this->getBinary(),
             'qty_unit' => Check::unit($this->asString()),
             default => throw new ImproperActionException('Invalid update target.'),
         };

c056681e421649c9b0c438a3a5dc333bfb9666fd
MoustaphaCamara <cmr.mous@gmail.com>
2026-02-03 15:31:11 +0100

    theme change e2e test & rename other tests to follow camelCase

---
+++ tests/cypress/integration/themeChange.cy.ts
@@ -0,0 +1,26 @@
+describe('User changes theme', () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it('defaults to light mode', () => {
+    cy.clearCookies();
+    cy.get('html').should('not.have.class', 'dark-mode');
+  });
+
+  it('user changes theme to dark mode', () => {
+    cy.clearCookies();
+    cy.login();
+    cy.visit('/dashboard.php');
+    cy.get('#navbarDropdown').click();
+    cy.get('label[aria-label="Change theme"]').should('be.visible').click();
+    cy.get('.overlay').should('contain', 'Saved');
+    cy.get('html').should('have.class', 'dark-mode');
+
+    // it keeps dark mode after logout thanks to cookies
+    cy.get('#navbarDropdown').should('exist').click();
+    cy.get('[data-action="logout"]').click();
+    cy.location('pathname').should('include', '/login.php');
+    cy.get('html').should('have.class', 'dark-mode');
+  });
+});

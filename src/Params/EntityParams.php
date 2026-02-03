ba9393815e08c1aff9eb06f45ca5e5665454aa94
Szilárd Andai <17235528+sandaihu@users.noreply.github.com>
2026-02-03 09:48:38 +0100

    SQL and param fixes

--- src/Params/EntityParams.php
+++ src/Params/EntityParams.php
@@ -34,7 +34,7 @@ final class EntityParams extends ContentParams implements ContentParamsInterface
             'canread_base', 'canwrite_base', 'canbook_base', 'canread_target_base', 'canwrite_target_base', 'canbook_target_base' => $this->getCanBase(),
             'color' => Check::color($this->asString()),
             'is_bookable', 'book_max_minutes', 'book_max_slots', 'book_is_cancellable', 'book_cancel_minutes', 'book_can_overlap', 'book_users_can_in_past',
-            'book_price_notax', 'book_price_tax' => $this->getUnfilteredContent(), 'book_currency' => Currency::from($this->asInt())->value,
+            'book_price_notax', 'book_price_tax' => $this->getUnfilteredContent(), 'book_price_currency' => Currency::from($this->asInt())->value,
             'content_type', 'proc_pack_qty', 'rating', 'userid', 'team' => $this->asInt(),
             'state' => $this->getState(),
             'custom_id', 'status', 'category', 'storage', 'qty_stored' => $this->getPositiveIntOrNull(),
--- src/sql/schema198-down.sql
+++ src/sql/schema198-down.sql
@@ -1,6 +1,6 @@
 -- revert schema 198
-ALTER TABLE `items` DROP COLUMN `book_price_notax`
-ALTER TABLE `items` DROP COLUMN `book_price_tax`
-ALTER TABLE `items` DROP COLUMN `book_price_currency`
+ALTER TABLE `items` DROP COLUMN `book_price_notax`;
+ALTER TABLE `items` DROP COLUMN `book_price_tax`;
+ALTER TABLE `items` DROP COLUMN `book_price_currency`;
 
 UPDATE config SET conf_value = 197 WHERE conf_name = 'schema';

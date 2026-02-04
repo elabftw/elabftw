86b478d0691f1173db16a4993c63ab8753e58f4f
MoustaphaCamara <cmr.mous@gmail.com>
2026-02-04 15:32:12 +0100

    remove cypress flaky test

--- tests/cypress/integration/themeChange.cy.ts
+++ tests/cypress/integration/themeChange.cy.ts
@@ -7,19 +7,4 @@ describe('User changes theme', () => {
     cy.clearCookies();
     cy.get('html').should('not.have.class', 'dark-mode');
   });
-
-  it('user changes theme to dark mode', () => {
-    cy.clearCookies();
-    cy.login('titi@yopmail.com');
-    cy.visit('/dashboard.php');
-    cy.get('#navbarDropdown').click();
-    cy.get('[data-action="toggle-dark-mode"]').click();
-    cy.get('.overlay').should('contain', 'Saved');
-    cy.get('html').should('have.class', 'dark-mode');
-    // it keeps dark mode after logout thanks to cookies
-    cy.get('#navbarDropdown').should('exist').click();
-    cy.get('[data-action="logout"]').click();
-    cy.location('pathname').should('include', '/login.php');
-    cy.get('html').should('have.class', 'dark-mode');
-  });
 });

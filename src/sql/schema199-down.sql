79de92d16a392042f46f6f730c99ba9e84062d1b
Szilárd Andai <17235528+sandaihu@users.noreply.github.com>
2026-02-05 07:04:11 +0100

    changed -down to use DropColumn()

--- src/sql/schema199-down.sql
+++ src/sql/schema199-down.sql
@@ -1,5 +1,5 @@
 -- revert schema 199
-ALTER TABLE `items` DROP COLUMN `book_price_notax`;
-ALTER TABLE `items` DROP COLUMN `book_price_tax`;
-ALTER TABLE `items` DROP COLUMN `book_price_currency`;
+CALL DropColumn('items', 'book_price_notax');
+CALL DropColumn('items', 'book_price_tax');
+CALL DropColumn('items', 'book_price_currency');
 UPDATE config SET conf_value = 198 WHERE conf_name = 'schema';

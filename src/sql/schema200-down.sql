fe42bbb1f386dd23042f8485038055199f210e6e
MoustaphaCamara <cmr.mous@gmail.com>
2026-02-13 17:30:35 +0100

    schema to match latest hypernext updates

--- src/sql/schema200-down.sql
+++ src/sql/schema200-down.sql
@@ -1,5 +1,5 @@
--- revert schema 199
+-- revert schema 200
 CALL DropColumn('items', 'book_price_notax');
 CALL DropColumn('items', 'book_price_tax');
 CALL DropColumn('items', 'book_price_currency');
-UPDATE config SET conf_value = 198 WHERE conf_name = 'schema';
+UPDATE config SET conf_value = 199 WHERE conf_name = 'schema';
--- src/sql/schema200.sql
+++ src/sql/schema200.sql
@@ -1,5 +1,4 @@
--- schema 199
+-- schema 200
 ALTER TABLE `items` ADD `book_price_notax` DECIMAL(10, 2) UNSIGNED NOT NULL DEFAULT 0.00;
 ALTER TABLE `items` ADD `book_price_tax` DECIMAL(10,2) UNSIGNED NOT NULL DEFAULT 0.00;
 ALTER TABLE `items` ADD `book_price_currency` TINYINT UNSIGNED NOT NULL DEFAULT 0;
-UPDATE config SET conf_value = 199 WHERE conf_name = 'schema';

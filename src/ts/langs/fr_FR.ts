9826cff18d11291a721457080654e81535e55263
Nicolas CARPi <nico-git@deltablot.email>
2026-02-15 17:52:58 +0100

    i18n: update terms and french

--- src/ts/langs/fr_FR.ts
+++ src/ts/langs/fr_FR.ts
@@ -14,7 +14,7 @@ const t = {
     "can-manage-users2teams": "Peut gérer les utilisateurs en équipes",
     "click-to-edit": "Cliquer pour modifier",
     "cancel": "Annuler",
-    "categories": "Categories",
+    "categories": "Catégories",
     "clear": "Effacer",
     "copied": "Copié dans le presse-papiers.",
     "check-required": "Veuillez vérifier les champs obligatoires.",

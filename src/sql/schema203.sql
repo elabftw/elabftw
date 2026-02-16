682b9fd067aaec74385ada08b69d07a91a6ced4c
MoustaphaCamara <cmr.mous@gmail.com>
2026-02-16 15:47:43 +0100

    schema023: add book_limit_days_in_advance and book_maximum_days_in_advance, openapi spec

---
+++ src/sql/schema203.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `items` ADD `book_limit_days_in_advance` TINYINT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `items` ADD `book_maximum_days_in_advance` TINYINT UNSIGNED NOT NULL DEFAULT 0;

f0b6cbc4d3cb8d5cc36cdfba6b74de322545fff7
Nicolas CARPi <nico-git@deltablot.email>
2026-02-23 11:20:07 +0100

    don't modify modified_at

--- src/sql/schema203.sql
+++ src/sql/schema203.sql
@@ -1,2 +1,2 @@
 -- schema 203
-update items_types set canbook = canread where json_unquote(canbook) = 'null';
+update items_types set canbook = canread, modified_at = modified_at where json_unquote(canbook) = 'null';

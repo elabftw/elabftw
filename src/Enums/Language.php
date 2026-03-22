6b8c3cd88d6c8efb62e43461ea4cf4474377dc0e
Nicolas CARPi <3043706+NicolasCARPi@users.noreply.github.com>
2026-03-22 12:45:57 +0900

    feat: i18n: add traditional chinese (#6623)
    fix #6530. Thanks to  劉柏均 for help with the translation.

--- src/Enums/Language.php
+++ src/Enums/Language.php
@@ -40,6 +40,7 @@ enum Language: string
     case Slovak = 'sk_SK';
     case Uzbek = 'uz_UZ';
     case Chinese = 'zh_CN';
+    case ChineseTraditional = 'zh_TW';
 
     public static function getAllHuman(): array
     {
@@ -75,6 +76,7 @@ enum Language: string
             self::Slovak => 'sk',
             self::Uzbek => 'uz',
             self::Chinese => 'zh-cn',
+            self::ChineseTraditional => 'zh-tw',
         };
     }
 
@@ -104,6 +106,7 @@ enum Language: string
             self::Slovak => 'Slovak',
             self::Uzbek => 'Uzbek',
             self::Chinese => 'Chinese Simplified',
+            self::ChineseTraditional => 'Chinese Traditional',
         };
     }
 }

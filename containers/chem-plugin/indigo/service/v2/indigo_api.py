65c5723af23d029492715ab3f2dcea1a30b7598c
Nicolas CARPi <nico-git@deltablot.email>
2026-04-07 23:29:34 +0200

    better text

--- containers/chem-plugin/indigo/service/v2/indigo_api.py
+++ containers/chem-plugin/indigo/service/v2/indigo_api.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*
-# This file was modified for eLabFTW
-# Apache 2.0 LICENSE
+# Copyright 2026 Nicolas CARPI - Deltablot
+# Originally from epam/indigo (Apache 2.0)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import base64
 import collections

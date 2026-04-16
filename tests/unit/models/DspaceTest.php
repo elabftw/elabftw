073bd5a5124de9da20b44cb7d1033580f4ea635c
MoustaphaCamara <cmr.mous@gmail.com>
2026-04-16 11:19:54 +0200

    updat etests for readall

--- tests/unit/models/DspaceTest.php
+++ tests/unit/models/DspaceTest.php
@@ -12,22 +12,23 @@ declare(strict_types=1);
 
 namespace Elabftw\Models;
 
-use Elabftw\Enums\EntityType;
-use GuzzleHttp\Handler\MockHandler;
 use Elabftw\Enums\Action;
 use Elabftw\Enums\DSpaceAction;
+use Elabftw\Enums\EntityType;
 use Elabftw\Exceptions\ImproperActionException;
 use Elabftw\Models\Users\Users;
-use Elabftw\Traits\TestsUtilsTrait;
 use Elabftw\Services\HttpGetter;
+use Elabftw\Traits\TestsUtilsTrait;
 use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\InputBag;
 
 use function json_encode;
 
-class DspaceTest extends \PHPUnit\Framework\TestCase
+class DspacePaginationTest extends TestCase
 {
     use TestsUtilsTrait;
 
@@ -45,39 +46,83 @@ class DspaceTest extends \PHPUnit\Framework\TestCase
     {
         $this->assertSame('api/v2/dspace/', $this->dspace->getApiPath());
     }
-
-    public function testReadAllWithGetCollections(): void
-    {
-        $result = $this->runReadAllTest(DSpaceAction::GetCollections, array(
-            '_embedded' => array(
-                'collections' => array(
-                    array('uuid' => 'abc-123', 'name' => 'Collection One'),
-                    array('uuid' => 'def-456', 'name' => 'Collection Two'),
-                ),
-            ),
-            'page' => array('totalPages' => 1),
-        ));
+    /**
+     * @dataProvider providePaginatedEndpoints
+     */
+    public function testPaginatedEndpoints(DSpaceAction $action, string $embeddedKey, array $pages, callable $assertion): void {
+        $result = $this->runPaginatedReadAllTest($action, $embeddedKey, $pages);
         $this->assertIsArray($result);
-        $this->assertCount(2, $result);
-        $this->assertEquals('abc-123', $result[0]['uuid']);
-        $this->assertEquals('Collection One', $result[0]['name']);
+        $assertion($this, $result);
     }
 
-    public function testReadAllWithGetTypesAction(): void
+    public static function providePaginatedEndpoints(): array
     {
-        $result = $this->runReadAllTest(DSpaceAction::GetTypes, array(
-            '_embedded' => array(
-                'entries' => array(
-                    array('value' => 'article', 'display' => 'Article'),
-                    array('value' => 'book', 'display' => 'Book'),
-                ),
-            ),
-            'page' => array('totalPages' => 1),
-        ));
-        $this->assertIsArray($result);
-        $this->assertCount(2, $result);
-        $this->assertEquals('article', $result[0]['value']);
-        $this->assertEquals('Article', $result[0]['display']);
+        return [
+            // COLLECTIONS
+            'collections single page' => [
+                DSpaceAction::GetCollections, 'collections',
+                [[
+                    ['uuid' => 'abc-123', 'name' => 'Collection One'],
+                    ['uuid' => 'def-456', 'name' => 'Collection Two'],
+                ]],
+                function (TestCase $test, array $result) {
+                    $test->assertCount(2, $result);
+                    $test->assertSame('abc-123', $result[0]['uuid']);
+                    $test->assertSame('Collection One', $result[0]['name']);
+                }
+            ],
+            'collections multi page' => [
+                DSpaceAction::GetCollections, 'collections',
+                [
+                    [['uuid' => 'abc-123', 'name' => 'Collection One']],
+                    [['uuid' => 'def-456', 'name' => 'Collection Two']],
+                ],
+                function (TestCase $test, array $result) {
+                    $test->assertCount(2, $result);
+                    $test->assertSame('abc-123', $result[0]['uuid']);
+                    $test->assertSame('def-456', $result[1]['uuid']);
+                }
+            ],
+            // TYPES
+            'types single page' => [
+                DSpaceAction::GetTypes, 'entries',
+                [[
+                    ['value' => 'article', 'display' => 'Article'],
+                    ['value' => 'book', 'display' => 'Book'],
+                ]],
+                function (TestCase $test, array $result) {
+                    $test->assertCount(2, $result);
+                    $test->assertSame('article', $result[0]['value']);
+                    $test->assertSame('Article', $result[0]['display']);
+                }
+            ],
+            'types multi page' => [
+                DSpaceAction::GetTypes, 'entries',
+                [
+                    [['value' => 'article']],
+                    [['value' => 'book']],
+                ],
+                function (TestCase $test, array $result) {
+                    $test->assertCount(2, $result);
+                    $test->assertSame('article', $result[0]['value']);
+                    $test->assertSame('book', $result[1]['value']);
+                }
+            ],
+        ];
+    }
+
+    private function runPaginatedReadAllTest(DSpaceAction $action, string $embeddedKey, array $pages): array {
+        $responses = [];
+        foreach ($pages as $entries) {
+            $responses[] = new Response(200, [], json_encode([
+                '_embedded' => [$embeddedKey => $entries],
+                'page' => ['totalPages' => count($pages)],
+            ]) ?: '{}');
+        }
+        $this->setMockResponses($responses);
+        $queryParams = new InputBag(['action' => $action->value]);
+        $q = $this->dspace->getQueryParams($queryParams);
+        return $this->dspace->readAll($q);
     }
 
     public function testReadAllWithUnhandledAction(): void
@@ -124,27 +169,16 @@ class DspaceTest extends \PHPUnit\Framework\TestCase
         $this->assertSame('1234-uuid', $res['uuid']);
     }
 
-    private function runReadAllTest(DSpaceAction $action, array $responseBody): array
-    {
-        $this->setMockResponses(array(new Response(200, array(), json_encode($responseBody) ?: '{}')));
-        $queryParams = new InputBag(array('action' => $action->value));
-        $q = $this->dspace->getQueryParams($queryParams);
-        return $this->dspace->readAll($q);
-    }
-
-    // create a mocked Dspace instance with custom HTTP responses
-    // optional password to test patch method
-    private function setMockResponses(array $responses, ?string $password = null): void
+    private function setMockResponses(array $responses): void
     {
         $mock = new MockHandler($responses);
         $handlerStack = HandlerStack::create($mock);
-        $encPassword = $password ?? 'encPasswordDummy';
-        $this->initDspace(new Client(array('handler' => $handlerStack)), $encPassword);
+        $this->initDspace(new Client(['handler' => $handlerStack]));
     }
 
-    private function initDspace(Client $client, string $password = 'encPasswordDummy'): void
+    private function initDspace(Client $client): void
     {
         $httpGetter = new HttpGetter($client);
-        $this->dspace = new Dspace($this->requester, $httpGetter, 'https://dspace.example.org/', 'user', $password);
+        $this->dspace = new Dspace($this->requester, $httpGetter, 'https://dspace.example.org/', 'user', 'password');
     }
 }

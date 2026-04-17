c622119b688d9f5610f633366d2481b4515aad6e
MoustaphaCamara <cmr.mous@gmail.com>
2026-04-17 14:24:54 +0200

    rephrase

--- tests/unit/models/ExperimentsTest.php
+++ tests/unit/models/ExperimentsTest.php
@@ -276,9 +276,8 @@ class ExperimentsTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($canread->value, $new->entityData['canread_base']);
         $this->assertEquals($canwrite->value, $new->entityData['canwrite_base']);
         $this->assertEquals(1, $new->entityData['hide_main_text']);
-        // assert only active files are duplicated
-        $newUploads = $new->Uploads->readAll();
-        $this->assertCount(1, $newUploads);
+        // only active files are duplicated
+        $this->assertCount(1, $new->Uploads->readAll());
     }
 
     public function testInsertTags(): void

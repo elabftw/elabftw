9d9cc91792fda9c136205624c28d8811b8494d99
eltouma <elsa@deltablot.email>
2026-06-05 18:18:45 +0200

    Add test

--- tests/unit/models/RequestActionsTest.php
+++ tests/unit/models/RequestActionsTest.php
@@ -15,6 +15,7 @@ namespace Elabftw\Models;
 use Elabftw\Enums\Action;
 use Elabftw\Enums\RequestableAction;
 use Elabftw\Exceptions\ImproperActionException;
+use Elabftw\Exceptions\ResourceNotFoundException;
 use Elabftw\Models\Users\Users;
 use Elabftw\Traits\TestsUtilsTrait;
 
@@ -105,6 +106,24 @@ class RequestActionsTest extends \PHPUnit\Framework\TestCase
         );
         $this->RequestActions->setId($id);
         $this->assertIsArray($this->RequestActions->readOne());
+    }
+
+    public function testCannotReadOneFromAnotherExperiment(): void
+    {
+        $targetUser = $this->getUserInTeam(2);
+        $reqBody = array(
+            'target_userid' => $targetUser->userid,
+            'target_action' => RequestableAction::Archive->value,
+        );
+        $id = $this->RequestActions->postAction(
+            Action::Create, // this action is irrelevant
+            $reqBody,
+        );
+        $OtherExperiments = $this->getFreshExperiment();
+        $OtherRequestActions = new RequestActions($targetUser, $OtherExperiments);
+        $OtherRequestActions->setId($id);
+        $this->expectException(ResourceNotFoundException::class);
+        $OtherRequestActions->readOne();
     }
 
     public function testGetApiPath(): void

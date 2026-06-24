bc4e602153dfd001044667ad332a29a79b74ae3e
MoustaphaCamara <cmr.mous@gmail.com>
2026-06-24 13:51:11 +0200

    changelog & comment out timestamp test

--- tests/unit/services/TimestampUtilsTest.php
+++ tests/unit/services/TimestampUtilsTest.php
@@ -29,39 +29,41 @@ class TimestampUtilsTest extends \PHPUnit\Framework\TestCase
 {
     use TestsUtilsTrait;
 
-    private Filesystem $fixturesFs;
+    //    private Filesystem $fixturesFs;
 
-    protected function setUp(): void
+    //    protected function setUp(): void
+    //    {
+    //        $this->fixturesFs = Storage::FIXTURES->getStorage()->getFs();
+    //    }
+
+    // unit fails: see https://github.com/elabftw/elabftw/issues/6613
+    /*
+    public function testTimestamp(): void
     {
-        $this->fixturesFs = Storage::FIXTURES->getStorage()->getFs();
-    }
+        $mockResponse = $this->fixturesFs->read('dfn.asn1');
+        $client = $this->getClient($mockResponse);
 
-    /* unit fails: see https://github.com/elabftw/elabftw/issues/6613 */
-//    public function testTimestamp(): void
-//    {
-//        $mockResponse = $this->fixturesFs->read('dfn.asn1');
-//        $client = $this->getClient($mockResponse);
-//
-//        $Maker = new MakeDfnTimestamp(
-//            new Users(1, 1),
-//            $this->getFreshExperiment(),
-//            array(),
-//            ExportFormat::Json,
-//        );
-//        $pdfBlob = $this->fixturesFs->read('dfn.pdf');
-//        $tsUtils = new TimestampUtils($client, $pdfBlob, $Maker->getTimestampParameters(), new TimestampResponse());
-//        $this->assertInstanceOf(TimestampResponse::class, $tsUtils->timestamp());
-//    }
-
-    private function getClient(string $mockResponse): Client
-    {
-        // don't use the real guzzle client, but use a mock
-        // https://docs.guzzlephp.org/en/stable/testing.html
-        $mock = new MockHandler(array(
-            new Response(200, array(), $mockResponse),
-            new RequestException('Server is down?', new Request('GET', 'test')),
-        ));
-        $handlerStack = HandlerStack::create($mock);
-        return new Client(array('handler' => $handlerStack));
+        $Maker = new MakeDfnTimestamp(
+            new Users(1, 1),
+            $this->getFreshExperiment(),
+            array(),
+            ExportFormat::Json,
+        );
+        $pdfBlob = $this->fixturesFs->read('dfn.pdf');
+        $tsUtils = new TimestampUtils($client, $pdfBlob, $Maker->getTimestampParameters(), new TimestampResponse());
+        $this->assertInstanceOf(TimestampResponse::class, $tsUtils->timestamp());
     }
+    */
+
+    //    private function getClient(string $mockResponse): Client
+    //    {
+    //        // don't use the real guzzle client, but use a mock
+    //        // https://docs.guzzlephp.org/en/stable/testing.html
+    //        $mock = new MockHandler(array(
+    //            new Response(200, array(), $mockResponse),
+    //            new RequestException('Server is down?', new Request('GET', 'test')),
+    //        ));
+    //        $handlerStack = HandlerStack::create($mock);
+    //        return new Client(array('handler' => $handlerStack));
+    //    }
 }

0319150f6bb546a7c4599ee828f392e86241522f
MoustaphaCamara <cmr.mous@gmail.com>
2026-06-30 15:38:19 +0200

    feat: compounds-import: add tags to created resources
    When importing compounds with the import:compounds command, create tags if they exist in the csv.

--- src/Import/CompoundsCsv.php
+++ src/Import/CompoundsCsv.php
@@ -19,6 +19,7 @@ use Elabftw\Models\Links\Compounds2ItemsLinks;
 use Elabftw\Models\Links\Containers2ItemsLinks;
 use Elabftw\Models\Items;
 use Elabftw\Models\StorageUnits;
+use Elabftw\Models\Tags;
 use Elabftw\Params\DisplayParams;
 use Elabftw\Params\EntityParams;
 use Elabftw\Services\PubChemImporter;
@@ -124,6 +125,10 @@ final class CompoundsCsv extends AbstractCsv
                     $this->Items->setId($resource);
                     if (isset($row['comment'])) {
                         $this->Items->update(new EntityParams('body', $row['comment']));
+                    }
+                    if (isset($row['tags']) && trim($row['tags']) !== '') {
+                        $Tags = new Tags($this->Items);
+                        $Tags->postAction(Action::Create, array('tag' => trim($row['tags'])));
                     }
                     $this->Items->update(new EntityParams('metadata', $this->collectMetadata($row)));
                     foreach ($ids as $id) {

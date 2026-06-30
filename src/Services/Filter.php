8629a564547ae575b417d6de2c7ebf67d1660094
Moritz R. Schaefer <moritzrichard.schaefer@outlook.de>
2026-06-30 16:17:52 +0200

    avoid code duplication

--- src/Services/Filter.php
+++ src/Services/Filter.php
@@ -193,13 +193,7 @@ final class Filter
      */
     public static function body(?string $input = null): string
     {
-        if ($input === null) {
-            return '';
-        }
-        // use strlen() instead of mb_strlen() because we want the size in bytes
-        if (strlen($input) > self::MAX_BODY_SIZE) {
-            throw new ImproperActionException('Content is too big! Cannot save!');
-        }
+        $input = self::validateBodySize($input);
         // create base config for html5
         $config = HTMLPurifier_HTML5Config::createDefault();
         // allow only certain elements
@@ -274,6 +268,11 @@ final class Filter
     }
 
     public static function bodyMarkdown(?string $input = null): string
+    {
+        return self::validateBodySize($input);
+    }
+
+    private static function validateBodySize(?string $input): string
     {
         if ($input === null) {
             return '';

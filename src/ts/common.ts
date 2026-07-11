elabftw/elabftw

commit 5601886751a4230a3d488b1ce6a3e90f39b320c9

feat/ui: make navbars sticky and add save btn in toolbar (#7010)
---------

Co-authored-by: MoustaphaCamara <cmr.mous@gmail.com>
Co-authored-by: Nicolas CARPi <nico-git@deltablot.email>

src/scss/main.scss
@@ -1942,3 +1942,26 @@ input:user-invalid {
   height: 128px;
   width: 128px;
 }
+
+:root {
+  --navbar-height: 0;
+}
+
+div > .sticky-navbar {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 100;
+}
+
+.sticky-toolbar {
+  background: var(--mainbackground);
+  position: sticky;
+  top: var(--navbar-height);
+  width: 100%;
+  z-index: 50;
+}
+
+.sticky-dropdown > #navbar-create-dropdown {
+  z-index: 1001;
+}

src/templates/head.html
@@ -44,7 +44,7 @@
 <div id='container' class='container-fluid'>
 
 {# begin main menu #}
-<nav aria-label='{{ 'Main menu'|trans }}' class='navbar navbar-expand-lg navbar-dark'>
+<nav aria-label='{{ 'Main menu'|trans }}' class='navbar navbar-expand-lg navbar-dark sticky-navbar'>
     {% if App.Session.has('is_auth') %}
     {# hamburger menu #}
     <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#main-nav' aria-controls='main-nav' aria-expanded='false' aria-label='{{ 'Toggle navigation'|trans }}'>
@@ -180,7 +180,7 @@
         <div class='ml-auto mt-2 d-flex'>
         <div class='nav-item dropdown ml-auto'>
           <button class='btn nav-link' id='createNewDropdown' type='button' aria-label='{{ 'Create new...'|trans }}' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'><i class='far fa-plus fa-2x'></i></button>
-          <div class='dropdown-menu dropdown-menu-right' id='navbar-create-dropdown'><span class='dropdown-header'>{{ 'Create new...'|trans }}</span>
+          <div class='dropdown-menu dropdown-menu-right sticky-dropdown' id='navbar-create-dropdown'><span class='dropdown-header'>{{ 'Create new...'|trans }}</span>
             <div data-action='toggle-create-modal' data-type='experiments' class='dropdown-item'><i class='fas fa-flask fa-fw mr-1'></i>{{ 'Experiment'|trans }}</div>
             <div data-action='toggle-create-modal' data-type='items' class='dropdown-item'><i class='fas fa-cubes-stacked fa-fw mr-1'></i>{{ 'Resource'|trans }}</div>
           </div>

src/ts/common.ts
@@ -79,6 +79,9 @@ interface Status extends SelectOptions {
   is_current_team: number;
 }
 
+// Use the sticky navbar height to offset the toolbar below it.
+document.documentElement.style.setProperty('--navbar-height', `${document.querySelector<HTMLElement>('div > .navbar')?.offsetHeight ?? 0}px`);
+
 on('toggle-dark-mode', (el: HTMLElement) => {
   const currentTheme = parseInt(el.dataset.currentTheme, 10);
   // Auto (0) and Light (1) should both toggle to Dark (2)
@@ -1040,13 +1043,14 @@ export function showModalAndFocusFirstInput(modalSelector: string) {
   $(modalSelector).modal('show');
 }
 
-on('update-entity-body', (el: HTMLElement) => {
-  updateEntityBody().then(() => {
-    // SAVE AND GO BACK BUTTON
-    if (el.matches('[data-redirect="view"]')) {
-      window.location.replace('?mode=view&id=' + entity.id);
-    }
-  });
+on('update-entity-body', async (el: HTMLElement) => {
+  const redirect = el.dataset.redirect === 'view';
+  await updateEntityBody(redirect);
+  // SAVE AND GO BACK BUTTON
+  if (redirect) {
+    sessionStorage.setItem('flash_saved', i18next.t('saved'));
+    window.location.replace('?mode=view&id=' + entity.id);
+  }
 });
 
 on('search-pubchem', (el: HTMLElement) => {

src/ts/create-new.ts
@@ -129,6 +129,8 @@ on('filter-category', (el: HTMLElement) => {
 });
 
 on('toggle-create-modal', async (el: HTMLElement) => {
+  const navbar = document.querySelector<HTMLElement>('.sticky-navbar');
+  navbar?.style.setProperty('position', 'static');
   // allow data-type to override selected type (for instance on dashboard)
   const entityType = el.dataset.type ? el.dataset.type as EntityType : getEntityTypeFromPage(window.location);
   setTypeRadio(entityType);
@@ -140,6 +142,9 @@ on('toggle-create-modal', async (el: HTMLElement) => {
     $('#editCompoundModal').modal('hide');
   }
 
+  $('#createModal').one('hidden.bs.modal', () => {
+    navbar?.style.removeProperty('position');
+  });
   showModalAndFocusFirstInput('#createModal');
 });
 

src/templates/edit.html
@@ -101,11 +101,6 @@
           <span id='lastSavedAt' class='ml-1 relative-moment smallgray' title='{{ Entity.entityData.modified_at }}'></span>
         </div>
       </div>
-
-      <div class='mt-1 text-center'>
-        <button type='button' data-action='update-entity-body' class='btn btn-primary'>{{ 'Save'|trans }}</button>
-        <button type='button' data-action='update-entity-body' data-redirect='view' class='btn btn-secondary'>{{ 'Save and go back'|trans }}</button>
-      </div>
   </div>
   {% endif %}
 </section>

src/ts/misc.ts
@@ -506,7 +506,16 @@ export async function reloadElements(elementIds: string[]): Promise<void> {
     return;
   }
 
-  const html = await fetchCurrentPage();
+  let html: Document;
+  try {
+    html = await fetchCurrentPage();
+  } catch (error) {
+    // Ignore fetch failures caused by navigating away while reloading page fragments.
+    if (error instanceof TypeError && error.message.includes('NetworkError')) {
+      return;
+    }
+    throw error;
+  }
   elementIds.forEach(elementId => {
     const currentElement = document.getElementById(elementId);
     const newElement = html.getElementById(elementId);
@@ -823,18 +832,21 @@ export async function saveStringAsFile(filename: string, content: string|Promise
 }
 
 // Shared function to UPDATE ENTITY BODY via save shortcut and/or save button, or autosave
-export async function updateEntityBody(): Promise<void> {
+export async function updateEntityBody(redirect = true): Promise<void> {
   const editor = getEditor();
   const entity = getEntity();
-  return ApiC.patch(`${entity.type}/${entity.id}`, {body: editor.getContent()}).then(response => response.json()).then(json => {
+
+  return ApiC.patch(`${entity.type}/${entity.id}`, {body: editor.getContent(), notifOnSaved: redirect ? 0 : 1}).then(response => response.json()).then(json => {
     if (editor.type === 'tiny') {
       // set the editor as non dirty so we can navigate out without a warning to clear
       tinymce.activeEditor.setDirty(false);
     }
-    const lastSavedAt = document.getElementById('lastSavedAt');
-    if (lastSavedAt) {
-      lastSavedAt.title = json.modified_at;
-      reloadElements(['lastSavedAt']);
+    if (!redirect) {
+      const lastSavedAt = document.getElementById('lastSavedAt');
+      if (lastSavedAt) {
+        lastSavedAt.title = json.modified_at;
+        reloadElements(['lastSavedAt']);
+      }
     }
   }).catch(() => {
     // detect if the session timedout (Session expired error is thrown)

src/ts/tinymce.ts
@@ -523,6 +523,7 @@ export function getTinymceBaseConfig(page: string): object {
       },
     ],
     toolbar_sticky: true,
+    toolbar_sticky_offset: document.querySelector<HTMLElement>('.sticky-toolbar')?.offsetHeight ?? 0,
     // render MathJax for TinyMCE preview
     init_instance_callback: (editor) => {
       editor.on('ExecCommand', (e) => {

commit fc13db7dd40cd1319760dca2b9895e0740f03903

bug/major: exports: fix export modal window broken
Fix bug introduced in #7010 impacting modal windows.

Make sure the modal is direct child of #container element and cleanup
previously introduced partial fix for this issue.

fix #7060

src/scss/main.scss
@@ -1971,7 +1971,3 @@ div > .sticky-navbar {
   width: 100%;
   z-index: 50;
 }
-
-.sticky-dropdown > #navbar-create-dropdown {
-  z-index: 1001;
-}

src/templates/head.html
@@ -255,7 +255,6 @@
         </div>
      </div>
     </div>
-    {% include 'create-new-modal.html' %}
   {% else %} {# not auth #}
     <div class='nav-item ml-auto'>
       <a href='#' class='btn btn-ghost-chrome-inverted' data-action='toggle-modal' data-target='selectLanguageModal'>
@@ -267,6 +266,7 @@
 
 {# SHOW FAVTAGS/TODOLIST PANELS #}
 {% if App.Session.has('is_auth') and not App.Session.has('is_anon') %}
+  {% include 'create-new-modal.html' %}
   <div class='pr-2 mt-3 float-left d-flex flex-column' id='sidepanel-buttons'>
     <button type='button' class='bounce-right sidepanel-btn sidepanel-closed' id='favtagsPanelOpener' data-action='toggle-sidepanel' data-target='favtags' aria-expanded='false' aria-controls='favtagsPanel' title='{{ 'Toggle favorite tags'|trans }}' aria-label='{{ 'Toggle favorite tags'|trans }}'>
       <i class='fas fa-folder-tree'></i>

src/ts/common.ts
@@ -82,6 +82,8 @@ interface Status extends SelectOptions {
 // Use the sticky navbar height to offset the toolbar below it.
 document.documentElement.style.setProperty('--navbar-height', `${document.querySelector<HTMLElement>('div > .navbar')?.offsetHeight ?? 0}px`);
 
+const container = document.getElementById('container')!;
+
 on('toggle-dark-mode', (el: HTMLElement) => {
   const currentTheme = parseInt(el.dataset.currentTheme, 10);
   // Auto (0) and Light (1) should both toggle to Dark (2)
@@ -1038,9 +1040,20 @@ function focusFirstTextInputOnShown(modalSelector: string) {
   });
 }
 
+function ensureModalIsContainerChild(modal: HTMLElement): void {
+  // we do that to avoid issues with sticky navbar/z-index
+  if (modal.parentElement !== document.body) {
+    container.appendChild(modal);
+  }
+}
+
 export function showModalAndFocusFirstInput(modalSelector: string) {
+  const modal = document.querySelector<HTMLElement>(modalSelector);
+  if (!modal) return;
+
+  ensureModalIsContainerChild(modal);
   focusFirstTextInputOnShown(modalSelector);
-  $(modalSelector).modal('show');
+  $(modal).modal('show');
 }
 
 on('update-entity-body', async (el: HTMLElement) => {
@@ -1540,7 +1553,6 @@ on('scope-change', async (el: HTMLElement) => {
 /**
  * MAIN click listener on container
  */
-const container = document.getElementById('container')!;
 container.addEventListener('click', (event: Event) => {
   const rawTarget = event.target as HTMLElement | null;
   const el = rawTarget?.closest('[data-action]') as HTMLElement | null;

src/ts/create-new.ts
@@ -129,8 +129,6 @@ on('filter-category', (el: HTMLElement) => {
 });
 
 on('toggle-create-modal', async (el: HTMLElement) => {
-  const navbar = document.querySelector<HTMLElement>('.sticky-navbar');
-  navbar?.style.setProperty('position', 'static');
   // allow data-type to override selected type (for instance on dashboard)
   const entityType = el.dataset.type ? el.dataset.type as EntityType : getEntityTypeFromPage(window.location);
   setTypeRadio(entityType);
@@ -142,9 +140,6 @@ on('toggle-create-modal', async (el: HTMLElement) => {
     $('#editCompoundModal').modal('hide');
   }
 
-  $('#createModal').one('hidden.bs.modal', () => {
-    navbar?.style.removeProperty('position');
-  });
   showModalAndFocusFirstInput('#createModal');
 });
 
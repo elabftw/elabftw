85fcd09fe054f9ac64be7e9407248f4bc9741bb6
Mouss <104678778+MoustaphaCamara@users.noreply.github.com>
2026-07-15 13:52:18 +0200

    bug/medium: metadata cst fields: fix the ux in edit mode (#7096)
    There's a visually disturbing flash that happens when removing a field or editing or adding. It's because it was clearing the html with a '' to rebuild the content.
    
    * fix #6347 - position is preserved on updating fields
    
    * use block:center to focus center of page with the desired element

--- src/ts/metadata.ts
+++ src/ts/metadata.ts
@@ -351,7 +351,10 @@ if (document.getElementById('metadataDiv') && entity.id) {
             // and finally close the modal
             $('#fieldBuilderModal').modal('toggle');
             // focus on the newly added element
-            document.querySelector(`[data-name="${fieldKey}"`).scrollIntoView({behavior: 'smooth'});
+            document.querySelector(`#metadataDiv [data-name="${CSS.escape(fieldKey)}"]`)?.scrollIntoView({
+              behavior: 'smooth',
+              block: 'center',
+            });
           });
         });
       // EDIT EXTRA FIELD
@@ -433,8 +436,9 @@ if (document.getElementById('metadataDiv') && entity.id) {
             const el = document.getElementById(id) as HTMLInputElement | null;
             if (el?.checked) field[key] = true;
           }
-          // preserve readonly
+          // preserve properties that aren't editable in this modal
           if (prevField?.readonly === true) field['readonly'] = true;
+          if (typeof prevField?.position === 'number') field['position'] = prevField.position;
           // ensure the old extra field is replaced
           if (!json['extra_fields']) json['extra_fields'] = {};
           if (originalFieldKey && originalFieldKey !== newFieldKey) {
@@ -445,7 +449,10 @@ if (document.getElementById('metadataDiv') && entity.id) {
           MetadataC.update(json as ValidMetadata).then(() => {
             $('#fieldBuilderModal').modal('toggle');
             // focus on the newly added element
-            document.querySelector(`[data-name="${newFieldKey}"]`).scrollIntoView({behavior: 'smooth'});
+            document.querySelector(`#metadataDiv [data-name="${CSS.escape(newFieldKey)}"]`)?.scrollIntoView({
+              behavior: 'smooth',
+              block: 'center',
+            });
           });
         });
       // ADD OPTION FOR SELECT OR RADIO

d3273f05fb551819012eb62e95761e318aa482b7
Nicolas CARPi <nico-git@deltablot.email>
2026-07-24 11:38:41 +0200

    fix open_basedir and add chown instructions

--- containers/elabimg/entrypoint/docker-entrypoint.sh
+++ containers/elabimg/entrypoint/docker-entrypoint.sh
@@ -316,7 +316,7 @@ phpConf() {
     # production open_basedir conf value
     # /etc/ssl/cert.pem is for openssl and timestamp related functions
     # for /run/s6-rc... see elabftw/elabftw#5249
-    open_basedir="/.dockerenv:/elabftw/:/tmp/:/run/elabftw/tmp:/run/elabftw/cache:/usr/bin/unzip:/etc/ssl/cert.pem:/run/s6-rc/servicedirs/s6rc-oneshot-runner"
+    open_basedir="/.dockerenv:/elabftw/:/var/lib/elabftw/:/tmp/:/run/elabftw/tmp:/run/elabftw/cache:/usr/bin/unzip:/etc/ssl/cert.pem:/run/s6-rc/servicedirs/s6rc-oneshot-runner"
     # DEV MODE
     if ($dev_mode); then
         # we don't want to use opcache as we want our changes to be immediately visible

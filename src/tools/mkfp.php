3c7a47e3b0b68e7283475b963ea4b203ae6a14a7
Nicolas CARPi <nico-git@deltablot.email>
2026-07-27 15:49:02 +0200

    add openbabel

--- src/tools/mkfp.php
+++ src/tools/mkfp.php
@@ -16,6 +16,7 @@ use Elabftw\Models\Compounds;
 use Elabftw\Models\Users\Users;
 use Elabftw\Services\Fingerprinter;
 use Elabftw\Services\HttpGetter;
+use Elabftw\Services\OpenBabelFingerprinter;
 use GuzzleHttp\Client;
 
 use function dirname;
@@ -109,6 +110,7 @@ $smiles = array(
 $startTime = microtime(true);
 $requester = new Users(1, 1);
 $httpGetter = new HttpGetter(new Client(), verifyTls: false);
+//$Fingerprinter = new OpenBabelFingerprinter();
 $Fingerprinter = new Fingerprinter($httpGetter, Env::asUrl('FINGERPRINTER_URL'));
 $Compounds = new Compounds($httpGetter, $requester, fingerprinter: $Fingerprinter, requireEditRights: false);
 foreach ($smiles as $mol) {

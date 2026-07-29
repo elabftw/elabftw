fac42bed22daef287e458ff41dc18ce1df15f2ca
MoustaphaCamara <cmr.mous@gmail.com>
2026-07-29 14:57:43 +0200

    update test

--- tests/unit/Import/HandlerTest.php
+++ tests/unit/Import/HandlerTest.php
@@ -49,6 +49,7 @@ class HandlerTest extends \PHPUnit\Framework\TestCase
             'category' => 1,
             'entity_type' => 'items',
             'owner' => 2,
+            'resource_template' => null,
         );
 
         $this->assertEquals(13, $this->handler->postAction(Action::Update, $req));

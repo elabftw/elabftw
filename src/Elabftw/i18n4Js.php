3576898b89b0fe9e86332ee3e8687315166d148a
eltouma <elsa@deltablot.email>
2026-07-31 16:06:24 +0200

    Capitalize notification text

--- src/Elabftw/i18n4Js.php
+++ src/Elabftw/i18n4Js.php
@@ -82,7 +82,7 @@ final class i18n4Js
             'current-edit' => _('Currently editing'),
             'custom-fields' => _('Custom fields'),
             'delete' => _('Delete'),
-            'delete_success' => _('deleted successfully'),
+            'delete-success' => _('Deleted successfully'),
             'done' => _('Done'),
             'dropzone-upload-area' => _('Drop files here to upload'),
             'dropzone-filesize-limit' => _('File size limit:'),

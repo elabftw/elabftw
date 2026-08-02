3516e3f27159b164ce777c27fec3e01f27014cf0
Nicolas CARPi <nico-git@deltablot.email>
2026-08-02 23:24:24 +0200

    oops

--- src/sql/schema217.sql
+++ src/sql/schema217.sql
@@ -2,9 +2,11 @@
 UPDATE users
 SET token = NULL,
     token_created_at = NULL
-WHERE token IS NOT NULL;
-UPDATE users
-SET token = NULL,
-    token_created_at = NULL
 WHERE token IS NOT NULL
    OR token_created_at IS NOT NULL;
+ALTER TABLE users
+    MODIFY token CHAR(32)
+        CHARACTER SET ascii
+        COLLATE ascii_bin
+        NULL,
+    ADD UNIQUE INDEX idx_users_token (token);

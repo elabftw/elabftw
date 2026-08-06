fe4d5ae6d6552af0c4f613fe7ff6e1c5a51677b8
MoustaphaCamara <cmr.mous@gmail.com>
2026-08-06 10:27:22 +0200

    fix cys

--- src/ts/components/RegisterForm.svelte
+++ src/ts/components/RegisterForm.svelte
@@ -49,7 +49,7 @@
   
   <div class='form-group mx-auto col-md-4'>
     <div class='row'>
-      <!-- [html-validate-disable-block prefer-native-element: suppress errors from tom-select] -->
+      {@html '<!-- [html-validate-disable-block valid-for, prefer-native-element: suppress errors from tom-select] -->'}
       <label for='team'>{options.labels.team}</label>
       <select
         name='team'

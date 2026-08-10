54c801bdfd814b3bac7d08caa2cf8dca725bbe90
Nicolas CARPi <nico-git@deltablot.email>
2026-08-10 14:49:38 +0200

    extract ketcher from main bundle

--- builder.js
+++ builder.js
@@ -32,12 +32,11 @@ module.exports = (env, argv) => {
       main: [
         './src/scss/main.scss',
         './src/ts/common.ts',
+        './src/ts/page-loader.ts',
         './src/ts/create-new.ts',
         './src/ts/i18n.ts',
         './src/ts/steps-links.ts',
-        './src/ts/chem-editor.ts',
         './src/ts/dspace.ts',
-        './src/ts/ketcher-editor.jsx',
         './src/ts/compounds-table.jsx',
         './src/ts/users-table.jsx',
         './src/ts/tags.ts',
---
+++ src/ts/page-loader.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Nicolas CARPi / Deltablot
+ * @copyright 2026 Nicolas CARPi
+ * @see https://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+if (document.getElementById('ketcher-root')) {
+  void import(
+    /* webpackChunkName: 'chem-editor' */
+    './chem-editor'
+  );
+}

54658e33fed078cae5048d4afd41a565bea45d35
Nicolas CARPi <3043706+NicolasCARPi@users.noreply.github.com>
2026-08-18 14:53:01 +0200

    bug/minor: fix procurement read (#7292)

--- tests/unit/models/ProcurementRequestsTest.php
+++ tests/unit/models/ProcurementRequestsTest.php
@@ -41,6 +41,7 @@ class ProcurementRequestsTest extends \PHPUnit\Framework\TestCase
         $this->assertIsArray($this->pr->readOne());
         $this->assertIsArray($this->pr->readActiveForEntity($entityId));
         $this->assertNotEmpty($this->pr->readActiveForEntity($entityId));
+        $this->assertSame(array(), $this->otherTeamPr->readActiveForEntity($entityId));
         $this->assertIsArray($this->pr->patch(Action::Update, array('qty_received' => 2)));
     }
 

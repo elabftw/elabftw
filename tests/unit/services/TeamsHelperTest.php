e1e6ad4c1a0ab83012f5ec6a773b97c148701412
Nicolas CARPi <nico-git@deltablot.email>
2026-08-23 12:58:02 +0200

    bug/minor: better query for active users

--- tests/unit/services/TeamsHelperTest.php
+++ tests/unit/services/TeamsHelperTest.php
@@ -11,11 +11,13 @@ declare(strict_types=1);
 
 namespace Elabftw\Services;
 
+use Elabftw\Elabftw\Db;
 use Elabftw\Enums\Action;
 use Elabftw\Enums\Usergroup;
 use Elabftw\Models\Teams;
 use Elabftw\Models\Users\Users;
 use Elabftw\Traits\TestsUtilsTrait;
+use PDO;
 
 class TeamsHelperTest extends \PHPUnit\Framework\TestCase
 {
@@ -67,6 +69,49 @@ class TeamsHelperTest extends \PHPUnit\Framework\TestCase
             $this->assertFalse($this->TeamsHelper->isActiveUserInTeam($target->userid));
         } finally {
             $this->updateArchiveStatus($target->userid, 0);
+        }
+    }
+
+    public function testSuspendedUserIsNotActiveInTeam(): void
+    {
+        $target = $this->getRandomUserInTeam(1);
+        $this->assertTrue($this->TeamsHelper->isActiveUserInTeam($target->userid));
+
+        $Db = Db::getConnection();
+        $req = $Db->prepare('UPDATE users SET validated = :validated WHERE userid = :userid');
+        $req->bindValue(':userid', $target->userid, PDO::PARAM_INT);
+        $req->bindValue(':validated', 0, PDO::PARAM_INT);
+        $Db->execute($req);
+        try {
+            $this->assertFalse($this->TeamsHelper->isActiveUserInTeam($target->userid));
+        } finally {
+            $req->bindValue(':validated', 1, PDO::PARAM_INT);
+            $Db->execute($req);
+        }
+    }
+
+    public function testExpiredUserIsNotActiveInTeam(): void
+    {
+        $target = $this->getRandomUserInTeam(1);
+        $originalValidUntil = $target->userData['valid_until'];
+        $Db = Db::getConnection();
+        $req = $Db->prepare('UPDATE users SET valid_until = :valid_until WHERE userid = :userid');
+        $req->bindValue(':userid', $target->userid, PDO::PARAM_INT);
+        $req->bindValue(':valid_until', null, PDO::PARAM_NULL);
+        $Db->execute($req);
+        try {
+            $this->assertTrue($this->TeamsHelper->isActiveUserInTeam($target->userid));
+
+            $req->bindValue(':valid_until', '2000-01-01');
+            $Db->execute($req);
+            $this->assertFalse($this->TeamsHelper->isActiveUserInTeam($target->userid));
+        } finally {
+            $req->bindValue(
+                ':valid_until',
+                $originalValidUntil,
+                $originalValidUntil === null ? PDO::PARAM_NULL : PDO::PARAM_STR,
+            );
+            $Db->execute($req);
         }
     }
 

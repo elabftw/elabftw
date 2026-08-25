b8982f365f189b80fc57742dd01c97b3ee2ae943
Nicolas CARPi <nico-git@deltablot.email>
2026-08-25 17:23:19 +0200

    feat: use uuidv4 instead of uuidv1 for access keys
    uuidv1 really is the md5 of uuids

--- tests/unit/services/AccessKeyHelperTest.php
+++ tests/unit/services/AccessKeyHelperTest.php
@@ -25,6 +25,8 @@ class AccessKeyHelperTest extends \PHPUnit\Framework\TestCase
         $AkHelper = new AccessKeyHelper(EntityType::Experiments, $id);
         // set an ak
         $ak = $AkHelper->toggleAccessKey();
+        $this->assertNotNull($ak);
+        $this->assertMatchesRegularExpression('/\A[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/', $ak);
         $this->assertEquals($id, $AkHelper->getIdFromAccessKey($ak));
         // now remove ak
         $AkHelper->toggleAccessKey();

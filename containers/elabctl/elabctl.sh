2551c36d24ac362ca85d466c193fa870d3d880a2
Nicolas CARPi <nico-git@deltablot.email>
2026-09-04 09:08:33 +0200

    feat: ensure MySQL 8.4 is running

--- containers/elabctl/elabctl.sh
+++ containers/elabctl/elabctl.sh
@@ -605,8 +605,7 @@ function uninstall
 
     # remove docker images
     docker rmi elabftw/elabimg || true
-    docker rmi mysql:5.7 || true
-    docker rmi mysql:8.0 || true
+    docker rmi mysql:8.4 || true
 
     echo ""
     echo "[✓] Everything has been obliterated. Have a nice day :)"

19c710c91d2bfa6c5e267397dd4124951072e806
MoustaphaCamara <cmr.mous@gmail.com>
2026-09-10 13:09:33 -0400

    fix cypress

--- tests/cypress/integration/containers.cy.ts
+++ tests/cypress/integration/containers.cy.ts
@@ -96,14 +96,12 @@ describe('Containers', () => {
             });
 
             // deleting the entity with the option checked must also free its storage locations
+            cy.intercept('GET', `/api/v2/items/${itemId}/containers?has_any=1`).as('getItemContainers');
             cy.intercept('DELETE', `/api/v2/items/${itemId}?delete_containers=1`).as('deleteItemWithContainers');
             cy.get('button[title="More options"]').click();
             cy.get('button[data-action="toggle-modal"][data-target="deleteSelectedEntitiesModal"]').click();
-            cy.get('#deleteSelectedEntitiesModal input[name="delete_containers"]')
-              .should('be.enabled')
-              .check()
-              .should('be.checked');
             cy.get('[data-output="delete-containers-count"]').should('have.text', '5');
+            cy.wait('@getItemContainers');
             cy.get('#deleteSelectedEntitiesButton').wait(2500).click();
             cy.wait('@deleteItemWithContainers');
 
